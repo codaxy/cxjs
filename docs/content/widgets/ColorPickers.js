447835a3cb094c8d0573d41b3c457aeb9cd3f6b8
acoj1993 <a.jankovic1993@gmail.com>
2016-09-26 12:53:33 +0200

    Committing ColorPicker and ColorField

--- docs/content/widgets/ColorPickers.js
+++ docs/content/widgets/ColorPickers.js
@@ -6,7 +6,7 @@ import {ConfigTable} from '../../components/ConfigTable';
 import {HtmlElement} from 'cx/ui/HtmlElement';
 import {ColorPicker} from 'cx/ui/form/ColorPicker';
 
-import configs from './configs/ColorFiel';
+import configs from './configs/ColorPicker';
 
 export const ColorPickers = <cx>
    <Md>
@@ -29,11 +29,11 @@ export const ColorPickers = <cx>
          `}</CodeSnippet>
       </CodeSplit>
          
-        > Use 'Mouse Cursor' to navigate through the ColorPicker. Use the color range pool on the top right corner to
-        choose a color. You can do the same by entering values for H, S and L in the respective fields or R, G and B
-        similarly. The pool field below is used for selecting alpha channel, which you can also select by specifying
-        the value in the A field.
-        
+        > Use 'Mouse Cursor' to navigate through the ColorPicker. Use the color range pool on the top right corner
+        to choose a color. You can do the same by entering values for H, S and L in the respective fields or R, G
+        and B similarly. The pool field below is used for selecting alpha channel, which you can also select by
+        specifying the value in the A field.
+
         > Use the color pool on the left side of the picker to choose the shade of the color. H and A values are fixed,
         while you change the values for S and L.
          
