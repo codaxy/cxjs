5b87424aec0d539943d942fdfbe682ca9591cb39
acoj1993 <a.jankovic1993@gmail.com>
2016-09-27 15:20:14 +0200

    ColorField edit

--- docs/content/widgets/configs/ColorField.js
+++ docs/content/widgets/configs/ColorField.js
@@ -14,7 +14,7 @@ export default {
         type: 'string',
         key: true,
         description: <cx><Md>
-            Represents the format of the selected color representation.
+            Format of the color representation. Either `rgba`, `hsla` or `hex`.
             </Md></cx>
     },
     inputError: {
