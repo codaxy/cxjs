817412b2fb7ceefafe0a6fdd98f5cc764151063e
Marko Stijak <mstijak@gmail.com>
2016-10-06 21:50:38 +0200

    Undo whishful viewport size fix

--- packages/cx-core/src/util/getViewportSize.js
+++ packages/cx-core/src/util/getViewportSize.js
@@ -1,12 +1,6 @@
 import {isTouchDevice} from './isTouchDevice';
 
 export function getViewportSize() {
-   if (isTouchDevice())
-      return {
-         width: Math.max(document.body.offsetWidth, window.innerWidth),
-         height: Math.max(document.body.offsetHeight, window.innerHeight)
-      }
-
    return {
       width: window.innerWidth,
       height: window.innerHeight
