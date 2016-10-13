219ab18cfdac43950a1b29f6cae993d66f8507eb
Marko Stijak <mstijak@gmail.com>
2016-10-13 11:46:09 +0200

    Advanced labels for LabeledContainer

--- packages/cx-core/src/ui/form/LabeledContainer.js
+++ packages/cx-core/src/ui/form/LabeledContainer.js
@@ -1,31 +1,54 @@
 import {Widget} from '../Widget';
 import {PureContainer} from '../PureContainer';
 import {Label} from './Label';
+import {isSelector} from '../../data/isSelector';
 
 export class LabeledContainer extends PureContainer
 {
-   render(context, instance, key) {
-      var {data} = instance;
-      return {
-         label: this.renderLabel(key, data),
-         content: this.renderChildren(context, instance, key + '-content')
-      }
-   }
-
-   renderLabel(key, data) {
-      var options = {
-         style: {
-            width: data.labelWidth
-         }
-      };
-      return Label(key + '-label', data.label, data.id)
-   }
-
    declareData() {
       super.declareData({
-         label: undefined,
-         labelWidth: undefined
+         label: undefined
       }, ...arguments);
+   }
+
+   init() {
+
+      if (this.label != null) {
+         let labelConfig = {
+            type: Label,
+            asterisk: this.asterisk,
+            required: true
+         };
+
+         if (this.label.isComponentType)
+            labelConfig = this.label;
+         else if (isSelector(this.label))
+            labelConfig.text = this.label;
+         else
+            Object.assign(labelConfig, this.label);
+
+         this.label = Widget.create(labelConfig);
+      }
+
+      super.init();
+   }
+
+   initComponents(context, instance) {
+      return super.initComponents(...arguments, {
+         label: this.label
+      });
+   }
+
+   renderLabel(context, instance, key) {
+      if (instance.components.label)
+         return instance.components.label.render(context, key);
+   }
+
+   render(context, instance, key) {
+      return {
+         label: this.renderLabel(context, instance, key),
+         content: this.renderChildren(context, instance, key)
+      }
    }
 }
 
