941e54b0cb9b2a3911fd2951c133a008b7b42755
Marko Stijak <mstijak@gmail.com>
2016-11-14 17:24:14 +0100

    About page

--- docs/content/intro/About.js
+++ docs/content/intro/About.js
@@ -5,6 +5,9 @@ import {Content} from 'cx/ui/layout/Content';
 import {Tab} from 'cx/ui/nav/Tab';
 import {ValidationGroup} from 'cx/ui/form/ValidationGroup';
 import {TextField} from 'cx/ui/form/TextField';
+import {Checkbox} from 'cx/ui/form/Checkbox';
+import {Button} from 'cx/ui/Button';
+import {Rescope} from 'cx/ui/Rescope';
 import {LabelsLeftLayout} from 'cx/ui/layout/LabelsLeftLayout';
 import {CodeSnippet} from '../../components/CodeSnippet';
 import {Svg} from 'cx/ui/svg/Svg';
@@ -27,120 +30,104 @@ class PageController extends Controller {
             y: Math.random() * 1000,
             r: Math.random() * 20
         }));
-        this.store.set('$page.bubbles', bubbles)
+        this.store.set('bubbles', bubbles)
     }
 }
 
 export const About = <cx>
-    <Md controller={PageController}>
-        # About Cx
+    <Rescope bind="$page">
+        <Md controller={PageController}>
+            # About Cx
 
-        Cx is a modern, multi-paradigm web framework inspired by React, Redux, Ext JS, Angular, D3 and other notable open-source libraries.
-        Unlike most libraries which try to do just one thing and do it right, Cx tries to provide all necessary ingredients for building
-        cool web applications. It is particularly convenient for building admin and dashboard applications as it includes
-        a complete set of widgets with form elements, grids, navigation, charts, etc.
-        There is support for inner (form) layouts and outer (app) layouts. The router is built-in, if you’re building
-        a single page application. Cx offers very powerful data-binding mechanisms, culture dependent formatting,
-        view controllers and much more. The most important thing is that developing new widgets or modding existing
-        widgets is easy, so each application developed using Cx can have a unique look and feel.
+            Cx is a modern framework for building visually appealing, data-oriented, web applications.
 
-        In combination with Babel and webpack, Cx offers great development experience.
-        Thanks to its own babel plugin, views are written using a friendly HTML (React) like syntax.
-        Thanks to webpack, changes are applied to the page without browser refreshes and page data is preserved.
-        Again thanks to Babel, the source code is written using the latest version of JavaScript.
 
-        The following browsers are supported:
-        - Chrome
-        - Firefox
-        - Edge
-        - IE11
-        - Safari
+            Cx features a large collection of composable and configurable widgets, JSX based syntax, declarative
+            data-binding options and simplicity of use.
 
-        ## Code Samples
 
-        ### Sample 1
+            <CodeSplit>
+                <div class="widgets">
+                    <div>
+                        <Checkbox value:bind="enabled">Enable</Checkbox>
+                        <br/>
+                        <TextField value:bind="text" enabled:bind="enabled"/>
+                    </div>
+                </div>
+                <CodeSnippet putInto="code">{`
+                <Checkbox value:bind="enabled">Enable</Checkbox>
+                <br/>
+                <TextField enabled:bind="enabled" value:bind="name" />
+            `}</CodeSnippet>
+            </CodeSplit>
 
-        <CodeSplit>
 
-            Login form with basic validation.
+            The main goal of Cx is to streamline the development of complex user interfaces which traditionally required
+            using components from many different vendors.
+            Unlike many other frameworks which solve just one problem, Cx tries to provide all necessary components in
+            one consistent package.
+            That means that, out of the box, Cx includes form elements, form validation, advanced grid control,
+            navigational
+            elements, charts, routing, layout support, theming support, culture dependent formatting and more.
 
-            <div class="widgets">
-                <ValidationGroup valid:bind="$page.login.valid" layout={LabelsLeftLayout}>
-                    <TextField label="Username" required={true} value:bind="$page.login.username"/>
-                    <TextField label="Password" inputType='password' required={true} value:bind="$page.login.password"/>
-                    <button type="button" disabled:expr="!{$page.login.valid}"
+            <CodeSplit>
+                <div class="widgets">
+                    <ValidationGroup valid:bind="login.valid" layout={LabelsLeftLayout}>
+                        <TextField label="Username" required value:bind="login.username"/>
+                        <TextField label="Password" inputType='password' required value:bind="login.password"/>
+                        <Button
+                            disabled:expr="!{login.valid}"
                             onClick={(e, {store}) => {
-                                MsgBox.alert('Hello ' + store.get('$page.login.username') + '!')
-                            }}>
-                        Submit
-                    </button>
+                                MsgBox.alert('Hello ' + store.get('login.username') + '!')
+                            }}
+                        >
+                            Submit
+                        </Button>
+                    </ValidationGroup>
+                </div>
+
+                <CodeSnippet putInto="code">{`
+                <ValidationGroup layout={LabelsLeftLayout} valid:bind="login.valid">
+                    <TextField label="Username" required value:bind="login.username" />
+                    <TextField label="Password" inputType='password' required={true} value:bind="login.password" />
+                    <Button
+                        disabled:expr="!{login.valid}"
+                        onClick={(e, {store}) => {
+                            MsgBox.alert('Hello ' + store.get('login.username') + '!')
+                        }}
+                    >
+                       Submit
+                    </Button>
                 </ValidationGroup>
-            </div>
-
-            <CodeSnippet putInto="code">{`
-               <ValidationGroup layout={LabelsLeftLayout} valid:bind="$page.login.valid">
-                  <TextField label="Username" required={true} value:bind="$page.login.username" />
-                  <TextField label="Password" inputType='password' required={true} value:bind="$page.login.password" />
-                  <button type="button" disabled:expr="!{$page.login.valid}"
-                          onClick={(e, {store}) => { MsgBox.alert('Hello ' + store.get('$page.login.username') + '!')}}>
-                     Submit
-                  </button>
-               </ValidationGroup>
             `}</CodeSnippet>
 
-        </CodeSplit>
+            </CodeSplit>
 
-        ### Sample 2
+            Cx is based on modern technologies such as React, npm, babel, webpack and Sass.
 
-        <CodeSplit>
+            Cx is made for individual developers and development teams who build business applications of any size.
+            Cx provides a solid foundation and reduces the burden of dealing with UI complexities and browser quirks,
+            making the project more predictable.
 
-            A bubble chart.
+            You may already be familiar with Cx as it is inspired by the best concepts from well known front-end
+            products,
+            such as React, Redux, Ext JS, Angular and D3.
 
-            <div class="widgets">
-                <Svg style={{width: '400px', height: '350px'}}>
-                    <Chart offset='10 -15 -20 40' axes={{x: {type: 'numeric'}, y: {type: 'numeric', vertical: true}}}>
-                        <Gridlines />
-                        <BubbleGraph data:bind='$page.bubbles'/>
-                    </Chart>
-                </Svg>
-                <button type="button" onClick={(e, {controller}) => {
-                    controller.generateBubbles()
-                }}>Generate
-                </button>
-            </div>
+            ## Browser Support
 
+            The following browsers are supported:
 
-            <CodeSnippet putInto="code">{`
-               class PageController extends Controller {
-                  init() {
-                     this.generateBubbles();
-                  }
+            - Chrome
+            - Firefox
+            - Edge
+            - IE11
+            - Safari
 
-                  generateBubbles() {
-                     var bubbles = Array.from({length: 100}).map(()=> ({
-                        x: Math.random() * 1000,
-                        y: Math.random() * 1000,
-                        r: Math.random() * 20
-                     }));
-                     this.store.set('$page.bubbles', bubbles)
-                  }
-               }
-               ...
-               <Svg style={{ width: '400px', height: '350px'}}>
-                  <Chart offset='10 -15 -20 40' axes={{x:{type:'numeric'}, y: {type:'numeric', vertical: true}}}>
-                     <Gridlines />
-                     <BubbleGraph data:bind='$page.bubbles' />
-                  </Chart>
-               </Svg>
-               <button type="button" onClick={(e, {controller}) => { controller.generateBubbles() }}>Generate</button>
-            `}</CodeSnippet>
+            ## Licensing
 
-        </CodeSplit>
+            Cx framework is available under commercial license. The framework is free to use for school/hobby projects
+            with non-commercial nature.
 
-        ## Licensing
-
-        Cx framework is available under commercial license. The framework is free to use for school/hobby projects
-        with non-commercial nature.
-
-    </Md>
+        </Md>
+    </Rescope>
 </cx>
