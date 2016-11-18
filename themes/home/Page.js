ebd8eb5b55cbd558c67322f8106ab41fac486803
Marko Stijak <mstijak@gmail.com>
2016-11-18 09:40:38 +0100

    ...

--- themes/home/Page.js
+++ themes/home/Page.js
@@ -12,11 +12,18 @@ const layout = createLayout(<cx>
 const themes = [{
    name: 'Neutral',
    url: 'neutral',
-   imgUrl: '~/img/neutral.png'
+   imgUrl: '~/img/neutral.png',
+   description: "Neutral theme has mild colors and minimalistic styling. It's a perfect base for new themes."
 }, {
    name: 'Dark',
    url: 'dark',
-   imgUrl: '~/img/dark.png'
+   imgUrl: '~/img/dark.png',
+   description: "Dark themes ease the stress on your eyes after long hours of use, so they are commonly used for tools and long running applications."
+}, {
+   name: 'Playground',
+   url: 'playground',
+   imgUrl: '~/img/play.jpg',
+   description: "This is not a real theme, but a place for doing experiments and develop new features."
 }];
 
 export default <cx>
@@ -24,9 +31,14 @@ export default <cx>
       <FlexRow distance wrap center>
          <Repeater records={themes} recordName="$theme">
             <Section mod="well" pad={false}>
-               <a href:bind="$theme.url">
-                  <img src:bind="$theme.imgUrl" />
-                  <p text:bind="$theme.name" />
+               <a href:bind="$theme.url" style="display:table">
+                  <div style="display: table-row; width:1px">
+                     <img src:bind="$theme.imgUrl" />
+                  </div>
+                  <div style="display: table-cell; width:1px; padding: 0 1.5rem">
+                     <h3 text:bind="$theme.name" />
+                     <p text:bind="$theme.description" />
+                  </div>
                </a>
             </Section>
          </Repeater>
