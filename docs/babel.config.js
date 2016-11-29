50c5296d4a2cd87e34b2b474c85d85e308ffa5ef
Marko Stijak <mstijak@gmail.com>
2016-11-29 21:54:19 +0100

    package cleaning

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     "cacheDirectory": true,
     "cacheIdentifier": "v7",
-    "presets": ["es2015-loose", "stage-0"],
+    "presets": [
+        ["es2015", { loose: true }],
+        "stage-0"
+    ],
     "plugins": [
         "babel-plugin-cx",
         ["babel-plugin-transform-react-jsx", {"pragma": "VDOM.createElement"}]
