8269886d2e43d980f75b76cea728527fefc62633
Marko Stijak <mstijak@gmail.com>
2016-12-06 20:27:23 +0100

    New env package

--- packages/babel-preset-cx-env/index.js
+++ packages/babel-preset-cx-env/index.js
@@ -1,16 +1,28 @@
-module.exports = function(context) {
-   let opts = arguments[1] || {};
+module.exports = function (context) {
+   var opts = arguments[1] || {
+         modules: false
+      };
+
    var pragma = opts.pragma || "VDOM.createElement";
    var imports = !opts.cx || typeof opts.cx.imports == 'undefined' ? true : opts.cx.imports;
+   var plugins = [];
+
+   if (imports !== false) {
+      if (typeof imports == 'object')
+         plugins.push(['transform-cx-imports', imports]);
+      else
+         plugins.push('transform-cx-imports');
+   }
+
+   plugins.push(
+      'transform-object-rest-spread',
+      "transform-function-bind",
+      'cx',
+      ["transform-react-jsx", {"pragma": pragma}]
+   );
 
    return {
       presets: [['babel-preset-env', opts]],
-      plugins: [
-         ['transform-cx-imports', imports],
-         'transform-object-rest-spread',
-         "transform-function-bind",
-         'cx',
-         ["transform-react-jsx", {"pragma": pragma}]
-      ]
+      plugins: plugins
    };
 };
