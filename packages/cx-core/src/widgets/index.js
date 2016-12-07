841053da40666b885bcafe317c40aef30c526739
Marko Stijak <mstijak@gmail.com>
2016-12-07 09:31:20 +0100

    Adding Section and Heading to widgets.js

--- packages/cx-core/src/widgets/index.js
+++ packages/cx-core/src/widgets/index.js
@@ -2,7 +2,6 @@ export * from '../ui/StaticText';
 export * from '../ui/Text';
 export * from '../ui/HtmlElement';
 export * from '../ui/Button';
-export * from '../ui/Container';
 export * from '../ui/DocumentTitle';
 export * from '../ui/List';
 export * from '../ui/Sandbox';
@@ -10,10 +9,14 @@ export * from '../ui/Repeater';
 export * from '../ui/Cx';
 export * from '../ui/CxCredit';
 export * from '../ui/layout/FlexBox';
+export * from '../ui/Heading';
+export * from '../ui/Section';
 
+//re-export
 import { Widget }  from '../ui/Widget';
 export { Widget };
 
+//re-export
 import { PureContainer } from '../ui/PureContainer';
 export { PureContainer };
 
