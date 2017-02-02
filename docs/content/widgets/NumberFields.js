76a856533fa7a0f3b7b8e17218e02438cc5d3089
Sasha Tatar <sasa.tatar@gmail.com>
2017-02-02 10:46:30 +0100

    Update NumberFields.js

--- docs/content/widgets/NumberFields.js
+++ docs/content/widgets/NumberFields.js
@@ -44,21 +44,23 @@ export const NumberFields = <cx>
             <Content name="code">
                 <CodeSnippet fiddle="PokioWF7">{`
                <div layout={LabelsLeftLayout}>
-                  <NumberField label="Standard" value:bind="$page.number" autoFocus/>
-                  <NumberField label="Disabled" value:bind="$page.number" disabled />
-                  <NumberField label="Readonly" value:bind="$page.number" readOnly />
-                  <NumberField label="Placeholder" value:bind="$page.number" placeholder="Type something here..." />
-                  <NumberField label="Validation" value:bind="$page.number" minValue={18} placeholder="Above 18..." />
-               </div>
-               <div layout={LabelsLeftLayout}>
-                  <NumberField label="Formatted" value:bind="$page.number" format="n;2" />
-                  <NumberField label="Percentage" value:bind="$page.number" format="ps" />
-                  <NumberField label="Suffix" value:bind="$page.number" format="suffix; kg" />
-                  <NumberField label="Required" value:bind="$page.number" required />
-                  <NumberField label="Styled" value:bind="$page.number" inputStyle={{border: '1px solid green'}} />
-                  <NumberField label="View" value:bind="$page.number" mode="view" />
-                  <NumberField label="EmptyText" value:bind="$page.number" mode="view" emptyText="N/A" />
-               </div>
+                    <NumberField label="Standard" value:bind="$page.number" autoFocus/>
+                    <NumberField label="Disabled" value:bind="$page.number" disabled/>
+                    <NumberField label="Readonly" value:bind="$page.number" readOnly/>
+                    <NumberField label="Placeholder" value:bind="$page.number" placeholder="Type something here..."/>
+                    <NumberField label="Validation" value:bind="$page.number" minValue={18} placeholder="Above 18..."/>
+                    <NumberField label="Currency" value:bind="$page.number" placeholder="EUR" format="currency;EUR"/>
+                    <NumberField label="Currency" value:bind="$page.number" placeholder="USD" format="currency;USD"/>
+                </div>
+                <div layout={LabelsLeftLayout}>
+                    <NumberField label="Formatted" value:bind="$page.number" format="n;2"/>
+                    <NumberField label="Percentage" value:bind="$page.number" format="ps"/>
+                    <NumberField label="Suffix" value:bind="$page.number" format="suffix; kg"/>
+                    <NumberField label="Required" value:bind="$page.number" required/>
+                    <NumberField label="Styled" value:bind="$page.number" inputStyle={{border: '1px solid green'}}/>
+                    <NumberField label="View" value:bind="$page.number" mode="view"/>
+                    <NumberField label="EmptyText" value:bind="$page.number" mode="view" emptyText="N/A"/>
+                </div>
             `}</CodeSnippet>
             </Content>
         </CodeSplit>
