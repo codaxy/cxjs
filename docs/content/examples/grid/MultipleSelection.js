3137393821247f620eeb5d1decef4762c88fae8b
Marko Stijak <mstijak@gmail.com>
2017-02-09 22:33:48 +0100

    Use indeterminate checkbox in grid multiple selection example

--- docs/content/examples/grid/MultipleSelection.js
+++ docs/content/examples/grid/MultipleSelection.js
@@ -18,10 +18,20 @@ class PageController extends Controller {
             selected: false
         })));
 
-        this.addTrigger('select-all', ['$page.selectAll'], (value) => {
-            this.store.set('$page.records', this.store.get('$page.records')
-                .map(r => Object.assign({}, r, {selected: value})));
+        this.addTrigger('select-all-click', ['$page.selectAll'], (value) => {
+            if (value != null)
+                this.store.set('$page.records', this.store.get('$page.records')
+                    .map(r => Object.assign({}, r, {selected: value})));
         });
+
+        this.addTrigger('item-click', ['$page.records'], (records) => {
+            if (records.every(a => a.selected))
+                this.store.set('$page.selectAll', true);
+            else if (records.every(a => !a.selected))
+                this.store.set('$page.selectAll', false);
+            else
+                this.store.set('$page.selectAll', null);
+        }, true);
     }
 }
 
@@ -41,7 +51,7 @@ export const MultipleSelection = <cx>
                 records:bind='$page.records'
                 style={{width: "100%"}}
                 columns={[{
-                    header: {items: <cx><Checkbox value:bind="$page.selectAll"/></cx>},
+                    header: {items: <cx><Checkbox value:bind="$page.selectAll" indeterminate /></cx>},
                     field: 'selected',
                     style: 'width: 1px',
                     items: <cx><Checkbox value:bind="$record.selected" /></cx>
@@ -55,8 +65,7 @@ export const MultipleSelection = <cx>
             />
 
             See also:
-
-         - [Selection](~/concepts/selections)
+            - [Selection](~/concepts/selections)
 
             <Content name="code">
                 <CodeSnippet fiddle="XEEzkJdp">{`
@@ -71,25 +80,41 @@ export const MultipleSelection = <cx>
                         selected: false
                      })));
 
-                     this.addTrigger('select-all', ['$page.selectAll'], (value, records) => {
-                        this.store.set('$page.records', this.store.get('$page.records')
-                           .map(r=>Object.assign({}, r, {selected: value})));
-                     });
+                     this.addTrigger('select-all-click', ['$page.selectAll'], (value) => {
+                        if (value != null)
+                            this.store.set('$page.records', this.store.get('$page.records')
+                                .map(r => Object.assign({}, r, {selected: value})));
+                    });
+
+                    this.addTrigger('item-click', ['$page.records'], (records) => {
+                        if (records.every(a => a.selected))
+                            this.store.set('$page.selectAll', true);
+                        else if (records.every(a => !a.selected))
+                            this.store.set('$page.selectAll', false);
+                        else
+                            this.store.set('$page.selectAll', null);
+                    }, true);
                   }
                }
 
                ...
 
-               <Grid records:bind='$page.records' style={{width: "100%"}}
-                     columns={[
-                        { header: { items: <cx><Checkbox value:bind="$page.selectAll" /></cx> }, field: 'selected', items: <cx><Checkbox value:bind="$record.selected" /></cx> },
-                        { header: 'Name', field: 'fullName', sortable: true },
-                        { header: 'Phone', field: 'phone' },
-                        { header: 'City', field: 'city', sortable: true }
-                     ]}
-                     selection={{type: PropertySelection, bind:"$page.selection", multiple: true}}
-                     sorters:bind="$page.sorters"
-               />`}
+               <Grid
+                   records:bind='$page.records'
+                   tyle={{width: "100%"}}
+                   columns={[{
+                       header: {items: <cx><Checkbox value:bind="$page.selectAll" indeterminate /></cx>},
+                       field: 'selected',
+                       style: 'width: 1px',
+                       items: <cx><Checkbox value:bind="$record.selected" /></cx>
+                   },
+                       {header: 'Name', field: 'fullName', sortable: true},
+                       {header: 'Phone', field: 'phone'},
+                       {header: 'City', field: 'city', sortable: true}
+                   ]}
+                   selection={{type: PropertySelection, bind: "$page.selection", multiple: true}}
+                   sorters:bind="$page.sorters"
+                />`}
                 </CodeSnippet>
             </Content>
 
