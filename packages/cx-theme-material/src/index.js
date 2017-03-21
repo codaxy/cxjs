88ff477290576d72b8c076c8d3c778948ee235b2
Ognjen <ognjen.kremenovic@codaxy.com>
2017-03-21 11:51:14 +0100

    forward icons created - work in progress

--- packages/cx-theme-material/src/index.js
+++ packages/cx-theme-material/src/index.js
@@ -41,3 +41,22 @@ Icon.register('drop-down', props => Icon.render('keyboard_arrow_down', props));
 Icon.register('sort-asc', props => Icon.render('arrow_upward', props));
 Icon.register('folder-open', props => Icon.render('folder_open', props));
 Icon.register('file', props => Icon.render('insert_drive_file', props));
+Icon.register('forward', props => Icon.render('arrow_forward', props));
+
+Icon.register('forward', props => {
+  props = { ...props };
+  props.className = 'material-icons ' + (props.className || '');
+  return <span style={{ display: 'inline-block' }}>
+     <i {...props} style={{ transform: 'translate(3px, 12px) rotate(180deg)' }} >navigate_next</i>
+     <i {...props} style={{ transform: 'translate(-3px, -12px) rotate(180deg)' }}>navigate_next</i>
+  </span>;
+});
+
+//Icon.register('forward', props => {
+//  props = { ...props };
+//  props.className = 'material-icons ' + (props.className || '');
+//  return <span style={{ display: 'inline-block', position: 'relative' }}>
+//     <i {...props} style={{ position: 'absolute', top: '-6px', right: '3px'}} >navigate_next</i>
+//     <i {...props} style={{ position: 'absolute', bottom: '-6px', left: '3px' }}>navigate_next</i>
+//  </span>;
+//});
