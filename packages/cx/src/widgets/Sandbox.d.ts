523746280d0e13961250323d747c84d0e0e37dac
Ranka Arsenovic <rannkaa@gmail.com>
2017-03-29 15:22:13 +0200

    Fix required commit for Sandbox

--- packages/cx/src/widgets/Sandbox.d.ts
+++ packages/cx/src/widgets/Sandbox.d.ts
@@ -1,9 +1,9 @@
 import * as Cx from '../core';
 
 interface SandboxProps extends Cx.PureContainerProps {
-    storage?: Cx.BooleanProp,
-    key?: Cx.BooleanProp,
-    recordName: boolean
+    storage: Cx.BooleanProp,
+    key: Cx.BooleanProp,
+    recordName?: boolean
 }
 
 export class Sandbox extends Cx.Widget<SandboxProps> {}
