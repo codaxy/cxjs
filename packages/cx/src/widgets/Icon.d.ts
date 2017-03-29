39234e8732d3bd70e16329c4274f1d0d7682739a
Ranka Arsenovic <rannkaa@gmail.com>
2017-03-29 15:32:33 +0200

    Icon prop

--- packages/cx/src/widgets/Icon.d.ts
+++ packages/cx/src/widgets/Icon.d.ts
@@ -1,3 +1,11 @@
 import * as Cx from '../core';
 
-export class Icon extends Cx.Widget<any> {}
+interface IconProps extends Cx.WidgetProps{
+    name?: Cx.StringProp,
+    className?: Cx.Prop<string | {}>,
+    class?: Cx.Prop<string | {}>,
+    style?: Cx.Prop<string | {}>,
+    baseClass?: string
+}
+
+export class Icon extends Cx.Widget<IconProps> {}
