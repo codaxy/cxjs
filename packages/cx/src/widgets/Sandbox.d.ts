f6372bed1d97aa5aa5391615bd20826def072748
Ranka Arsenovic <rannkaa@gmail.com>
2017-03-29 15:02:14 +0200

    Sandbox prop

--- packages/cx/src/widgets/Sandbox.d.ts
+++ packages/cx/src/widgets/Sandbox.d.ts
@@ -1,3 +1,9 @@
 import * as Cx from '../core';
 
-export class Sandbox extends Cx.Widget<any> {}
+interface SandboxProps extends Cx.PureContainerProps {
+    storage?: Cx.BooleanProp,
+    key?: Cx.BooleanProp,
+    recordName: boolean
+}
+
+export class Sandbox extends Cx.Widget<SandboxProps> {}
