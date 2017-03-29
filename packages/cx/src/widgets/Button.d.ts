70b875872b7cffcabf3be2872306b0dd08142798
Sasa Tatar <sasa.tatar@gmail.com>
2017-03-29 11:45:43 +0200

    Add Button props definitions

--- packages/cx/src/widgets/Button.d.ts
+++ packages/cx/src/widgets/Button.d.ts
@@ -1,3 +1,10 @@
 import * as Cx from '../core';
+/**  */
+interface ButtonProps extends Cx.HtmlElementProps {
+   confirm?: string | object,
+   disabled?: boolean,
+   pressed?: boolean,
+   icon?: string
+}
 
 export class Button extends Cx.Widget<any> {}
