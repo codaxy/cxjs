8ee86ef281aea3e0a87c21e892714c656e0f1ea1
Ranka Arsenovic <rannkaa@gmail.com>
2017-03-29 15:59:59 +0200

    Icon with comments

--- packages/cx/src/widgets/Icon.d.ts
+++ packages/cx/src/widgets/Icon.d.ts
@@ -1,10 +1,17 @@
 import * as Cx from '../core';
 
 interface IconProps extends Cx.WidgetProps{
+    /** Name under which the icon is registered. */
     name?: Cx.StringProp,
+    /** Additional CSS classes to be applied to the field. 
+     * If an object is provided, all keys with a "truthy" value will be added to the CSS class list. */
     className?: Cx.Prop<string | {}>,
+    /** Additional CSS classes to be applied to the field. 
+     * If an object is provided, all keys with a "truthy" value will be added to the CSS class list. */
     class?: Cx.Prop<string | {}>,
+    /** Style object applied to the wrapper div. Used for setting the dimensions of the field. */
     style?: Cx.Prop<string | {}>,
+    /** Base CSS class to be applied to the element. Default is `icon`. */
     baseClass?: string
 }
 
