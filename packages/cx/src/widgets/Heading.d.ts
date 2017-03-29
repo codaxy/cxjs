e430d986585e5837d9e7f4dad63f76d286e258f6
Ranka Arsenovic <rannkaa@gmail.com>
2017-03-29 13:52:30 +0200

    Heading prop

--- packages/cx/src/widgets/Heading.d.ts
+++ packages/cx/src/widgets/Heading.d.ts
@@ -1,3 +1,11 @@
 import * as Cx from '../core';
 
-export class Heading extends Cx.Widget<any> {}
+interface HeadingProps extends Cx.HtmlElementProps{
+    /** Heading level.
+     * Allowed values go from 1 to 6. Default is 2. */
+    level?: number,
+    /** Base CSS class. Default is `heading`. */
+    baseClass?: string
+}
+
+export class Heading extends Cx.Widget<HeadingProps> {}
