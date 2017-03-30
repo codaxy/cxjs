e2e43f192c00597ad76bc8e6eea614a673adbe90
Ranka Arsenovic <rannkaa@gmail.com>
2017-03-30 12:29:21 +0200

    ...

--- packages/cx/src/widgets/HtmlElement.d.ts
+++ packages/cx/src/widgets/HtmlElement.d.ts
@@ -1,3 +1,12 @@
 import * as Cx from '../core';
 
+interface HtmlElementProps extends Cx.PureContainerProps{
+    text?: Cx.StringProp,
+    innerHtml?: Cx.StringProp,
+    attrs?: ,
+    data?: ,
+    tag?: string,
+    div?: 
+}
+
 export class HtmlElement extends Cx.Widget<Cx.HtmlElementProps> {}
--- packages/cx/src/widgets/List.d.ts
+++ packages/cx/src/widgets/List.d.ts
@@ -1,3 +1,25 @@
 import * as Cx from '../core';
 
-export class List extends Cx.Widget<any> {}
+interface ListProps extends Cx.WidgetProps {
+    /** An array of records to be displayed in the list. */
+    records?: Cx.RecordsProp,
+    /** Used for sorting the list. */
+    sorters?: Cx.Sorter[],
+    filterParams?: , 
+    /** Style object applied to the wrapper div. Used for setting the dimensions of the field. */
+    style?: Cx.StyleProp,
+    itemStyle?: ,
+    /** 
+     * Additional CSS classes to be applied to the field. 
+     * If an object is provided, all keys with a "truthy" value will be added to the CSS class list.
+     */
+    class?: Cx.ClassProp,
+    /** 
+     * Additional CSS classes to be applied to the field. 
+     * If an object is provided, all keys with a "truthy" value will be added to the CSS class list. 
+     */
+    className?: Cx.ClassProp,
+    emptyText?:  
+}
+
+export class List extends Cx.Widget<ListProps> {}
