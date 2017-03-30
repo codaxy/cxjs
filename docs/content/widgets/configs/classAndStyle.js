4883a0569ce9599507529b445534a77d1dfe1863
Sasa Tatar <sasa.tatar@gmail.com>
2017-03-30 14:52:01 +0200

    Update style and class configs in docs

--- docs/content/widgets/configs/classAndStyle.js
+++ docs/content/widgets/configs/classAndStyle.js
@@ -6,14 +6,14 @@ export default {
       type: 'string/object',
       alias: 'className',
       description: <cx><Md>
-         Additional CSS classes to be applied to the field. If an object is provided, all keys with a
+         Additional CSS classes to be applied to the element. If an object is provided, all keys with a
          "truthy" value will be added to the CSS class list.
       </Md></cx>
    },
    style: {
       type: 'string/object',
       description: <cx><Md>
-         Style object applied to the wrapper div. Used for setting the dimensions of the field.
+         Style object applied to the wrapper div. Used for setting the dimensions of the element.
       </Md></cx>
    }
 };
