95d7f483afc23036871dd9123f932cd4e8db1efc
Sasa Tatar <sasa.tatar@gmail.com>
2017-03-31 16:13:34 +0200

    Add Field and DateField interfaces

--- packages/cx/src/widgets/form/DateField.d.ts
+++ packages/cx/src/widgets/form/DateField.d.ts
@@ -1,3 +1,55 @@
 import * as Cx from '../../core';
+import { FieldProps } from './Field';
 
-export class DateField extends Cx.Widget<any> {}
+interface DateFieldProps extends FieldProps {
+   
+   /** Selected datee. This should be a valid date string consumable by Date.parse function. */
+   value?: Cx.StringProp | Date,
+
+   /** Defaults to `false`. Set to `true` to disable the field. */
+   disabled?: Cx.BooleanProp,
+
+   /** Defaults to false. Used to make the field read-only. */
+   readOnly?: Cx.BooleanProp,
+   
+   /** The opposite of `disabled`. */
+   enabled?: Cx.BooleanProp, 
+
+   /** Default text displayed when the field is empty. */
+   placeholder?: Cx.StringProp,
+
+   /** Defaults to `false`. Used to make the field required. */
+   required?: Cx.StringProp,
+
+   /** Minimum date value. This should be a valid date string consumable by Date.parse function. */
+   minValue?: Cx.StringProp | Date,
+
+   /** Minimum (exclusive) date value. This should be a Date object or a valid date string consumable by Date.parse function. */
+   minExclusive?: Cx.StringProp | Date,
+
+   /** Maximum date value. This should be a Date object or a valid date string consumable by Date.parse function. */
+   maxValue?: Cx.StringProp | Date,
+   
+   /** Maximum (exclusive) date value. This should be a Date object or a valid date string consumable by Date.parse function. */
+   maxExclusive?: Cx.StringProp | Date,
+   
+   /** Date format used to display the selected date. See Formatting for more details. */
+   format?: Cx.StringProp,
+
+   /** Base CSS class to be applied to the field. Defaults to `datefield`. */
+   baseClass?: string,
+
+   memoize?: boolean,
+   suppressErrorTooltipsUntilVisited?: boolean,
+
+   /** Name of the icon to be put on the left side of the input. */
+   icon?: string,
+
+   /** Set to false to hide the clear button. It can be used interchangeably with the hideClear property. Default value is true. */
+   showClear?: boolean,
+
+   /** Set to true to hide the clear button. It can be used interchangeably with the showClear property. Default value is false. */
+   hideClear?: boolean
+}
+
+export class DateField extends Cx.Widget<DateFieldProps> {}
--- packages/cx/src/widgets/form/Field.d.ts
+++ packages/cx/src/widgets/form/Field.d.ts
@@ -1,3 +1,35 @@
 import * as Cx from '../../core';
 
-export class Field extends Cx.Widget<any> {}
+export interface FieldProps extends Cx.StyledContainerProps {
+   
+   /** Field label. For advanced use cases, see Labels. */
+   label?: Cx.StringProp,
+
+   labelWidth?: Cx.NumberProp,
+   mode?: any, // TODO: check type
+   id?: string | number | Cx.Binding | Cx.Selector<string | number>,
+   
+   /** Used for validation. If error evaluates to non-null, the field is marked in red. */
+   error?: Cx.StringProp,
+
+   /** Style object applied to the input element. Used for setting visual elements, such as borders and backgrounds. */
+   inputStyle?: Cx.StyleProp,
+
+   /** Additional attributes that should be rendered on the input element. E.g. inputAttrs={{ autoComplete: "off" }}. */
+   inputAttrs?: Cx.Config,
+   
+   /** Text to be rendered in view mode when the field is empty. */
+   emptyText?: Cx.StringProp,
+
+   /** Validation errors are not shown until the user visits the field. Setting this field to `true` will cause that 
+    * validation error indicators become visible immediately. */
+   visited?: Cx.BooleanProp,
+
+   /** Set to `true` to automatically focus the field, after it renders for the first time. */
+   autoFocus?: Cx.BooleanProp,
+
+   /** Defines how to present validation errors. Default mode is `tooltip`. Other options are `help` and `help-block`. */
+   validationMode?: 'tooltip' | 'help' | 'help-block';
+}
+
+export class Field extends Cx.Widget<Cx.FieldProps> {}
