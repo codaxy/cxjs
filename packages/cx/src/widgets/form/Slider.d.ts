2a421ea745d30fbc6d25ccb51ab79a65a39ebdab
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-03 15:22:04 +0200

    Add Slider interface

--- packages/cx/src/widgets/form/Slider.d.ts
+++ packages/cx/src/widgets/form/Slider.d.ts
@@ -1,3 +1,50 @@
 import * as Cx from '../../core';
+import { FieldProps } from './Field';
 
-export class Slider extends Cx.Widget<any> {}
+interface SliderProps extends FieldProps {
+
+   /** Low value of the slider range. */
+   from?: Cx.NumberProp,
+
+   /** High value of the slider range. */
+   to?: Cx.NumberProp,
+
+   /** Rounding step. */
+   step?: Cx.NumberProp,
+
+   /** Minimum allowed value. Default is `0`. */
+   minValue?: Cx.NumberProp,
+
+   /** Maximum allowed value. Default is `100`. */
+   maxValue?: Cx.NumberProp,
+
+   /** Defaults to `false`. Set to `true` to disable the field. */
+   disabled?: Cx.BooleanProp,
+
+   /** Defaults to `false`. Used to make the field read-only. */
+   readOnly?: Cx.BooleanProp,
+
+   /** Style object to be applied on the selected axis range. */
+   rangeStyle?: Cx.StyleProp,
+
+   /** Style object to be applied on the handle. */
+   handleStyle?: Cx.StyleProp,
+
+   /** Minimum allowed value. Default is `0`. */
+   min?: number,
+
+   /** Maximum allowed value. Default is `100`. */
+   max?: number,
+   
+   /** High value of the slider range. */
+   value?: number,
+
+   /** Base CSS class to be applied to the field. Defaults to `slider`. */
+   baseClass?: string,
+
+   /** Set to `true` to orient the slider vertically. */
+   vertical?: boolean
+
+}
+
+export class Slider extends Cx.Widget<SliderProps> {}
