fb601dee609099bb6044d4d5b9ee4ad8430aff6b
Marko Stijak <mstijak@gmail.com>
2017-04-03 11:56:40 +0200

    Remove extra line

--- docs/content/widgets/LookupFields.js
+++ docs/content/widgets/LookupFields.js
@@ -67,12 +67,6 @@ export const LookupFields = <cx>
                         values:bind="$page.s10ids"
                         options:bind="$page.options10"
                         multiple/>
-
-                    <LookupField
-                        label="Records"
-                        values:bind="$page.s10ids"
-                        options:bind="$page.options10"
-                        multiple disabled/>
                 </div>
                 <div layout={LabelsLeftLayout}>
                     <LookupField
