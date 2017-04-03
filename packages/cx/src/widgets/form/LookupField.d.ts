ef0605125af642d26f4b59d8811731fe8449d721
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-03 12:22:01 +0200

    LookupField, MonthField, TextField

--- packages/cx/src/widgets/form/LookupField.d.ts
+++ packages/cx/src/widgets/form/LookupField.d.ts
@@ -1,3 +1,106 @@
 import * as Cx from '../../core';
+import { FieldProps } from './Field';
 
-export class LookupField extends Cx.Widget<any> {}
+interface LookupFieldProps extends FieldProps {
+
+   /** Defaults to `false`. Set to `true` to enable multiple selection. */
+   multiple?: Cx.BooleanProp,
+
+   /** Selected value. Used only if `multiple` is set to `false`. */
+   value?: Cx.Prop<number | string>,
+
+   /** A list of selected records. Used only if `multiple` is set to `true`. */
+   records?: Cx.Record[],
+
+   /** Text associated with the selection. Used only if `multiple` is set to `false`. */
+   text?: Cx.StringProp,
+
+   /** Defaults to `false`. Set to `true` to disable the field. */
+   disabled?: Cx.BooleanProp,
+
+   /** The opposite of `disabled`. */
+   enabled?: Cx.BooleanProp,
+
+   /** Default text displayed when the field is empty. */
+   placeholder?: Cx.StringProp,
+
+   /** Defaults to `false`. Used to make the field required. */
+   required?: Cx.BooleanProp,
+
+   /** A list of available options. */
+   options?: Cx.Prop<Array<string>>,
+
+   /** 
+    * Set to `true` to hide the clear button. It can be used interchangeably with the `showClear` property. 
+    * No effect if `multiple` is used. Default value is `false`. 
+    */
+   hideClear?: boolean,
+
+   /** 
+    * Set to `false` to hide the clear button. It can be used interchangeably with the `hideClear` property. 
+    * No effect if `multiple` is used. Default value is `true`. 
+    */
+   showClear?: boolean,
+
+   /** Base CSS class to be applied to the field. Defaults to `lookupfield`. */
+   baseClass?: string,
+
+   memoize?: boolean,
+   queryDelay?: number,
+   minQueryLength?: number,
+
+   /** Set to `true` to hide the search field. */
+   hideSearchField?: boolean,
+
+   /** 
+    * Number of options required to show the search field. 
+    * If there are only a few options, there is no need for search. Defaults to `7`. 
+    */
+   minOptionsForSearchField?: number,
+
+   loadingText?: string,
+   queryErrorText?: string,
+   noResultsText?: string,
+
+   /** Name of the field which holds the id of the option. Default value is `id`. */
+   optionIdField?: string,
+
+   /** Name of the field which holds the display text of the option. Default value is `text`. */
+   optionTextField?: string,
+
+   /**
+    * Available only in `multiple` selection mode and without custom `bindings`. 
+    * Name of the field to store id of the selected value. Default value is `id`. 
+    */
+   valueIdField?: string,
+
+   /** 
+    * Available only in `multiple` selection mode. 
+    * Name of the field to store display text of the selected value. Default value is `text`. 
+    */
+   valueTextField?: string,
+
+   suppressErrorTooltipsUntilVisited?: boolean,
+
+   /** 
+    * If `true` `onQuery` will be called only once to fetch all options. 
+    * After that options are filtered client-side. 
+    */
+   fetchAll?: boolean,
+
+   /** 
+    * If this flag is set along with `fetchAll`, fetched options are cached for the lifetime of the widget. 
+    * Otherwise, data is fetched whenever the dropdown is shown. 
+    */
+   cacheAll?: boolean,
+   
+   /** Close the dropdown after selection. Default is `true`. */
+   closeOnSelect?: boolean,
+
+   minQueryLengthMessageText?: string,
+
+   /** Name of the icon to be put on the left side of the input. */
+   icon?: string
+}
+
+export class LookupField extends Cx.Widget<LookupFieldProps> {}
--- packages/cx/src/widgets/form/MonthField.d.ts
+++ packages/cx/src/widgets/form/MonthField.d.ts
@@ -1,3 +1,69 @@
 import * as Cx from '../../core';
+import { FieldProps } from './Field';
 
-export class MonthField extends Cx.Widget<any> {}
+interface MonthFieldProps extends FieldProps {
+   
+   /** Either `view` or `edit` (default). In view mode, the field is displayed as plain text. */
+   mode?: Cx.Prop<"view" | "edit">,
+
+   /** Set to `true` to allow range select. */
+   range?: Cx.BooleanProp,
+
+   /** Defaults to `false`. Set to `true` to disable the field. */
+   disabled?: Cx.BooleanProp,
+
+   /** Defaults to `false`. Used to make the field read-only. */
+   readOnly?: Cx.BooleanProp,
+
+   /** The opposite of `disabled`. */
+   enabled?: Cx.BooleanProp,
+
+   /** Default text displayed when the field is empty. */
+   placeholder?: Cx.StringProp,
+
+   /** Defaults to `false`. Used to make the field required. */
+   required?: Cx.BooleanProp,
+
+   /** Minimum date value. This should be a valid date string consumable by Date.parse function. */
+   minValue?: Cx.StringProp | Date,
+
+   /** Minimum (exclusive) date value. This should be a Date object or a valid date string consumable by Date.parse function. */
+   minExclusive?: Cx.StringProp | Date,
+
+   /** Maximum date value. This should be a Date object or a valid date string consumable by Date.parse function. */
+   maxValue?: Cx.StringProp | Date,
+   
+   /** Maximum (exclusive) date value. This should be a Date object or a valid date string consumable by Date.parse function. */
+   maxExclusive?: Cx.StringProp | Date,
+
+   /** String representing culture. Default is `en` */
+   culture?: string,
+
+   /** 
+    * Set to `true` to hide the clear button. It can be used interchangeably with the `showClear` property. 
+    * Default value is `false`. 
+    */
+   hideClear?: boolean,
+
+   /** Base CSS class to be applied on the field. Defaults to `monthfield`. */
+   baseClass?: string,
+
+   memoize?: boolean,
+   maxValueErrorText?: string,
+   maxExclusiveErrorText?: string,
+   minValueErrorText?: string,
+   minExclusiveErrorText?: string,
+   inputErrorText?: string,
+   suppressErrorTooltipsUntilVisited?: boolean,
+
+   /** Name of the icon to be put on the left side of the input. */
+   icon?: string,
+
+   /** 
+    * Set to `false` to hide the clear button. It can be used interchangeably with the `hideClear` property. 
+    * Default value is `true`. 
+    */
+   showClear?: boolean
+}
+
+export class MonthField extends Cx.Widget<MonthFieldProps> {}
