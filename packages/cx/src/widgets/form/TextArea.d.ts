ae4ffe9ad9bdc77f910c488e6c0dafb9cfd1db15
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-03 15:22:26 +0200

    TextArea interface

--- packages/cx/src/widgets/form/TextArea.d.ts
+++ packages/cx/src/widgets/form/TextArea.d.ts
@@ -1,3 +1,15 @@
 import * as Cx from '../../core';
+import { TextFieldProps } from './TextField';
 
-export class TextArea extends Cx.Widget<any> {}
+interface TextAreaProps extends TextFieldProps {
+   
+   /** Specifies the number of visible lines. */
+   rows?: Cx.NumberProp,
+
+   /** Event used to report on a new value. Defaults to `blur`. Other permitted value is `input`. */
+   reachOn?: string,
+   
+   suppressErrorTooltipsUntilVisited?: boolean
+}
+
+export class TextArea extends Cx.Widget<TextAreaProps> {}
