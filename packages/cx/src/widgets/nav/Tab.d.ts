979307d528c7bb6763c325d08fd348f10e33ebcd
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-04 13:37:37 +0200

    Tab prop

--- packages/cx/src/widgets/nav/Tab.d.ts
+++ packages/cx/src/widgets/nav/Tab.d.ts
@@ -1,3 +1,28 @@
 import * as Cx from '../../core';
 
-export class Tab extends Cx.Widget<any> {}
+interface TabProps extends Cx.HtmlElementProps {
+
+   /** A value to be written to the `value` property if the tab is clicked. */
+   tab?: Cx.Prop<string | number>,
+
+   /** 
+    * Value of the currently selected tab. 
+    * If `value` is equal to `tab`, the tab appears active. 
+    */
+   value?: Cx.StringProp,
+
+   /** Set to `true` to disable selection. */
+   disabled?:Cx.StringProp,
+ 
+   /** Base CSS class to be applied to the element. No class is applied by default. */
+   baseClass?: string,
+
+   /** Name of the HTML element to be rendered. Default is `div`. */
+   tag?: string,
+   
+   /** Determines if tab should receive focus on `mousedown` event. 
+    *  Default is `false`, which means that focus can be set only using the keyboard `Tab` key.
+    */
+   focusOnMouseDown?: boolean
+
+export class Tab extends Cx.Widget<TabProps> {}
