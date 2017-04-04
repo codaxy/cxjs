0fcc9ac83de4f2c9b52c2c3c9f0b4676b9a2584d
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-04 12:11:12 +0200

    Calendar prop

--- packages/cx/src/widgets/form/Calendar.d.ts
+++ packages/cx/src/widgets/form/Calendar.d.ts
@@ -1,3 +1,22 @@
 import * as Cx from '../../core';
+import { FieldProps } from './Field';
 
-export class Calendar extends Cx.Widget<any> {}
+interface CalendarProps extends FieldProps {
+   value?:
+   refDate?:
+   disabled?:
+   minValue?:
+   minExclusive?:
+   maxValue?:
+   maxExclusive?:
+
+   baseClass?: string,
+   highlihtToday?: boolean,
+   maxValueErrorText?: string,
+   maxExclusiveErrorText?: string,
+   minValueErrorText?: string, 
+   minExclusiveErrorText?: string
+
+}
+
+export class Calendar extends Cx.Widget<CalendarProps> {}
