d709dff48cee8d07b210d28b2a366c3842ae9ede
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-04 16:11:05 +0200

    Tooltip and Window prop

--- packages/cx/src/widgets/overlay/Tooltip.d.ts
+++ packages/cx/src/widgets/overlay/Tooltip.d.ts
@@ -1,3 +1,41 @@
 import * as Cx from '../../core';
+import { DropdownProps } from './Dropdown';
 
-export class Tooltip extends Cx.Widget<any> {}
+interface TooltipProps extends DropdownProps {
+
+   /** Text to be displayed inside the tooltip. */
+   text?: Cx.StringProp,
+
+   /** Text to be displayed in the header. */
+   title?: Cx.StringProp,
+
+   /** 
+    * Set to true to make the tooltip always visible. 
+    * This is useful e.g. in product tours, when instructions need to be shown, even if the mouse pointer is not around. 
+    */
+   alwaysVisible?: Cx.BooleanProp,
+   
+   /** 
+    * Set to `true` to append the set `animate` state after the initial render.
+    *  Appended CSS class may be used to add show/hide animations.
+    */
+   animate?: boolean,
+   
+   /** 
+    * Number of milliseconds to wait, before removing the element from the DOM.
+    * Used in combination with the `animate` property. 
+    */
+   destroyDelay?: number,
+   
+   /** Set to `true` to make the tooltip follow the mouse movement. */
+   trackMouse?: string, 
+
+   /** 
+    * This property controls how tooltips behave on touch events. 
+    * Default value is `toggle` which means that the tooltip is shown on first tap and closed on the second tap. 
+    * Use `ignore` to skip showing tooltips on touch events. 
+    */
+   touchBehavior?: string,
+}
+
+export class Tooltip extends Cx.Widget<TooltipProps> {}
--- packages/cx/src/widgets/overlay/Window.d.ts
+++ packages/cx/src/widgets/overlay/Window.d.ts
@@ -1,3 +1,22 @@
 import * as Cx from '../../core';
+import { OverlayProps } from './Overlay';
 
-export class Window extends Cx.Widget<any> {}
+interface WindowProps extends OverlayProps {
+
+   /** Text to be displayed in the header. */
+   title?: Cx.StringProp,
+
+   /** Controls the close button visibility. Defaults to `true`. */
+   closeable?: Cx.BooleanProp,
+
+   /** A custom style which will be applied to the body. */
+   bodyStyle?: Cx.StyleProp,
+
+   /** A custom style which will be applied to the header. */
+   headerStyle?: Cx.StyleProp,
+
+   /** A custom style which will be applied to the footer. */
+   footerStyle?: Cx.StyleProp
+}
+
+export class Window extends Cx.Widget<WindowProps> {}
