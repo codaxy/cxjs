af39c1ad5ae2716851c16d8574c15838a4627e3a
Sasa Tatar <sasa.tatar@gmail.com>
2017-04-05 10:33:53 +0200

    MenuItem interface

--- packages/cx/src/widgets/nav/MenuItem.d.ts
+++ packages/cx/src/widgets/nav/MenuItem.d.ts
@@ -1,3 +1,17 @@
 import * as Cx from '../../core';
 
-export class MenuItem extends Cx.Widget<any> {}
+interface MenuItemProps extends Cx.HtmlElementProps {
+   
+   baseClass?: string,
+   hoverFocusTimeout?: number,
+   clickToOpen?: boolean,
+   horizontal?: boolean,
+   arrow?: boolean,
+   dropdownOptions?: Cx.Config | null,
+   showCursor?: boolean,
+   pad?: boolean,
+   placement?: string | null
+   
+}
+
+export class MenuItem extends Cx.Widget<MenuItemProps> {}
