fff93b2657901b340500bcac0a1f5d4bfa420621
Sasa Tatar <sasa.tatar@gmail.com>
2017-04-10 11:01:15 +0200

    ...

--- packages/cx/src/widgets/drag-drop/ops.d.ts
+++ packages/cx/src/widgets/drag-drop/ops.d.ts
@@ -1,17 +1,17 @@
 import * as Cx from '../../core';
 import * as React from 'react';
 import { DragSourceProps } from './DragSource';
-
+import { DropZone } from './DropZone';
+import { CursorPosition } from '../overlay/captureMouse';
 
 export interface DragEvent {
    eventType: string,
    event: React.SyntheticEvent<any>,
-   // TODO: define type/interface in captureMouse ts file
-   cursor: { clientX: number, clientY: number },
+   cursor: CursorPosition,
    source: DragSourceProps
 }
 
+export function initiateDragDrop(e: DragEvent, options?: Cx.Config, onDragEnd?: (e: DragEvent) => void) : void;
 
-export function initiateDragDrop(e: any, options: any, onDragEnd: any) : void;
-
+export function registerDropZone(dropZone: DropZone) : () => void;
 
