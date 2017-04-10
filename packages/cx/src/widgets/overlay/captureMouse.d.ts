00525554ce86a1ad231b7304a5b1d8e90e5b1276
Sasha Tatar <sasa.tatar@gmail.com>
2017-04-11 01:55:03 +0200

    captureMouse

--- packages/cx/src/widgets/overlay/captureMouse.d.ts
+++ packages/cx/src/widgets/overlay/captureMouse.d.ts
@@ -1,16 +1,27 @@
 import * as Cx from '../../core';
 import * as React from 'react';
 
-export interface CursorPosition {
-   clientX: number;
-   clientY: number;
-}
-
 type MouseEventHandler = (e: React.SyntheticEvent<any>, captureData: Cx.Config) => void; 
 
+/**
+ * Returns the object representing cursor position
+ * @param {React.SyntheticEvent<any>} e - Event
+ * @returns {{ clientX: number, clientY: number }}
+ */
+export function getCursorPos(e: React.SyntheticEvent<any>) : { clientX: number, clientY: number };
 
-export function getCursorPos(e: React.SyntheticEvent<any>) : CursorPos;
+export function captureMouse(
+        e: React.SyntheticEvent<any>, 
+        onMouseMove?: MouseEventHandler, 
+        onMouseUp?: (e: React.SyntheticEvent<any>) => void, 
+        captureData?: Cx.Config, 
+        cursor?: string
+    ) : void;
 
-export function captureMouse(e: React.SyntheticEvent<any>, onMouseMove?: MouseEventHandler, onMouseUp?: (e: React.SyntheticEvent<any>) => void, captureData?: Cx.Config, cursor?: string) : void;
-
-export function captureMouseOrTouch(e: React.SyntheticEvent<any>, onMouseMove?: MouseEventHandler, onMouseUp?: (e: React.SyntheticEvent<any>) => void, captureData?: Cx.Config, cursor: string) : void;
+export function captureMouseOrTouch(
+        e: React.SyntheticEvent<any>, 
+        onMouseMove?: MouseEventHandler, 
+        onMouseUp?: (e: React.SyntheticEvent<any>) => void, 
+        captureData?: Cx.Config, 
+        cursor: string
+    ) : void;
