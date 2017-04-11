b8ca5e06a096779f80bf16ac0764e48e50b69a90
Sasa Tatar <sasa.tatar@gmail.com>
2017-04-11 09:50:41 +0200

    ...

--- packages/cx/src/widgets/CxCredit.d.ts
+++ packages/cx/src/widgets/CxCredit.d.ts
@@ -2,7 +2,7 @@ import * as Cx from '../core';
 
 interface CxCreditProps extends Cx.HtmlElementProps {
    
-   /** TODO: Check type */
+   // TODO: Check type
    tooltip?: any,
    baseClass?: string,
    tag?: string
