0b8154fb4e5f0636e36ce2c65965030bb6c9b00c
Sasa Tatar <sasa.tatar@gmail.com>
2017-04-12 15:10:12 +0200

    TextProps

--- packages/cx/src/ui/Text.d.ts
+++ packages/cx/src/ui/Text.d.ts
@@ -3,8 +3,8 @@ import * as Cx from '../core';
 interface TextProps extends Cx.WidgetProps {
     value?: Cx.StringProp,
     bind?: string,
-    tpl: string,
-    expr: string
+    tpl?: string,
+    expr?: string
 }
 
 export class Text extends Cx.Widget<TextProps> {}
