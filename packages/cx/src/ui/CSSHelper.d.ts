266859421a04653ed9b74a43468fd23e0ae3511f
Sasa Tatar <sasa.tatar@gmail.com>
2017-04-12 14:07:30 +0200

    CSSHelper

--- packages/cx/src/ui/CSSHelper.d.ts
+++ packages/cx/src/ui/CSSHelper.d.ts
@@ -2,10 +2,10 @@ import * as Cx from '../core';
 
 export class CSSHelper {
 
-   static get(code: string) : {};
+   static get(code: string) : any;
 
-   static register(code?: string, helper?: {}): void;
+   static register(code?: string, helper?: any): void;
 
-   static alias(code?: string, helper?: {}): void;
+   static alias(code?: string, helper?: any): void;
    
 } 
