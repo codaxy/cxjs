bd5d96e14f993e909463831295995eb1e6a66201
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-12 13:18:25 +0200

    CSS fix

--- packages/cx/src/ui/CSS.d.ts
+++ packages/cx/src/ui/CSS.d.ts
@@ -2,14 +2,6 @@ import * as Cx from '../core';
 
 export class CSS {
 
-   push(list?: string[], item?: string): string[];
-
-   pushMore(list?: string[], itemArray?: string): string[];
-
-   pushMap(list?: string[], itemArray?: string, mapF?: any): string[];
-
-   join(list?: string[]): string;
-
    static resolve(...args): string[];
 
    static block(baseClass: string, styleModifiers: any, stateModifiers: any): string;
@@ -24,4 +16,5 @@ export class CSS {
    static parseStyle(str: any): any;
 
    static classPrefix: string;
+   
 }
