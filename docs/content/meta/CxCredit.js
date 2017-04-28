edaf070ca566320e2ba5b6e43e9a5a0040aceaec
Ranka Arsenovic <rannkaa@gmail.com>
2017-04-28 10:13:03 +0200

    Add import path for CxCredit widget

--- docs/content/meta/CxCredit.js
+++ docs/content/meta/CxCredit.js
@@ -3,12 +3,13 @@ import {Md} from '../../components/Md';
 import {CodeSplit} from '../../components/CodeSplit';
 import {CodeSnippet} from '../../components/CodeSnippet';
 import {ConfigTable} from '../../components/ConfigTable';
-
-
+import {ImportPath} from '../../components/ImportPath';
 
 export const CxCreditPage = <cx>
     <Md>
         # CxCredit
+
+        <ImportPath path="import { CxCredit } from 'cx/widgets';" />
 
         The `CxCredit` widget is used to provide a credit backlink to the Cx product page.
         It is required to use this widget, if you're using a free version of the product.
