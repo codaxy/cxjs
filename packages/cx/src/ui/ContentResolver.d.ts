3e1134d0ec88142d3debe3615f93e3b979a4e07f
Marko Stijak <mstijak@gmail.com>
2017-05-08 14:13:09 +0200

    ...

--- packages/cx/src/ui/ContentResolver.d.ts
+++ packages/cx/src/ui/ContentResolver.d.ts
@@ -3,8 +3,9 @@ import * as Cx from '../core';
 interface ContentResolverProps extends Cx.PureContainerProps {
 
    params?: Cx.StructuredProp,
-   mode?: 'replace' | 'prepend' | 'append'
+   mode?: 'replace' | 'prepend' | 'append',
 
+   onResolve?: (params, Instance) => any
 }
 
 export class ContentResolver extends Cx.Widget<ContentResolverProps> {}
