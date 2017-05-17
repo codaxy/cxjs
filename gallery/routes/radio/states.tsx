e188d0da7de4c558123fec669eb3d38e9b78bbe1
Sasa Tatar <sasa.tatar@gmail.com>
2017-05-17 09:38:49 +0200

    Radio

--- gallery/routes/radio/states.tsx
+++ gallery/routes/radio/states.tsx
@@ -9,22 +9,44 @@ const PageController = class extends Controller {
 
 export default <cx>
     <FlexRow wrap spacing="large" target="tablet">
+        <Section 
+            mod="card"
+            title="Material Labels"
+            visible={{expr: "{$root.$route.theme} == 'material'"}}
+            hLevel={4}
+        >
+            <div style={{ marginTop: "-2.5rem" }}>
+                <Radio label="Standard" value={bind("radio")} option="0" text="Radio" labelPlacement="material" />
+                <br/>
+                <Radio label="Disabled" value={bind("radio")} option="0" disabled text="Radio" labelPlacement="material" />
+                <br/>
+                <Radio label="Readonly" value={bind("radio")} option="0" readOnly text="Radio" labelPlacement="material" />
+                <br/>
+                <Radio label="Styled" value={bind("radio")} option="0" inputStyle="color:red" text="Radio" labelPlacement="material" />
+                <br/>
+                <Radio label="Label only" value={bind("radio")} option="0" labelPlacement="material" />
+                <br/>
+                <Radio label="View Mode" value={bind("radio")} option="0" mode="view" text="Checked" emptyText="N/A" labelPlacement="material" />
+            </div>
+        </Section>
         <Section mod="well" title="Standard" hLevel={4} layout={LabelsLeftLayout} controller={PageController}>
-            <Radio label="Standard" value={bind("radio")} option="0" text="Radio"/>
-            <Radio label="Disabled" value={bind("radio")} option="0" disabled text="Radio"/>
-            <Radio label="Readonly" value={bind("radio")} option="0" readOnly text="Radio"/>
-            <Radio label="Styled" value={bind("radio")} option="0" inputStyle="color:red" text="Radio"/>
-            <Radio label="View Mode" value={bind("radio")} option="0" mode="view" text="Checked" emptyText="N/A"/>
+            <Radio label="Standard" value={bind("radio")} option="1" text="Radio"/>
+            <Radio label="Disabled" value={bind("radio")} option="1" disabled text="Radio"/>
+            <Radio label="Readonly" value={bind("radio")} option="1" readOnly text="Radio"/>
+            <Radio label="Styled" value={bind("radio")} option="1" inputStyle="color:red" text="Radio"/>
+            <Radio label="Label only" value={bind("radio")} option="1" />
+            <Radio label="View Mode" value={bind("radio")} option="1" mode="view" text="Checked" emptyText="N/A" />
         </Section>
         <Section mod="well" title="Native" hLevel={4} layout={LabelsLeftLayout}>
-            <Radio label="Standard" value={bind("radio")} option="1" text="Radio" native/>
-            <Radio label="Disabled" value={bind("radio")} option="1" disabled text="Radio" native/>
-            <Radio label="Readonly" value={bind("radio")} option="1" readOnly text="Radio" native/>
-            <Radio label="View Mode" value={bind("radio")} option="1" mode="view" text="Radio" emptyText="N/A" native/>
+            <Radio label="Standard" value={bind("radio")} option="2" text="Radio" native/>
+            <Radio label="Disabled" value={bind("radio")} option="2" disabled text="Radio" native/>
+            <Radio label="Readonly" value={bind("radio")} option="2" readOnly text="Radio" native/>
+            <Radio label="Label only" value={bind("radio")} option="0" native />
+            <Radio label="View Mode" value={bind("radio")} option="2" mode="view" text="Checked" emptyText="N/A" native/>
         </Section>
         <Section mod="well" title="Label On Top" hLevel={4} layout={{ type: LabelsTopLayout, vertical: true }}>
-            <Radio label="Label" value={bind("radio")} option="2" text="Text"  />
-            <Radio label="Label Only" value={bind("radio")} option="2" />
+            <Radio label="Label" value={bind("radio")} option="3" text="Text"  />
+            <Radio label="Label Only" value={bind("radio")} option="3" />
         </Section>
     </FlexRow>
 </cx>
