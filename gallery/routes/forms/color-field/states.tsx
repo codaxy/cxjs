47c678468fb3d9f6bdc292d590b802c3a3089af3
Sasa Tatar <sasa.tatar@gmail.com>
2017-05-23 15:23:31 +0200

    ColorField

--- gallery/routes/forms/color-field/states.tsx
+++ gallery/routes/forms/color-field/states.tsx
@@ -1,5 +1,5 @@
 
-import {cx, Section, FlexRow, ColorField} from 'cx/widgets';
+import {cx, Section, FlexRow, ColorField, HelpText} from 'cx/widgets';
 import {bind, LabelsLeftLayout, LabelsTopLayout} from 'cx/ui';
 
 export default <cx>
@@ -13,29 +13,49 @@ export default <cx>
             <br/>
             <ColorField value={bind("standard")} label="View mode" labelPlacement="material" mode="view" emptyText="N/A"/>
             <br/>
-            <ColorField value={bind("placeholder")} label="Placeholder" labelPlacement="material" placeholder="Enter a color..." />
+            <ColorField value={bind("placeholder")} label="Placeholder" labelPlacement="material" placeholder="Select a color..." />
         </Section>
         <Section mod="card" title="Horizontal Labels" hLevel={4} layout={LabelsLeftLayout} >
             <ColorField value={bind("standard")} label="Standard" />
-            <br/>
             <ColorField value={bind("standard")} label="Disabled" disabled />
-            <br/>
             <ColorField value={bind("standard")} label="Read only" readOnly />
-            <br/>
             <ColorField value={bind("standard")} label="View mode" mode="view" emptyText="N/A" />
-            <br/>
-            <ColorField value={bind("placeholder")} label="Placeholder" placeholder="Enter a color..." />
+            <ColorField value={bind("placeholder")} label="Placeholder" placeholder="Select a color..." />
         </Section>
         <Section mod="card" title="Labels on Top" hLevel={4} layout={{type: LabelsTopLayout, vertical: true}} >
             <ColorField value={bind("standard")} label="Standard" />
-            <br/>
             <ColorField value={bind("standard")} label="Disabled" disabled />
-            <br/>
             <ColorField value={bind("standard")} label="Read only" readOnly />
-            <br/>
             <ColorField value={bind("standard")} label="View mode" mode="view" emptyText="N/A" />
-            <br/>
-            <ColorField value={bind("placeholder")} label="Placeholder" placeholder="Enter a color..." />
+            <ColorField value={bind("placeholder")} label="Placeholder" placeholder="Select a color..." />
+        </Section>
+        <Section mod="card" title="Helpers" hLevel={4} layout={LabelsLeftLayout} >
+            <ColorField value={bind("placeholder")} label="Placeholder" placeholder="Select a color..." />
+            <ColorField value={{ bind: "clear", defaultValue: "rgba(34,194,236,1)"}} label="Clear" />
+            <ColorField value={bind("tooltip")} label="Tooltip" tooltip="This is a tooltip." />
+            <ColorField value={bind("inline")} label="Help" help="Inline" />
+            <ColorField 
+                value={bind("block")} 
+                label="Help" 
+                help={<cx><HelpText mod="block">Block</HelpText></cx>} 
+            />
+        </Section>
+        <Section mod="card" title="Validation" hLevel={4} layout={LabelsLeftLayout} >
+            <ColorField value={bind("required")} label="Required" required />
+            <ColorField value={bind("visited")} label="Visited" required visited />
+            <ColorField value={bind("asterisk")} label="Asterisk" required asterisk />
+        </Section>
+        <Section mod="card" title="Validation Modes" hLevel={4} layout={LabelsLeftLayout} >
+            <ColorField value={bind("validation.tooltip")} label="Tooltip" required />
+            <ColorField value={bind("validation.help")} label="Help" required validationMode="help" />
+            <ColorField value={bind("validation.block")} label="Help Block" required validationMode="help-block" />
+            <ColorField value={bind("validation.block")} 
+                label="Material" 
+                required 
+                validationMode="help"
+                helpPlacement="material"
+                visible={{expr: "{$root.$route.theme} == 'material'"}}
+            />
         </Section>
     </FlexRow>
 </cx>
