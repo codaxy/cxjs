4ea319b63380042851f3b51029dccc4b34a478bc
Sasa Tatar <sasa.tatar@gmail.com>
2017-05-23 08:13:24 +0200

    ...

--- gallery/routes/forms/list/states.tsx
+++ gallery/routes/forms/list/states.tsx
@@ -1,23 +1,15 @@
 
 import {cx, Section, FlexRow, List, Text} from 'cx/widgets';
-import {bind, tpl, Controller, PropertySelection, KeySelection} from 'cx/ui';
+import {bind, Controller, PropertySelection} from 'cx/ui';
 import casual from '../../../util/casual';
 
 class PageController extends Controller {
   init() {
+    super.init();
+
     this.store.init(
-      "$page.records",
-      Array.from({ length: 20 }).map((v, i) => {
-        var name = casual.full_name;
-        return {
-          id: i + 1,
-          fullName: name,
-          phone: casual.phone,
-          city: casual.city,
-          email: name.toLowerCase().replace(" ", ".") + "@example.com",
-          country: casual.country
-        };
-      })
+      "records",
+      Array.from({ length: 5 }).map((x, i) => ({ text: `${i + 1}` }))
     );
   }
 }
@@ -26,24 +18,16 @@ export default <cx>
     <FlexRow wrap spacing="large" target="desktop">
         <Section mod="well" controller={PageController}>
             <List
-              records={bind("$page.records")}
-              selection={{ type: KeySelection, bind: "$page.selection" }}
-              grouping={{
-                  key: { firstLetter: { expr: "{$record.fullName}[0]" } },
-                  aggregates: { count: { type: "count", value: 1 } },
-                  header: (
-                    <div style={{ paddingTop: '25px'}}>
-                      <strong text={bind("$group.firstLetter")} />
-                    </div>
-                  ),
-                  footer: <strong text={tpl("{$group.count} item(s)")} />
-                }}
-            >
-              <strong text={bind("$record.fullName")}></strong>
-              <br />
-               Phone: <span text={bind("$record.phone")} />
-              <br />
-               City: <span text={bind("$record.city")} />
+                records={bind("records")}
+                selection={PropertySelection}
+                style={{ width: '200px' }}
+                emptyText="Nothing found."
+                mod="bordered"
+            >   
+                <div>
+                  <strong>Header <Text expr="{$index}+1" /></strong>
+                </div>
+                Description
             </List>
         </Section>
     </FlexRow>
