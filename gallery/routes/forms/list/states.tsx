e69fdaeb05cb06023e22666c10c5f9324c9028c3
Sasa Tatar <sasa.tatar@gmail.com>
2017-05-25 12:06:47 +0200

    ...

--- gallery/routes/forms/list/states.tsx
+++ gallery/routes/forms/list/states.tsx
@@ -1,47 +1,62 @@
 
 import {cx, Section, FlexRow, List, Text} from 'cx/widgets';
-import {bind, Controller, PropertySelection} from 'cx/ui';
+import {bind, tpl, Controller, KeySelection} from 'cx/ui';
+import casual from '../../../util/casual';
 
 class PageController extends Controller {
-  init() {
-    super.init();
-
-    this.store.init(
-      "records",
-      Array.from({ length: 5 }).map((x, i) => ({ text: `${i + 1}` }))
-    );
-  }
+    init() {
+        this.store.init('records', Array.from({length: 15}).map((v, i) => {
+            var name = casual.full_name;
+            return {
+                id: i + 1,
+                fullName: name
+            }
+        }));
+    }
 }
 
-const listMod = mod => <cx>
+const generateList = ({mod, grouping=false, multiple=false}) => <cx>
     <List
         records={bind('records')}
-        selection={PropertySelection} 
         style={{ width: '200px' }}
         emptyText="Nothing found."
-        mod={mod}
+        selection={{
+           type: KeySelection,
+           bind: mod,
+           keyField: 'id',
+           multiple: multiple
+        }}
+        grouping={grouping && {
+            key: {
+                firstLetter: {expr: '{$record.id} % 2 == 0'}
+            },
+            aggregates: {
+                count: {
+                    type: 'count',
+                    value: 1
+                }
+            },
+            header: <div style={{ paddingTop: '25px' }} >
+                <strong text={bind("$group.firstLetter")} />
+            </div>,
+            footer: <strong text={tpl("{$group.count} item(s)")} />
+        }}
     >   
-        <div>
-          <strong>Header <Text expr="{$index}+1" /></strong>
-        </div>
-        Description
+        <span text={bind("$record.fullName")}></span>
     </List>
 </cx>
 
 
 export default <cx>
     <FlexRow wrap spacing="large" target="desktop" controller={PageController}>
-        <Section mod="well" >
-            <FlexRow wrap spacing="large" >
-            <div>
-                <h6>Bordered</h6>
-                {listMod("bordered")}
-            </div>
-            <div>
-                <h6>Big</h6>
-                {listMod("big")}
-            </div>
-            </FlexRow>
+        <Section mod="well" title="Standard" hLevel={4} >
+            {generateList({mod: "standard"})}
+        </Section>
+        <Section mod="well" title="Grouped" hLevel={4} >
+            {generateList({mod: "grouped", grouping: true})}
+        </Section>
+        <Section mod="well" title="Multiple selection" hLevel={4} >
+            {generateList({mod: "multiple", multiple: true})}
         </Section>
     </FlexRow>
 </cx>
