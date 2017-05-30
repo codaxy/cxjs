b7f7defb68b57b84982e766c0f460bc68f896c0d
Marko Stijak <mstijak@gmail.com>
2017-05-30 16:10:20 +0200

    Fix onDestroy handling for deep trees

--- docs/content/concepts/Controllers.js
+++ docs/content/concepts/Controllers.js
@@ -1,13 +1,39 @@
-import { Button, Content, HtmlElement, Checkbox, TextField, NumberField, Select, Option, Repeater, Text, MsgBox } from 'cx/widgets';
-import { LabelsLeftLayout, Controller } from 'cx/ui';
+import {
+    Button,
+    Content,
+    Tab,
+    HtmlElement,
+    Checkbox,
+    TextField,
+    NumberField,
+    Select,
+    Option,
+    Repeater,
+    Text,
+    MsgBox
+} from 'cx/widgets';
+import {LabelsLeftLayout, Controller} from 'cx/ui';
 import {Md} from '../../components/Md';
 import {CodeSplit} from '../../components/CodeSplit';
 import {CodeSnippet} from '../../components/CodeSnippet';
 import {ImportPath} from '../../components/ImportPath';
 
 
+class TabController extends Controller {
+    onInit() {
+        this.store.set('$page.tabOpenTime', 0);
+        this.timer = setInterval(() => {
+            this.store.update('$page.tabOpenTime', t => t + 1);
+        }, 1000);
+    }
+
+    onDestroy() {
+        clearInterval(this.timer);
+    }
+}
+
 class CbController extends Controller {
-    init() {
+    onInit() {
         this.addTrigger('t1', ['$page.cb1'], cb1 => {
             this.store.set('$page.cb2', !cb1);
         });
@@ -18,7 +44,7 @@ class CbController extends Controller {
 }
 
 class InfoController extends Controller {
-    init() {
+    onInit() {
         this.store.set('$page.cities', [{id: 'ams', text: 'Amsterdam', population: '1.6M'}, {
             id: 'bg',
             text: 'Belgrade',
@@ -26,7 +52,7 @@ class InfoController extends Controller {
         }]);
 
         this.addComputable('$page.city', ['$page.cities', '$page.cityId'], (options, id) => {
-            return options.find(o=>o.id == id);
+            return options.find(o => o.id == id);
         });
     }
 }
@@ -40,21 +66,70 @@ class MethodController extends Controller {
 export const Controllers = <cx>
 
     <Md>
-        # Controllers
-        
-        <ImportPath path="import {Controller} from 'cx/ui';" />        
-
         <CodeSplit>
 
+            # Controllers
+
+            <ImportPath path="import {Controller} from 'cx/ui';"/>
+
             Controllers are used to concentrate business logic required for views. This includes preparing data for
-            rendering purposes and also executing callbacks based on user input.
-
+            rendering purposes, calculating values, reacting on changes, defining callbacks, etc.
             Controllers are assigned to widgets using the `controller` attribute. Once assigned, controllers are passed
-            down the widget tree.
+            down the widget tree to all descendants.
 
+            ### `onInit`, `onDestroy`
+
+            `onInit` method is invoked once the controller is created. Similarly, `onDestroy` is called when controller
+            just before the controller is destroyed.
+
+            <div class="widgets">
+                <div>
+                    <div>
+                        <Tab value:bind="$page.tab" tab="1" default>Tab 1</Tab>
+                        <Tab value:bind="$page.tab" tab="2">Tab 2</Tab>
+                    </div>
+                    <div visible:bind="$page.tab=='1'" controller={TabController}>
+                        Tab 1 is open <span text:bind="$page.tabOpenTime"/> seconds.
+                    </div>
+                    <div visible:bind="$page.tab=='2'" controller={TabController}>
+                        Tab 2 is open <span text:bind="$page.tabOpenTime"/> seconds.
+                    </div>
+                </div>
+            </div>
+
+            <CodeSnippet putInto="code">{`
+                class TabController extends Controller {
+                    onInit() {
+                        this.store.set('$page.tabOpenTime', 0);
+                        this.timer = setInterval(() => {
+                            this.store.update('$page.tabOpenTime', t => t + 1);
+                        }, 1000);
+                    }
+                    onDestroy() {
+                        clearInterval(this.timer);
+                    }
+                }
+                ...
+                <div>
+                    <Tab value:bind="$page.tab" tab="1" default>Tab 1</Tab>
+                    <Tab value:bind="$page.tab" tab="2">Tab 2</Tab>
+                </div>
+                <div visible:bind="$page.tab=='1'" controller={TabController}>
+                    Tab 1 is open <span text:bind="$page.tabOpenTime"/> seconds.
+                </div>
+                <div visible:bind="$page.tab=='2'" controller={TabController}>
+                    Tab 2 is open <span text:bind="$page.tabOpenTime"/> seconds.
+                </div>
+
+            `}</CodeSnippet>
+
+            `onInit` method is a good place to initialize or fetch data and define triggers and computables.
+        </CodeSplit>
+
+        <CodeSplit>
             ### Triggers
 
-            Triggers execute given callback when specified data change.
+            Triggers execute a given callback when specified data change.
 
             <div class="widgets">
                 <div controller={CbController}>
@@ -69,8 +144,8 @@ export const Controllers = <cx>
             `removeTrigger` method.
             The second argument is a list of bindings to be monitored.
             The third argument is a function which will be executed when any of the bindings change.
-            The fourth argument should be a boolean value which controls if the trigger should be immediately executed or
-            not.
+            The fourth argument should be a boolean value which controls if the trigger should be immediately executed
+            or not.
 
             Triggers are used to:
 
@@ -80,7 +155,7 @@ export const Controllers = <cx>
             <Content name="code">
                 <CodeSnippet fiddle="QmlMqPUa">{`
                class CbController extends Controller {
-                  init() {
+                  onInit() {
                      this.addTrigger('t1', ['$page.cb1'], cb1 => {
                         this.store.set('$page.cb2', !cb1);
                      });
@@ -122,7 +197,7 @@ export const Controllers = <cx>
 
             <CodeSnippet putInto="code" fiddle="ooYt3HD9">{`
             class InfoController extends Controller {
-               init() {
+               onInit() {
                   this.store.set('$page.cities', [{id: 'ams', text: 'Amsterdam', population: '1.6M'}, {id: 'bg',text: 'Belgrade',population: '3M'}]);
 
                   this.addComputable('$page.city', ['$page.cities', '$page.cityId'], (options, id) => {
@@ -157,16 +232,16 @@ export const Controllers = <cx>
 
         </CodeSplit>
 
-        ## Controller Methods
+        ### Callback Methods
 
         <CodeSplit>
 
-            It's allowed to define custom methods within a controller, which can be invoked when necessary.
-            This keeps the view code tidy.
+            It's allowed to define methods within a controller which can be invoked when necessary from the view.
+            This helps keeping the view code tidy.
 
             <div class="widgets">
                 <div controller={MethodController}>
-                    <Button onClick={(e, {controller})=> {
+                    <Button onClick={(e, {controller}) => {
                         controller.sayHello();
                     }}>Say Hello
                     </Button>
