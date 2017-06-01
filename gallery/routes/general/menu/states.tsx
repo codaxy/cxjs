7d7d24716e5244760b11917a58f4abc147f16547
Marko Stijak <mstijak@gmail.com>
2017-06-01 12:59:56 +0200

    Vertical menu example in Gallery

--- gallery/routes/general/menu/states.tsx
+++ gallery/routes/general/menu/states.tsx
@@ -1,8 +1,61 @@
-import {cx, Section, FlexRow, Menu, Submenu, TextField, Checkbox, DateField, MenuItem} from 'cx/widgets';
+import {cx, Section, FlexRow, Menu, Submenu, TextField, Checkbox, DateField, MenuItem, Calendar} from 'cx/widgets';
 import {bind, expr} from 'cx/ui';
 
 export default <cx>
-    <FlexRow wrap spacing="large" target="desktop">
+    <FlexRow wrap spacing="large" target="tablet" align="start">
+
+        <Section mod="well" title="Vertical" hLevel={4}>
+            <Menu>
+                <Submenu>
+                    File
+                    <Menu putInto="dropdown">
+                        <a href="#">Link</a>
+                        <hr/>
+                        <TextField value={bind("text" )} mod="menu"/>
+                        <Checkbox value={bind("checked")} mod="menu">Checkbox</Checkbox>
+                        <Submenu arrow>
+                            Submenu 1
+                            <Menu putInto="dropdown">
+                                <a href="#">Sub-Item 1</a>
+                                <a href="#">Sub-Item 2</a>
+                            </Menu>
+                        </Submenu>
+                        <Submenu arrow>
+                            Submenu 2
+                            <Menu putInto="dropdown">
+                                <a href="#">Item 1</a>
+                                <a href="#">Item 2</a>
+                            </Menu>
+                        </Submenu>
+                        <DateField value={bind("date")} mod="menu"  />
+                        <MenuItem class="test" style="color:red;" autoClose>
+                            <a href="#">Item Level CSS</a>
+                        </MenuItem>
+                    </Menu>
+                </Submenu>
+                <Submenu>
+                    Edit
+                    <Menu putInto="dropdown">
+                        <a href="#">Item 1</a>
+                        <a href="#">Item 2</a>
+                        <a href="#">Item 3</a>
+                        <a href="#">Item 4</a>
+                        <a href="#">Item 5</a>
+                    </Menu>
+                </Submenu>
+                <Submenu>
+                    View
+                    <Menu putInto="dropdown">
+                        <a href="#">Item 1</a>
+                        <a href="#">Item 2</a>
+                        <a href="#">Item 3</a>
+                        <a href="#">Item 4</a>
+                        <a href="#">Item 5</a>
+                    </Menu>
+                </Submenu>
+            </Menu>
+        </Section>
+
         <Section mod="well" title="Horizontal" hLevel={4}>
             <Menu horizontal>
             <Submenu>
@@ -73,6 +126,12 @@ export default <cx>
                             </Menu>
                         </Submenu>
                     </Menu>
+                </Submenu>
+                <Submenu arrow>
+                    Calendar
+                    <Calendar putInto="dropdown" value={bind("date")} />
+
+
                 </Submenu>
             </Menu>
         </Section>
