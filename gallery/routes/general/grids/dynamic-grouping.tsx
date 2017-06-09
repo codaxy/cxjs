bc755b8cf140bba9cf31d414c55c8d31a1fdb5e1
Ognjen <ognjen.kremenovic@codaxy.com>
2017-06-09 14:59:49 +0200

    grouping grids added; grid dynamic grouping modified;

---
+++ gallery/routes/general/grids/dynamic-grouping.tsx
@@ -0,0 +1,127 @@
+import { Grid, HtmlElement, LookupField, Section, Select, cx } from "cx/widgets";
+import { Controller, bind } from "cx/ui";
+import { Format } from "cx/util";
+import casual from '../../../util/casual';
+
+function plural(word, count) {
+  if (count > 1) return word + "s";
+  return word;
+}
+
+class PageController extends Controller {
+  init() {
+    super.init();
+
+    //init grid data
+    this.store.set(
+      "$page.records",
+      Array
+        .from({ length: 100 })
+        .map((v, i) => ({
+          id: i + 1,
+          fullName: casual.full_name,
+          continent: casual.continent,
+          browser: casual.browser,
+          os: casual.operating_system,
+          visits: casual.integer(1, 100)
+        }))
+    );
+
+    //init grouping options
+    this.store.set("$page.groupableFields", [
+      { id: "continent", text: "Continent" },
+      { id: "browser", text: "Browser" },
+      { id: "os", text: "Operating System" }
+    ]);
+
+    this.store.set("$page.groups", [{ id: "browser", text: "Browser" }]);
+
+    //when changed, apply grouping to the grid
+    this.addTrigger("grouping", ["$page.groups"], g => {
+      var groupings = [{ key: {}, showFooter: true }];
+      groupings.push(...(g || []).map(x => x.id));
+      var grid = this.widget.findFirst(Grid);
+      grid.groupBy(groupings, { autoConfigure: true });
+    }, true);
+  }
+}
+
+Format.registerFactory("plural", (format, text) => {
+  return value => plural(text, value);
+});
+
+
+export default (
+  <cx>
+    <a href="https://github.com/codaxy/cx/tree/master/gallery/routes/general/grids/plain.tsx" target="_blank" putInto="github">GitHub</a>
+    <Section
+      mod="well"
+      style="height: 100%"
+      bodyStyle="display:flex; flex-direction:column"
+      controller={PageController}
+    >
+      <p>
+        Group by: <LookupField
+          records={bind("$page.groups")}
+          options={bind("$page.groupableFields")}
+          multiple={true}
+        />
+      </p>
+      <Grid
+        records={bind("$page.records")}
+        style={{ width: "100%" }}        
+        columns={
+        [
+          {
+            header: "Name",
+            field: "fullName",
+            sortable: true,
+            aggregate: "count",
+            footer: {
+              expr: '({$group.$level} > 1 ? {$group.$name:s:TOTAL} + " - " : "") + {$group.fullName} + " " + {$group.fullName:plural;item}'
+            }
+          },
+          {
+            header: "Continent",
+            field: "continent",
+            sortable: true,
+            aggregate: "distinct",
+            aggregateField: "continents",
+            footer: {
+              tpl: "{$group.continents} {$group.continents:plural;continent}"
+            }
+          },
+          {
+            header: "Browser",
+            field: "browser",
+            sortable: true,
+            aggregate: "distinct",
+            aggregateField: "browsers",
+            footer: {
+              tpl: "{$group.browsers} {$group.browsers:plural;browser}"
+            }
+          },
+          {
+            header: "OS",
+            field: "os",
+            sortable: true,
+            aggregate: "distinct",
+            aggregateField: "oss",
+            footer: { tpl: "{$group.oss} {$group.oss:plural;OS}" }
+          },
+          {
+            header: "Visits",
+            field: "visits",
+            sortable: true,
+            aggregate: "sum",
+            align: "right"
+          }
+        ]
+      }
+      />
+    </Section>
+  </cx>
+);
+
+import { hmr } from '../../hmr.js';
+hmr(module);
--- gallery/routes/general/grids/index.tsx
+++ gallery/routes/general/grids/index.tsx
@@ -1,19 +1,21 @@
 import {cx, PureContainer, RedirectRoute, Route} from 'cx/widgets';
 import {bind, expr, FirstVisibleChildLayout} from 'cx/ui';
 
-import Plain from "./plain";
+import Default from "./basic";
 
 declare let System: any;
 
-import {getHeader} from "../../../components/getHeader";
-import {asyncRoute} from "../../../components/asyncRoute";
+import { getHeader } from "../../../components/getHeader";
+import { asyncRoute } from "../../../components/asyncRoute";
 
 const header = getHeader({
     title: "Grid",
     tabs: {
-        plain: 'Plain',
+        "basic": "Basic",
         "multi-select": "Multiple Selection",
-        misc: "Misc"
+        "misc": "Misc",
+        "grouping": "Grouping",
+        "dynamic-grouping": "Dynamic Grouping"
     },
     docsUrl: 'https://cxjs.io/docs/widgets/grids'
 });
@@ -23,12 +25,14 @@ export default <cx>
     { header }
 
     <PureContainer layout={FirstVisibleChildLayout}>
-        <Route url={{bind: "$root.url"}} route="+/plain">
-            {Plain}
+        <Route url={{bind: "$root.url"}} route="+/basic">
+            {Default}
         </Route>
         { asyncRoute("+/multi-select", () => System.import("./multi-select")) }
+        { asyncRoute("+/grouping", () => System.import("./grouping")) }
+        { asyncRoute("+/dynamic-grouping", () => System.import("./dynamic-grouping")) }
         { asyncRoute("+/misc", () => System.import("./misc")) }
-        <RedirectRoute redirect="+/plain" />
+        <RedirectRoute redirect="+/basic" />
     </PureContainer>
 </cx>
 
