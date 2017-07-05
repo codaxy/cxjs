f7460e3ab8a7f3559d986753a2a72bd41b3fae9e
Marko Stijak <mstijak@gmail.com>
2017-07-05 08:39:43 +0200

    Minor fix

--- misc/tracking/gtm.js
+++ misc/tracking/gtm.js
@@ -9,7 +9,7 @@ if (id) {
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', ${id});</script>
+    })(window,document,'script','dataLayer', '${id}');</script>
     <!-- End Google Tag Manager -->
 `;
    body = `
