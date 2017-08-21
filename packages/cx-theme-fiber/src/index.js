0efd7ea0f901ce11ed57427b44b38dfd4c70fd48
Ognjen <ognjen.kremenovic@codaxy.com>
2017-08-21 11:56:41 +0200

    fa icons included;

--- packages/cx-theme-fiber/src/index.js
+++ packages/cx-theme-fiber/src/index.js
@@ -41,36 +41,23 @@ export function applyThemeOverrides() {
 
    // material icons added
    Icon.registerFactory((name, props) => {
-      return VDOM.createElement('i', {
-         ...props,
-         className: 'material-icons ' + (props.className || '')
-      }, name)
-   });
+      props = { ...props };
+      props.className = `fa fa-${name} ${props.className || ''}`;
+      return <i {...props} />
+  });
 
    Icon.unregister('close');
+   Icon.unregister('clear');
    Icon.unregister('folder');
+   Icon.unregister('forward');
 
-   Icon.register('calendar', props => Icon.render('date_range', props));
-   Icon.register('drop-down', props => Icon.render('keyboard_arrow_down', props));
-   Icon.register('sort-asc', props => Icon.render('arrow_upward', props));
-   Icon.register('folder-open', props => Icon.render('folder_open', props));
-   Icon.register('file', props => Icon.render('insert_drive_file', props));
-
-   Icon.register('forward', props => {
-      return <svg
-         {...props}
-         viewBox="0 0 24 24">
-         <path fill="currentColor"
-               strokeWidth="0"
-               stroke="currentColor"
-               d="M11.59 16.34l4.58-4.59-4.58-4.59L13 5.75l6 6-6 6z"/>
-
-         <path fill="currentColor"
-               strokeWidth="0"
-               stroke="currentColor"
-               d="M4.59 16.34l4.58-4.59-4.58-4.59L6 5.75l6 6-6 6z"/>
-      </svg>
-   });
+   Icon.register('calendar', props => Icon.render('calendar-o', props));
+   Icon.register('drop-down', props => Icon.render('chevron-down', props));
+   Icon.register('sort-asc', props => Icon.render('chevron-down', props));
+   Icon.register('folder-open', props => Icon.render('folder-open-o', props));
+   Icon.register('folder', props => Icon.render('folder-o', props));
+   Icon.register('file', props => Icon.render('file-o', props));
+   Icon.register('clear', props => Icon.render('times', props));
 }
 
 applyThemeOverrides();
