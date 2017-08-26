fe188f585a5301d5859a6742adc08c3b3142de69
Marko Stijak <mstijak@gmail.com>
2017-08-26 22:43:26 +0200

    ...

---
+++ litmus/features/tracking/LineTracker.js
@@ -0,0 +1,49 @@
+import {PureContainer} from 'cx/ui';
+
+export class LineTracker extends PureContainer {
+   declareData() {
+      return super.declareData(...arguments, {
+         x: undefined,
+         y: undefined
+      });
+   }
+
+   explore(context, instance) {
+      let lineTracker = context.lineTracker;
+      instance.parentLineTracker = lineTracker;
+
+      if (!instance.lineTracker) {
+         let onCollect = this.onCollect && instance.getCallback("onCollect");
+         let accumulator = {};
+         instance.resetAccumulator = () => {
+            accumulator = {};
+            if (this.onPrepareAccumulator)
+               instance.invoke('onPrepareAccumulator', accumulator, instance);
+         };
+
+         instance.lineTracker = (x, y, name) => {
+            onCollect(accumulator, x, y, name);
+            if (lineTracker)
+               lineTracker(accumulator, x, y, name);
+         };
+
+         instance.write = () => {
+            if (this.onWrite)
+               instance.invoke('onWrite', accumulator, instance);
+         }
+      }
+
+      instance.resetAccumulator();
+      context.lineTracker = instance.lineTracker;
+
+      super.explore(context, instance);
+      context.lineTracker = lineTracker;
+   }
+
+   prepare(context, instance) {
+      let lineTracker = context.lineTracker;
+      super.prepare(context, instance);
+      instance.write();
+      context.lineTracker = lineTracker;
+   }
+}
