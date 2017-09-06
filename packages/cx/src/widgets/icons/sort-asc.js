69d62fcb818b2ad182881e035cb7c5b74510fb75
Marko Stijak <mstijak@gmail.com>
2017-09-06 13:07:21 +0200

    Fix sort arrow direction

--- packages/cx/src/widgets/icons/sort-asc.js
+++ packages/cx/src/widgets/icons/sort-asc.js
@@ -6,7 +6,8 @@ export default registerIcon('sort-asc', props => {
       {...props}
       viewBox="0 0 16 16">
       <path fill="currentColor"
-         d="M4.5 10.2l3 3 3-3-.7-.7L8 11.3V3H7v8.3L5.2 9.5z"/>
+         d="M10.5 5.8l-3-3-3 3 .707.708L7 4.688v8.312h1V4.69l1.793 1.817z"
+      />
    </svg>
 }, true);
 
