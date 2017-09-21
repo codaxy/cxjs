0b2e8849cfbdf3df0f3d8ead9367bb2fd685d849
Marko Stijak <mstijak@gmail.com>
2017-09-21 12:12:22 +0200

    ...

--- docs/content/examples/grid/InfiniteScrolling.js
+++ docs/content/examples/grid/InfiniteScrolling.js
@@ -1,5 +1,5 @@
-import { HtmlElement, Checkbox, Grid } from 'cx/widgets';
-import { Controller, KeySelection } from 'cx/ui';
+import {HtmlElement, DetachedScope, Grid} from 'cx/widgets';
+import {Controller, KeySelection} from 'cx/ui';
 import {Md} from '../../../components/Md';
 import {CodeSplit} from '../../../components/CodeSplit';
 import {CodeSnippet} from '../../../components/CodeSnippet';
@@ -56,36 +56,41 @@ class PageController extends Controller {
 }
 
 export const InfiniteScrolling = <cx>
-    <Md controller={PageController}>
+    <Md>
 
         # Infinite Grid Scrolling
 
         <CodeSplit>
 
             The `Grid` widgets supports buffered rendering which dramatically improves performance with many rows.
-            Set grid to `buffered` mode and tweak `bufferSize` and `bufferStep` parameters for the best scrolling experience.
+            Set grid to `buffered` mode and tweak `bufferSize` and `bufferStep` parameters for the best scrolling
+            experience.
 
-            <Grid
-                records:bind="$page.records"
-                buffered
-                style="height: 800px"
-                lockColumnWidths
-                totalRecordCount:bind="$page.recordCount"
-                offset:bind="$page.offset"
-                loadingOffset:bind="$page.nextOffset"
-                bufferedLoading
-                cached
-                keyField="id"
-                columns={[
-                    { header: "#", field: "id", sortable: true },
-                    { header: "Name", field: "fullName", sortable: true },
-                    { header: "Continent", field: "continent", sortable: true },
-                    { header: "Browser", field: "browser", sortable: true },
-                    { header: "OS", field: "os", sortable: true },
-                    { header: "Visits", field: "visits", sortable: true, align: "right" }
-                ]}
-                selection={{ type: KeySelection, bind: "$page.selection" }}
-            />
+            <DetachedScope bind="$page">
+                <div controller={PageController}>
+                    <Grid
+                        records:bind="$page.records"
+                        buffered
+                        style="height: 800px"
+                        lockColumnWidths
+                        totalRecordCount:bind="$page.recordCount"
+                        offset:bind="$page.offset"
+                        loadingOffset:bind="$page.nextOffset"
+                        bufferedLoading
+                        cached
+                        keyField="id"
+                        columns={[
+                            {header: "#", field: "id", sortable: true},
+                            {header: "Name", field: "fullName", sortable: true},
+                            {header: "Continent", field: "continent", sortable: true},
+                            {header: "Browser", field: "browser", sortable: true},
+                            {header: "OS", field: "os", sortable: true},
+                            {header: "Visits", field: "visits", sortable: true, align: "right"}
+                        ]}
+                        selection={{type: KeySelection, bind: "$page.selection"}}
+                    />
+                </div>
+            </DetachedScope>
 
             <CodeSnippet putInto="code" fiddle="Qq5LHNJc">{}</CodeSnippet>
         </CodeSplit>
