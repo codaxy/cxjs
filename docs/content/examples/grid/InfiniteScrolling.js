f11304722539b10f1f7966096aada2f5ab71b9a5
Marko Stijak <mstijak@gmail.com>
2017-09-27 12:00:27 +0200

    ...

--- docs/content/examples/grid/InfiniteScrolling.js
+++ docs/content/examples/grid/InfiniteScrolling.js
@@ -1,78 +1,92 @@
 import {HtmlElement, DetachedScope, Grid} from 'cx/widgets';
 import {Controller, KeySelection} from 'cx/ui';
-import {debounce} from 'cx/util';
 import {Md} from '../../../components/Md';
 import {CodeSplit} from '../../../components/CodeSplit';
 import {CodeSnippet} from '../../../components/CodeSnippet';
 
 import {casual} from '../data/casual';
 
-class PageController extends Controller {
-    loadPage({page, pageSize}) {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                //console.log("LOAD", page, pageSize)
-                let records = [];
-                for (let i = 0; i < pageSize; i++)
-                    records.push({
-                        index: (page-1) * pageSize + i + 1,
-                        question_id: page * pageSize + i + 1,
-                        fullName: casual.full_name,
-                        continent: casual.continent,
-                        browser: casual.browser,
-                        title: casual.full_name,
-                        answer_count: casual.integer(1, 100),
-                        view_count: casual.integer(1, 100),
-                    });
-                resolve({
-                    records,
-                    totalRecordCount: 100000
-                });
-            }, 100);
-        });
-
-        return fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&page=${page}&pagesize=${pageSize}`)
-            .then(x => x.json())
-            .then(x => ({
-                records: x.items,
-                lastPage: !x.has_more
-            }));
-    }
-}
-
 export const InfiniteScrolling = <cx>
     <Md>
-
         # Infinite Grid Scrolling
 
         <CodeSplit>
 
             Infinite scrolling is a technique of dynamically loading more data based on the scroll position. Grids
             support infinite by setting the `infinite` flag and implementing the `onFetchRecords` callback which
-            is responsible for fetching data based on current sorting and filtering parameters.
+            is responsible for fetching data based on current the visible page and active sorting and filtering
+            parameters.
 
-            <DetachedScope bind="$page">
-                <div controller={PageController}>
-                    <Grid
-                        infinite
-                        onFetchRecords="loadPage"
-                        style="height: 800px"
-                        lockColumnWidths
-                        cached
-                        keyField="question_id"
-                        columns={[
-                            {header: "#", field: "index"},
-                            {header: "Answers", field: "answer_count", sortable: true},
-                            {header: "Question", field: "title"},
-                            {header: "Views", field: "view_count", sortable: true, align: "right"}
-                        ]}
-                        selection={{type: KeySelection, bind: "$page.selection", keyField: "question_id"}}
-                    />
-                </div>
-            </DetachedScope>
 
-            <CodeSnippet putInto="code">{}</CodeSnippet>
+            <Grid
+                infinite
+                style="height: 800px"
+                lockColumnWidths
+                cached
+                keyField="question_id"
+                columns={[
+                    {header: "#", field: "id", sortable: true},
+                    {header: "Name", field: "fullName", sortable: true},
+                    {header: "Continent", field: "continent", sortable: true},
+                    {header: "Browser", field: "browser", sortable: true},
+                    {header: "Visits", field: "visits", sortable: true, align: "right"}
+                ]}
+                selection={{type: KeySelection, bind: "$page.selection", keyField: "id"}}
+                onFetchRecords={({page, pageSize}) => new Promise(resolve => {
+                    setTimeout(() => {
+                        let records = [];
+                        for (let i = 0; i < pageSize; i++)
+                            records.push({
+                                id: (page - 1) * pageSize + i + 1,
+                                fullName: casual.full_name,
+                                continent: casual.continent,
+                                browser: casual.browser,
+                                title: casual.full_name,
+                                visits: casual.integer(1, 100)
+                            });
+                        resolve({
+                            records,
+                            totalRecordCount: 100000
+                        });
+                    }, 100);
+                })}
+            />
+
+            <CodeSnippet putInto="code">{`
+                <Grid
+                    infinite
+                    style="height: 800px"
+                    lockColumnWidths
+                    cached
+                    keyField="question_id"
+                    columns={[
+                        {header: "#", field: "id", sortable: true},
+                        {header: "Name", field: "fullName", sortable: true},
+                        {header: "Continent", field: "continent", sortable: true},
+                        {header: "Browser", field: "browser", sortable: true},
+                        {header: "Visits", field: "visits", sortable: true, align: "right"}
+                    ]}
+                    selection={{type: KeySelection, bind: "$page.selection", keyField: "id"}}
+                    onFetchRecords={({page, pageSize}) => new Promise(resolve => {
+                        setTimeout(() => {
+                            let records = [];
+                            for (let i = 0; i < pageSize; i++)
+                                records.push({
+                                    id: (page - 1) * pageSize + i + 1,
+                                    fullName: casual.full_name,
+                                    continent: casual.continent,
+                                    browser: casual.browser,
+                                    title: casual.full_name,
+                                    visits: casual.integer(1, 100)
+                                });
+                            resolve({
+                                records,
+                                totalRecordCount: 100000
+                            });
+                        }, 100);
+                    })}
+                />
+            `}</CodeSnippet>
         </CodeSplit>
-
     </Md>
 </cx>
