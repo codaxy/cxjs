3502e74c49a4eb24f16ba4d197ed2a95af259f67
Sasa Tatar <sasa.tatar@gmail.com>
2017-10-11 16:35:34 +0200

    Add readme and basic implementation

--- packages/create-cx-app/index.js
+++ packages/create-cx-app/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log(process.argv);
+var spawn = require('cross-spawn');
+var execSync = require('child_process').execSync;
+
+var useYarn = shouldUseYarn();
+var projectName = process.argv[2];
+
+var commands = ['create-app'];
+if (projectName) commands.push(projectName);
+if (useYarn) commands.push('--yarn');
+spawn.sync('cx', commands, {stdio: 'inherit'});
+
+function shouldUseYarn() {
+   try {
+     execSync('yarnpkg --version', { stdio: 'ignore' });
+     return true;
+   } catch (e) {
+     return false;
+   }
+}
