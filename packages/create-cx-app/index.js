3b59f34e39574841546422dafdca24e234711efb
Sasa Tatar <sasa.tatar@gmail.com>
2017-10-12 15:33:24 +0200

    script approach, not working :/

--- packages/create-cx-app/index.js
+++ packages/create-cx-app/index.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
 var spawn = require('cross-spawn');
-var execSync = require('child_process').execSync;
 
 var useYarn = shouldUseYarn();
 var projectName = process.argv[2];
 
-var commands = ['create-app'];
+var commands = ['run', 'execute', '--', 'create'];
 if (projectName) commands.push(projectName);
 if (useYarn) commands.push('--yarn');
-spawn.sync('cx', commands, {stdio: 'inherit'});
+spawn.sync('npm', commands, {stdio: 'inherit'});
 
 function shouldUseYarn() {
    try {
