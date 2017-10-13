d46531f93ac854b4e613961a47c3d8641a52215d
Ognjen <ognjen.kremenovic@codaxy.com>
2017-10-13 15:19:57 +0200

    add progressbar page to gallery

---
+++ gallery/routes/general/progressbar/states.tsx
@@ -0,0 +1,34 @@
+// import {cx, ProgressBar, FlexRow, List, Text} from 'cx/widgets';
+// import {bind, tpl, expr, Controller, KeySelection} from 'cx/ui';
+// import casual from '../../../util/casual';
+// import '../../../util/plural';
+
+// const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+// export default <cx>
+//     <a href="https://github.com/codaxy/cx/tree/master/gallery/routes/general/ProgressBar/progressbar.tsx" target="_blank" putInto="github">Source Code</a>
+//     <FlexRow wrap spacing="large" target="tablet" align="start">
+//         <ProgressBar mod="card" title="Card" hLevel={4}>
+//             <p>{loremIpsum}</p>
+//         </ProgressBar>
+
+//         <ProgressBar mod="primary" title="Primary" hLevel={4}>
+//             <p>{loremIpsum}</p>
+//         </ProgressBar>
+
+//         <ProgressBar mod="success" title="Success" hLevel={4}>
+//             <p>{loremIpsum}</p>
+//         </ProgressBar>
+
+//         <ProgressBar mod="warning" title="Warning" hLevel={4}>
+//             <p>{loremIpsum}</p>
+//         </ProgressBar>
+
+//         <ProgressBar mod="error" title="Error" hLevel={4}>
+//             <p>{loremIpsum}</p>
+//         </ProgressBar>
+//     </FlexRow>
+// </cx>
+
+// import {hmr} from '../../hmr.js';
+// hmr(module);
