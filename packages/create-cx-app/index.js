c3575bfa853d63d7fdfb18eadbf6ff9606f6315a
Sasa Tatar <sasa.tatar@gmail.com>
2017-10-16 17:37:07 +0200

    ...

--- packages/create-cx-app/index.js
+++ packages/create-cx-app/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var execSync = require('child_process').execSync;
-var exec = require('child_process').exec;
+const spawn = require('cross-spawn');
 
 var useYarn = shouldUseYarn();
 var projectName = process.argv[2];
@@ -9,7 +9,9 @@ var projectName = process.argv[2];
 var args = ['create'];
 if (projectName) args.push(projectName);
 if (useYarn) args.push('--yarn');
-exec('cx ' + args.join(' '), {stdio: 'inherit'});
+// first try, should switch to spawn
+execSync(useYarn ? 'yarn global add cx-cli' : 'npm install cx-cli --global', {stdio: 'inherit'});
+execSync('cx ' + args.join(' '), {stdio: 'inherit'});
 
 
 function shouldUseYarn() {
