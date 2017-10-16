d4ebf38e1dd590978ccb554f0fb549b6c7784064
Ognjen <ognjen.kremenovic@codaxy.com>
2017-10-16 11:24:48 +0200

    Style progressbar for different themes

--- gallery/routes/general/progressbar/states.tsx
+++ gallery/routes/general/progressbar/states.tsx
@@ -1,13 +1,16 @@
-import {cx, ProgressBar, Slider, FlexRow, Section} from 'cx/widgets';
+import {cx, ProgressBar, Slider, FlexRow, Section, Text} from 'cx/widgets';
 import {bind, tpl, expr, Controller, KeySelection} from 'cx/ui';
 import '../../../util/plural';
 
 export default <cx>
     <a href="https://github.com/codaxy/cx/tree/master/gallery/routes/general/ProgressBar/progressbar.tsx" target="_blank" putInto="github">Source Code</a>
     <FlexRow wrap spacing="large" target="tablet" align="start">
-      <Section mod="card">
-         <ProgressBar text={tpl("{$page.value:p;0}")} value={bind('$page.value')} />
-         <Slider value={bind('$page.value')} maxValue={1} />
+      <Section mod="card" title="Standard">
+        <ProgressBar text={tpl("{$page.value:p;0}")} value={bind('$page.value')} />     
+        <Slider value={bind('$page.value')} maxValue={1} />
+      </Section>  
+      <Section mod="card" title="Disabled">
+        <ProgressBar disabled text={'50%'} value={0.5} />     
       </Section>  
     </FlexRow>
 </cx>
