fe847a0452f3deebec2458985ab14ab399ae8d8d
Sasa Tatar <sasa.tatar@gmail.com>
2017-10-17 12:05:24 +0200

    Remove unnecessary dependencies

--- packages/create-cx-app/index.js
+++ packages/create-cx-app/index.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 
 var execSync = require('child_process').execSync;
-//const spawn = require('cross-spawn');
 
 var useYarn = shouldUseYarn();
-var projectName = process.argv[2];
 
-// if (useYarn) {
-//   spawn.sync('yarn', ['global', 'add', 'cx-cli'], {stdio: 'inherit'});
-// } else {
-//   spawn.sync('npm', ['install', 'cx-cli', '--global'], {stdio: 'inherit'});
-// }
 execSync(useYarn ? 'yarn global add cx-cli' : 'npm install cx-cli --global', {stdio: 'inherit'});
 
+var projectName = process.argv[2];
 var args = ['create'];
 if (projectName) args.push(projectName);
 if (useYarn) args.push('--yarn');
+
 execSync('cx ' + args.join(' '), {stdio: 'inherit'});
 
 function shouldUseYarn() {
