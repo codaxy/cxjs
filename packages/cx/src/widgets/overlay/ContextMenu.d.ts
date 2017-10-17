2dd358bbe30414218219543c704ca4dc4bce53fc
Sasa Tatar <sasa.tatar@gmail.com>
2017-10-17 10:29:52 +0200

    Fix typo in TS definition

--- packages/cx/src/widgets/overlay/ContextMenu.d.ts
+++ packages/cx/src/widgets/overlay/ContextMenu.d.ts
@@ -6,4 +6,4 @@ export interface ContextMenuProps extends DropdownProps {}
 
 export class ContextMenu extends Cx.Widget<ContextMenuProps> {}
 
-export function opentContextMenu(e: MouseEvent, contents: any, store?: View, options?: any);
+export function openContextMenu(e: MouseEvent, contents: any, store?: View, options?: any);
