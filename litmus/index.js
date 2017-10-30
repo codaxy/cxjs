58f839d4fc83d9ca4a7f4b524396e5d7c7af96fb
Marko Stijak <mstijak@gmail.com>
2017-10-30 15:55:30 +0100

    Allow invoking controller methods from DetachedScopes (fixes #309)

--- litmus/index.js
+++ litmus/index.js
@@ -36,7 +36,7 @@ import './index.scss';
 //import Demo from './features/logo';
 //import Demo from './features/destroy';
 //import Demo from './features/tracking';
-//import Demo from './features/caching/IsolatedBoxes';
+import Demo from './features/caching/IsolatedBoxes';
 //import Demo from './features/context-menu';
 //import Demo from './features/charts/time-axis/LocalTime';
 
@@ -44,7 +44,7 @@ import './index.scss';
 //import Demo from './features/grid/GridBuffering';
 //import Demo from './features/grid/RowEditing';
 //import Demo from './features/hscroll';
-import Demo from './features/grid/InfiniteScroll';
+//import Demo from './features/grid/InfiniteScroll';
 
 //import Demo from './features/menu/icons';
 
