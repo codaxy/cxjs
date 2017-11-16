5d9452034a199b43fed95a9f990cfaf25124134d
Marko Stijak <mstijak@gmail.com>
2017-11-16 10:04:12 +0100

    Fix findScrollableParent problem

--- packages/cx/src/util/findScrollableParent.js
+++ packages/cx/src/util/findScrollableParent.js
@@ -2,10 +2,12 @@ import {closest} from './DOM';
 
 export function findScrollableParent(sourceEl, horizontal = false) {
    let scrollParent = sourceEl && closest(sourceEl, el => {
-         if (el.offsetHeight >= el.scrollHeight)
-            return false;
-         let oy = getComputedStyle(el)[horizontal ? "overflow-x" : "overflow-y"];
-         return oy == "auto" || oy == "scroll";
-      });
+      if (el.nodeType != Node.ELEMENT_NODE)
+         return false;
+      if (el.offsetHeight >= el.scrollHeight)
+         return false;
+      let oy = getComputedStyle(el)[horizontal ? "overflow-x" : "overflow-y"];
+      return oy == "auto" || oy == "scroll";
+   });
    return scrollParent || document.scrollingElement || document.documentElement;
 }
