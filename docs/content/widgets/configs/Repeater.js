cac2be761c68a5ec02a6d60d76072e498cc71c58
Sasa Tatar <sasa.tatar@gmail.com>
2017-12-04 16:33:49 +0100

    Add support for sortField and sortDirection to Repeater, update docs.

---
+++ docs/content/widgets/configs/Repeater.js
@@ -0,0 +1,43 @@
+import {Md} from '../../../components/Md';
+
+export default {
+    recordName: {
+        type: 'string',
+        key: true,
+        description: <cx><Md>
+            Name used to expose record data. Defaults to `$record`.
+        </Md></cx>
+    },
+    indexName: {
+        type: 'string',
+        key: true,
+        description: <cx><Md>
+            Name used to expose record index. Defaults to `$index`.
+        </Md></cx>
+    },
+    sortField: {
+        type: 'string',
+        key: true,
+        description: <cx><Md>
+            A binding used to store the name of the field used for sorting the collection. 
+            Available only if `sorters` are not used.
+        </Md></cx>
+    },
+    sortDirection: {
+        type: 'string',
+        key: true,
+        description: <cx><Md>
+            A binding used to store the sort direction. Available only if `sorters` are not used.
+            Possible values are `"ASC"` and `"DESC"`. Deafults to `"ASC"`.
+        </Md></cx>
+    },
+    sorters: {
+        type: 'array',
+        key: true,
+        description: <cx><Md>
+            A binding used to store the sorting order list. 
+            This should be an array of objects with `field` and `direction` properties
+            (equivalent to `sortField` and `sortDirection` properties).
+        </Md></cx>
+    },
+};
