7aba01bea61cee771e496e66ba02c1cdcf54744a
Jiří Pudil <me@jiripudil.cz>
2017-12-06 09:44:08 +0100

    Modify TextField.icon type definition to allow binding

--- packages/cx/src/widgets/form/TextField.d.ts
+++ packages/cx/src/widgets/form/TextField.d.ts
@@ -64,7 +64,7 @@ export interface TextFieldProps extends FieldProps {
    validationRegExp?: RegExp,
    
    /** Name of the icon to be put on the left side of the input. */
-   icon?: string,
+   icon?: Cx.StringProp,
 
    /** Set to `true` to monitor and report if input value has changed after the form has been
     automatically filled. */
