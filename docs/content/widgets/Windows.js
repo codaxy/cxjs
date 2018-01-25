7678ac8fcd21ec130968a2eb1b9030cbdfb3acad
Sasa Tatar <sasa.tatar@gmail.com>
2018-01-25 16:55:51 +0100

    Intervention issue when trying to drag Window using touch

--- docs/content/widgets/Windows.js
+++ docs/content/widgets/Windows.js
@@ -27,43 +27,47 @@ export const Windows = <cx>
                     visible={{ bind: "$page.contact.visible", defaultValue: false }}
                     center
                     style={{width: '500px'}}
-                    modal>
-               <div style={{padding: "20px"}} layout={{type: LabelsLeftLayout, mod: 'stretch'}}>
-                  <TextField label="Name" value:bind="$page.contact.name" style={{width: '100%'}} tooltip="A Tooltip" />
-                  <TextField label="Email" value:bind="$page.contact.email" style={{width: '100%'}}/>
-                  <TextArea label="Message" value:bind="$page.contact.message" rows={10} style={{width: '100%'}}/>
-                  <DateField label="Date" value:bind="$page.contact.date" />
-               </div>
-               <div putInto="footer" style={{float:"right"}} trimWhitespace={false}>
-                  <Button mod="primary">Submit</Button>
-                  <Button onClick={(e, ins) => { ins.parentOptions.dismiss() }}>
-                     Cancel
-                  </Button>
-               </div>
+                    modal
+                    draggable
+                >
+                <div style={{padding: "20px"}} layout={{type: LabelsLeftLayout, mod: 'stretch'}}>
+                    <TextField label="Name" value:bind="$page.contact.name" style={{width: '100%'}} tooltip="A Tooltip" />
+                    <TextField label="Email" value:bind="$page.contact.email" style={{width: '100%'}}/>
+                    <TextArea label="Message" value:bind="$page.contact.message" rows={10} style={{width: '100%'}}/>
+                    <DateField label="Date" value:bind="$page.contact.date" />
+                </div>
+                <div putInto="footer" style={{float:"right"}} trimWhitespace={false}>
+                    <Button mod="primary">Submit</Button>
+                    <Button onClick={(e, ins) => { ins.parentOptions.dismiss() }}>
+                        Cancel
+                    </Button>
+                </div>
             </Window>
          </div>
 
          <Content name="code">
             <CodeSnippet fiddle="5GzabX9A">{`
-               <Button onClick={(e, {store}) => { store.set('$page.contact.visible', true)}}>Open</Button>
-               <Window title="Contact"
-                       visible={{ bind: "$page.contact.visible", defaultValue: false }}
-                       center
-                       style={{width: '500px'}}
-                       modal>
-                  <div style={{padding: "20px"}} layout={{type: LabelsLeftLayout, mod: 'stretch'}}>
-                     <TextField label="Name" value:bind="$page.contact.name" style={{width: '100%'}} />
-                     <TextField label="Email" value:bind="$page.contact.email" style={{width: '100%'}}/>
-                     <TextArea label="Message" value:bind="$page.contact.message" rows={10} style={{width: '100%'}}/>
-                     <DateField label="Date" value:bind="$page.contact.date" />
-                  </div>
-                  <div putInto="footer" style={{float:"right"}} trimWhitespace={false}>
-                     <Button mod="primary">Submit</Button>
-                     <Button onClick={(e, ins) => { ins.parentOptions.dismiss() }}>
-                        Cancel
-                     </Button>
-                  </div>
-               </Window>
+                <Button onClick={(e, {store}) => { store.set('$page.contact.visible', true)}}>Open</Button>
+                <Window title="Contact"
+                        visible={{ bind: "$page.contact.visible", defaultValue: false }}
+                        center
+                        style={{width: '500px'}}
+                        modal
+                        draggable
+                    >
+                    <div style={{padding: "20px"}} layout={{type: LabelsLeftLayout, mod: 'stretch'}}>
+                        <TextField label="Name" value:bind="$page.contact.name" style={{width: '100%'}} />
+                        <TextField label="Email" value:bind="$page.contact.email" style={{width: '100%'}}/>
+                        <TextArea label="Message" value:bind="$page.contact.message" rows={10} style={{width: '100%'}}/>
+                        <DateField label="Date" value:bind="$page.contact.date" />
+                    </div>
+                    <div putInto="footer" style={{float:"right"}} trimWhitespace={false}>
+                        <Button mod="primary">Submit</Button>
+                        <Button onClick={(e, ins) => { ins.parentOptions.dismiss() }}>
+                            Cancel
+                        </Button>
+                    </div>
+                </Window>
             `}</CodeSnippet>
          </Content>
 
