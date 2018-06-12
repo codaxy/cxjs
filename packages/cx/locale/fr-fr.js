a8063afbb6214bceb2f72421f2c7aebb32459341
mrdjanm <40194123+mrdjanm@users.noreply.github.com>
2018-06-12 12:57:17 +0200

    Add French localization

--- packages/cx/locale/fr-fr.js
+++ packages/cx/locale/fr-fr.js
@@ -5,7 +5,7 @@ var c = 'fr-fr';
 // Field
 Localization.localize(c, 'cx/widgets/Field', {
    requiredText: 'Ce champ est obligatoire.',
-   validatingText: 'La validation est en cours ...',
+   validatingText: 'La validation est en cours...',
    validationExceptionText: 'Un problème est survenu lors de la validation des données. Consultez le journal pour plus de détails'
 });
 
@@ -19,10 +19,10 @@ Localization.localize(c, 'cx/widgets/LookupField', {
 
 // In common for Calendar and MonthPicker
 const calendarErrorMessages = {
-   maxValueErrorText: 'La date sélectionnée est postérieure à la dernière date autorisée {0: d}.',
-   maxExclusiveErrorText: 'La date sélectionnée doit être antérieure à {0: d}.',
-   minValueErrorText: 'La date sélectionnée est antérieure à {0: d}.',
-   minExclusiveErrorText: 'La date sélectionnée doit être postérieure à {0: d}.',
+   maxValueErrorText: 'La date sélectionnée est postérieure à la dernière date autorisée {0:d}.',
+   maxExclusiveErrorText: 'La date sélectionnée doit être antérieure à {0:d}.',
+   minValueErrorText: 'La date sélectionnée est antérieure à {0:d}.',
+   minExclusiveErrorText: 'La date sélectionnée doit être postérieure à {0:d}.',
 };
 // Calendar
 Localization.localize(c, 'cx/widgets/Calendar', calendarErrorMessages);
@@ -32,7 +32,7 @@ Localization.localize(c, 'cx/widgets/MonthPicker', calendarErrorMessages);
 // In common for DateField and MonthField
 const dateFieldErrorMessages = {
    ...calendarErrorMessages,
-   inputErrorText: 'Invalid date entered.' 
+   inputErrorText: 'Date invalide.' 
 };
 // MonthField
 Localization.localize(c, 'cx/widgets/MonthField', dateFieldErrorMessages);
@@ -41,21 +41,21 @@ Localization.localize(c, 'cx/widgets/DateField', dateFieldErrorMessages);
 
 // NumberField
 Localization.localize(c, 'cx/widgets/NumberField', {
-   maxValueErrorText: 'Enter {0:n} or less.',
-   maxExclusiveErrorText: 'Enter a number less than {0:n}.',
-   minValueErrorText: 'Enter {0:n} or more.',
-   minExclusiveErrorText: 'Enter a number greater than {0:n}.',
-   inputErrorText: 'Invalid number entered.'
+   maxValueErrorText: 'Entrez {0:n} ou moins.',
+   maxExclusiveErrorText: 'Entrez un nombre inférieur à {0:n}.',
+   minValueErrorText: 'Entrez {0:n} ou plus.',
+   minExclusiveErrorText: 'Entrez un nombre supérieur à {0:n}.',
+   inputErrorText: 'Numéro invalide.'
 });
 
 // TextField
 Localization.localize(c, 'cx/widgets/TextField', {
-   validationErrorText: 'The entered value is not valid.',
-   minLengthValidationErrorText: 'Enter {[{0}-{1}]} more character(s).',
-   maxLengthValidationErrorText: 'Use {0} characters or fewer.'
+   validationErrorText: "La valeur entrée n'est pas valide.",
+   minLengthValidationErrorText: 'Entrez {[{0}-{1}]} caractères supplémentaires.',
+   maxLengthValidationErrorText: 'Utilisez {0} caractères ou moins.'
 });
 
 // UploadButton
 Localization.localize(c, 'cx/widgets/UploadButton', {
-   validationErrorText: 'Upload is in progress.'
+   validationErrorText: 'Le téléchargement est en cours.'
 });
