df367e9b4aa920c7acd1993b7d58b35812892397
Sasa Tatar <sasa.tatar@gmail.com>
2018-08-23 09:03:43 +0200

    Add PrivateState alias for Restate

--- packages/cx/src/ui/Restate.d.ts
+++ packages/cx/src/ui/Restate.d.ts
@@ -8,3 +8,4 @@ interface RestateProps extends Cx.PureContainerProps {
 }
 
 export class Restate extends Cx.Widget<RestateProps> {}
+export class PrivateState extends Cx.Widget<RestateProps> {}
--- packages/cx/src/ui/Restate.js
+++ packages/cx/src/ui/Restate.js
@@ -154,3 +154,5 @@ class RestateStore extends Store {
       super.doNotify();
    }
 }
+
+export class PrivateState extends Restate {};
