a63272859688dcabdf59622824859d38dd0aa5f0
Marko Stijak <mstijak@gmail.com>
2018-09-12 14:50:45 +0200

    Add the missing test for UseParentLayout and FirstVisibleChildLayout

--- packages/cx/src/ui/layout/FirstVisibleChildLayout.spec.js
+++ packages/cx/src/ui/layout/FirstVisibleChildLayout.spec.js
@@ -6,6 +6,8 @@ import {Store} from '../../data/Store';
 import renderer from 'react-test-renderer';
 import assert from 'assert';
 import {FirstVisibleChildLayout} from "./FirstVisibleChildLayout";
+import {UseParentLayout} from "./UseParentLayout";
+import {PureContainer} from "../PureContainer";
 
 describe('FirstVisibleChildLayout', () => {
 
@@ -39,6 +41,33 @@ describe('FirstVisibleChildLayout', () => {
       let widget = <cx>
          <div layout={FirstVisibleChildLayout}>
             <header visible={false}></header>
+            <main></main>
+            <footer></footer>
+         </div>
+      </cx>;
+
+      let store = new Store();
+
+      const component = renderer.create(
+         <Cx widget={widget} store={store} subscribe immediate/>
+      );
+
+      let tree = component.toJSON();
+      assert.deepEqual(tree, {
+            type: 'div',
+            props: {},
+            children: [{type: 'main', props: {}, children: null}]
+         }
+      )
+   });
+
+   it.skip('skips pure containers which use parent layouts', () => {
+
+      let widget = <cx>
+         <div layout={FirstVisibleChildLayout}>
+            <PureContainer layout={UseParentLayout}>
+               <header visible={false}></header>
+            </PureContainer>
             <main></main>
             <footer></footer>
          </div>
