380c42ef23dee48f1b5075ae8f3d85f15eb19f80
Marko Stijak <mstijak@gmail.com>
2018-11-11 05:36:40 +0100

    Fix search in Docs

--- docs/components/DocSearch.js
+++ docs/components/DocSearch.js
@@ -5,8 +5,8 @@ export class DocSearch extends VDOM.Component
     render() {
         return <input
             type="text"
+            id="docsearch"
             placeholder="Search..."
-            ref={el => this.el = el}
             className="docsearch"
         />
     }
@@ -17,7 +17,7 @@ export class DocSearch extends VDOM.Component
             docsearch({
                 apiKey: 'b77ab797ddcee40f03751aeb694168ed',
                 indexName: 'cxjs',
-                inputSelector: this.el,
+                inputSelector: '#docsearch',
                 debug: false // Set debug to true if you want to inspect the dropdown
             });
             } 
