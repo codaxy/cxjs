6c246f70c69603c396ebd62280bf9e7b83606185
Sasha Tatar <sasa.tatar@gmail.com>
2019-06-29 22:21:59 +0200

    ...

--- docs/content/concepts/PrivateStores.js
+++ docs/content/concepts/PrivateStores.js
@@ -1,10 +1,9 @@
-import { computable, Controller, UseParentLayout } from 'cx/ui';
-import { Icon, LookupField, PrivateStore, Rescope, Slider, TextField } from 'cx/widgets';
+import { computable } from 'cx/ui';
+import { PrivateStore, Rescope, Slider } from 'cx/widgets';
 import { CodeSnippet } from 'docs/components/CodeSnippet';
 import { CodeSplit } from 'docs/components/CodeSplit';
 import { ImportPath } from 'docs/components/ImportPath';
 import { Md } from 'docs/components/Md';
-import { casual } from 'docs/content/examples/data/casual';
 import { ConfigTable } from '../../components/ConfigTable';
 import config from './configs/PrivateStore';
 
@@ -42,11 +41,6 @@ export const PrivateStores = <cx>
                     </PrivateStore>
                 </div>
 
-                Private stores have the lifespan of the `PrivateStore` component, meaning each time 
-                a `PrivateStore` components is destroyed, the data is lost. 
-                This can be observed if you move the sliders, navigate to another page and come back again. 
-                Only the sliders within the global store will be in the same position as before, while the others will be in their starting positions.
-
                 <CodeSnippet putInto="code" /* fiddle="F3RHqb0x" */>{`
                     <div class="widgets">  
                         <div class="flex-column">
@@ -68,6 +62,11 @@ export const PrivateStores = <cx>
                     </div>
                 `}</CodeSnippet>
             </CodeSplit>
+
+            Private stores have the lifespan of the `PrivateStore` component, meaning each time 
+            a `PrivateStore` components is destroyed, the data is lost. 
+            This can be observed if you move the sliders, navigate to another page and come back again. 
+            Only the sliders within the global store will be in the same position as before, while the others will be in their starting positions.
 
             <CodeSplit>
                 ## Sharing data between the stores
@@ -113,14 +112,16 @@ export const PrivateStores = <cx>
                     </div>
                 `}</CodeSnippet>
 
-                Property values can be primitives, bindings, expressions or computables.
             </CodeSplit>
+            Property values can be primitives, bindings, expressions or computables.
 
             <CodeSplit>
                 ### Read-only values
 
                 Primitives, expressions and computables defined in the `data` object are treated as read-only.
-                This means, data will flow only in one direction, from parent to child store.
+                This means, data will flow only in one direction, from parent to child store. You can test this by 
+                trying to move one of the sliders within the `PrivateStore`. In the example below, `slider` binding from the
+                global store is available as `globalValue` binding within the `PrivateStore`.
             
                 <div class="widgets flex-row flex-start">  
                     <div class="flex-column">
@@ -137,9 +138,9 @@ export const PrivateStores = <cx>
                     >
                         <div class="flex-column">
                             <strong>Private store</strong>
-                            <Slider value-bind="globalValueExpr" label="Global value" disabled/>
-                            <Slider value-bind="primitiveValue" label="Primitive value" disabled/>
-                            <Slider value-bind="computedValue" label="Computed value" disabled/>
+                            <Slider value-bind="globalValueExpr" label="Global value" />
+                            <Slider value-bind="primitiveValue" label="Primitive value" />
+                            <Slider value-bind="computedValue" label="Computed value" />
                         </div> 
                     </PrivateStore>
                 </div>
@@ -160,24 +161,24 @@ export const PrivateStores = <cx>
                         >
                             <div class="flex-column">
                                 <strong>Private store</strong>
-                                <Slider value-bind="globalValueExpr" label="Global value" disabled/>
-                                <Slider value-bind="primitiveValue" label="Primitive value" disabled/>
-                                <Slider value-bind="computedValue" label="Computed value" disabled/>
+                                <Slider value-bind="globalValueExpr" label="Global value" />
+                                <Slider value-bind="primitiveValue" label="Primitive value" />
+                                <Slider value-bind="computedValue" label="Computed value" />
                             </div> 
                         </PrivateStore>
                     </div>
                 `}</CodeSnippet>
-
-                Trying to change read-only values from within the child store will log an error to the console, 
-                so the UI should prevent it.
             </CodeSplit>
+
+            Attemting to change `PrivateStore's` read-only values will log an error to the console, so the UI should prevent it.
 
             ## Performance improvements
 
             `PrivateStore` can also be used for performance improvements since it supports some advanced features such es 
             deferred rendering and seperate (detached) render loop. See the configuration table below for more information.
 
-            **Note:** when using `detached` property, be aware that it may break some advanced Cx features, such as layouts, that depend on the use of `context`.
+            **Note:** when using `detached` property, be aware that it may break some advanced Cx features, such as layouts, 
+            that depend on the use of `context`.
 
             <ConfigTable props={config} />
         </Md>
