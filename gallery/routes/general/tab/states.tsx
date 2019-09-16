077a01fd736b0914da3f28cd6ff3fd4dac03b0b2
danijela991 <d.cvisic91@gmail.com>
2019-09-16 15:55:59 +0200

    New theme finish tuches

--- gallery/routes/general/tab/states.tsx
+++ gallery/routes/general/tab/states.tsx
@@ -40,7 +40,7 @@ export default <cx>
             title="Classic"
             hLevel={4}
             style="max-width: 400px"
-            visible={{expr: ("{$root.$route.theme}!='material' && {$root.$route.theme}!='material-dark'")}}
+            visible={{expr: ("{$root.$route.theme}!='material' && {$root.$route.theme}!='material-dark' && {$root.$route.theme}!='queenblue'")}}
         >
             <div styles="padding-left:10px;white-space:nowrap;">
                 <Tab tab="tab1" value={bind("$page.t3")} mod="classic" default>Tab 1</Tab>
@@ -49,6 +49,36 @@ export default <cx>
                 <Tab tab="tab4" value={bind("$page.t3")} mod="classic" disabled>Tab 4</Tab>
             </div>
             <div mod="cover" styles="border-width: 1px; padding: 20px">
+                <p visible={expr("{$page.t2}=='tab1'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet cursus diam. Proin ultricies congue vehicula. In at felis id.</p>
+                <p visible={expr("{$page.t2}=='tab2'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ex magna, maximus in venenatis at, iaculis eu augue. Vestibulum id.</p>
+                <p visible={expr("{$page.t2}=='tab3'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor arcu, hendrerit ultrasonic cursus eget, efficitur quis erat. Donec dui.</p>
+                <p visible={expr("{$page.t2}=='tab4'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porta ultricies arcu nec auctor. Nullam felis nibh, accumsan a ultrices.</p>
+            </div>
+        </Section>
+        <Section mod="well" title="Bottom Tabs" hLevel={4} style="max-width: 400px" visible={{expr: ("{$root.$route.theme}=='queenblue'")}}>
+            <p>
+                <Tab tab="tab1" value={bind("$page.t2")} mod="bottom" default>Tab 1</Tab>
+                <Tab tab="tab2" value={bind("$page.t2")} mod="bottom">Tab 2</Tab>
+                <Tab tab="tab3" value={bind("$page.t2")} mod="bottom">Tab 3</Tab>
+                <Tab tab="tab4" value={bind("$page.t2")} mod="bottom" disabled>Tab 4</Tab>
+            </p>
+
+            <div>
+                <p visible={expr("{$page.t2}=='tab1'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet cursus diam. Proin ultricies congue vehicula. In at felis id.</p>
+                <p visible={expr("{$page.t2}=='tab2'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ex magna, maximus in venenatis at, iaculis eu augue. Vestibulum id.</p>
+                <p visible={expr("{$page.t2}=='tab3'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor arcu, hendrerit ultrasonic cursus eget, efficitur quis erat. Donec dui.</p>
+                <p visible={expr("{$page.t2}=='tab4'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi porta ultricies arcu nec auctor. Nullam felis nibh, accumsan a ultrices.</p>
+            </div>
+        </Section>
+        <Section mod="well" title="Dashboard Tabs" hLevel={4} style="max-width: 400px" visible={{expr: ("{$root.$route.theme}=='queenblue'")}}>
+            <p>
+                <Tab tab="tab1" value={bind("$page.t2")} mod="dashboard-nav" default>Tab 1</Tab>
+                <Tab tab="tab2" value={bind("$page.t2")} mod="dashboard-nav">Tab 2</Tab>
+                <Tab tab="tab3" value={bind("$page.t2")} mod="dashboard-nav">Tab 3</Tab>
+                <Tab tab="tab4" value={bind("$page.t2")} mod="dashboard-nav" disabled>Tab 4</Tab>
+            </p>
+
+            <div>
                 <p visible={expr("{$page.t2}=='tab1'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet cursus diam. Proin ultricies congue vehicula. In at felis id.</p>
                 <p visible={expr("{$page.t2}=='tab2'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ex magna, maximus in venenatis at, iaculis eu augue. Vestibulum id.</p>
                 <p visible={expr("{$page.t2}=='tab3'")}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dolor arcu, hendrerit ultrasonic cursus eget, efficitur quis erat. Donec dui.</p>
