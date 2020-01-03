531caa2f40fc6c730b7187132177a015b3a43453
Marko Stijak <mstijak@gmail.com>
2020-01-03 23:30:12 +0100

    ...

--- packages/cx/build/index.js
+++ packages/cx/build/index.js
@@ -1,145 +1,193 @@
-const
-   build = require('./build'),
-   getPathResolver = require('./getPathResolver'),
+const build = require("cx-build-tools/build"),
+   getPathResolver = require("cx-build-tools/getPathResolver"),
    resolvePath = getPathResolver(__dirname),
-   cxSrc = getPathResolver(resolvePath('../src'));
+   cxSrc = getPathResolver(resolvePath("../src"));
 
-const entries = [{
-   name: 'util',
-   options: {
-      input: cxSrc('util/index.js')
+const entries = [
+   {
+      name: "util",
+      options: {
+         input: cxSrc("util/index.js")
+      },
+      output: {}
    },
-   output: {}
-}, {
-   name: 'data',
+   {
+      name: "data",
 
-   options: {
-      input: cxSrc('data/index.js')
+      options: {
+         input: cxSrc("data/index.js")
+      },
+      output: {}
    },
-   output: {}
-}, {
-   name: 'ui',
-   options: {
-      input: [cxSrc('ui/index.js')]
+   {
+      name: "ui",
+      options: {
+         input: [cxSrc("ui/index.js")]
+      },
+      output: {}
    },
-   output: {}
-}, {
-   name: 'widgets',
-   css: true,
-   options: {
-      input: [cxSrc('widgets/index.js'), cxSrc('variables.scss'), cxSrc('widgets/index.scss'), cxSrc('ui/index.scss')]
+   {
+      name: "widgets",
+      css: true,
+      options: {
+         input: [
+            cxSrc("widgets/index.js"),
+            cxSrc("variables.scss"),
+            cxSrc("widgets/index.scss"),
+            cxSrc("ui/index.scss")
+         ]
+      },
+      output: {}
    },
-   output: {}
-}, {
-   name: 'svg',
-   css: true,
-   options: {
-      input: [cxSrc('svg/index.js'), cxSrc('variables.scss'), cxSrc('svg/index.scss')]
+   {
+      name: "svg",
+      css: true,
+      options: {
+         input: [
+            cxSrc("svg/index.js"),
+            cxSrc("variables.scss"),
+            cxSrc("svg/index.scss")
+         ]
+      },
+      output: {}
    },
-   output: {}
-}, {
-   name: 'charts',
-   css: true,
-   options: {
-      input: [cxSrc('charts/index.js'), cxSrc('variables.scss'), cxSrc('charts/index.scss')]
+   {
+      name: "charts",
+      css: true,
+      options: {
+         input: [
+            cxSrc("charts/index.js"),
+            cxSrc("variables.scss"),
+            cxSrc("charts/index.scss")
+         ]
+      },
+      output: {}
    },
-   output: {}
-}, {
-   name: 'reset',
-   css: true,
-   options: {
-      input: [cxSrc('variables.scss'), resolvePath("reset.scss"), cxSrc('global.scss')]
+   {
+      name: "reset",
+      css: true,
+      options: {
+         input: [
+            cxSrc("variables.scss"),
+            resolvePath("reset.scss"),
+            cxSrc("global.scss")
+         ]
+      },
+      output: {}
    },
-   output: {}
-}, {
-   name: 'hooks',
-   options: {
-      input: [cxSrc('hooks/index.js')]
-   },
-   output: {}
-}];
+   {
+      name: "hooks",
+      options: {
+         input: [cxSrc("hooks/index.js")]
+      },
+      output: {}
+   }
+];
 
 const externalPaths = {
-   [cxSrc('./util/')]: 'cx/util',
-   [cxSrc('./data/')]: 'cx/data',
-   [cxSrc('./ui/')]: 'cx/ui',
-   [cxSrc('./widgets')]: 'cx/widgets',
-   [cxSrc('./charts')]: 'cx/charts',
-   [cxSrc('./svg/')]: 'cx/svg',
-   [cxSrc('./hooks/')]: 'cx/hooks'
+   [cxSrc("./util/")]: "cx/util",
+   [cxSrc("./data/")]: "cx/data",
+   [cxSrc("./ui/")]: "cx/ui",
+   [cxSrc("./widgets")]: "cx/widgets",
+   [cxSrc("./charts")]: "cx/charts",
+   [cxSrc("./svg/")]: "cx/svg",
+   [cxSrc("./hooks/")]: "cx/hooks"
 };
 
 (async function buildAll() {
-
    console.log("Building cx...");
-   await build(resolvePath('../src'), resolvePath('../dist'), entries, externalPaths);
+   await build(
+      resolvePath("../src"),
+      resolvePath("../dist"),
+      entries,
+      externalPaths
+   );
 
    console.log("Building cx-redux...");
    await build(
-      resolvePath('../../cx-redux/src'),
-      resolvePath('../../cx-redux/dist'),
-      [{
-         name: 'index',
-         options: {
-            input: [resolvePath('../../cx-redux/src/index.js')]
-         },
-         output: {}
-      }],
+      resolvePath("../../cx-redux/src"),
+      resolvePath("../../cx-redux/dist"),
+      [
+         {
+            name: "index",
+            options: {
+               input: [resolvePath("../../cx-redux/src/index.js")]
+            },
+            output: {}
+         }
+      ],
       null,
-      ['redux', 'cx/data']
+      ["redux", "cx/data"]
    );
 
    function buildTheme(themeName) {
       console.log(`Building ${themeName}...`);
-      let theme = getPathResolver(resolvePath('../../' + themeName));
-      let themeSrc = getPathResolver(theme('src'));
+      let theme = getPathResolver(resolvePath("../../" + themeName));
+      let themeSrc = getPathResolver(theme("src"));
 
       return build(
-         theme('src'),
-         theme('dist'),
-         [{
-            name: 'index',
-            options: {
-               input: [themeSrc('index.js')]
+         theme("src"),
+         theme("dist"),
+         [
+            {
+               name: "index",
+               options: {
+                  input: [themeSrc("index.js")]
+               },
+               output: {}
             },
-            output: {}
-         }, {
-            name: 'reset',
-            css: true,
-            options: {
-               input: [themeSrc('variables.scss'), resolvePath("reset.scss"), themeSrc('reset.scss')]
+            {
+               name: "reset",
+               css: true,
+               options: {
+                  input: [
+                     themeSrc("variables.scss"),
+                     resolvePath("reset.scss"),
+                     themeSrc("reset.scss")
+                  ]
+               },
+               output: {}
             },
-            output: {}
-         }, {
-            name: 'charts',
-            css: true,
-            options: {
-               input: [themeSrc('variables.scss'), cxSrc('charts/index.scss')]
+            {
+               name: "charts",
+               css: true,
+               options: {
+                  input: [
+                     themeSrc("variables.scss"),
+                     cxSrc("charts/index.scss")
+                  ]
+               },
+               output: {}
             },
-            output: {}
-         }, {
-            name: 'widgets',
-            css: true,
-            options: {
-               input: [themeSrc('variables.scss'), cxSrc('widgets/index.scss'), cxSrc('ui/index.scss'), themeSrc('widgets.scss')]
+            {
+               name: "widgets",
+               css: true,
+               options: {
+                  input: [
+                     themeSrc("variables.scss"),
+                     cxSrc("widgets/index.scss"),
+                     cxSrc("ui/index.scss"),
+                     themeSrc("widgets.scss")
+                  ]
+               },
+               output: {}
             },
-            output: {}
-         }, {
-            name: 'svg',
-            css: true,
-            options: {
-               input: [themeSrc('variables.scss'), cxSrc('svg/index.scss')]
-            },
-            output: {}
-         }],
+            {
+               name: "svg",
+               css: true,
+               options: {
+                  input: [themeSrc("variables.scss"), cxSrc("svg/index.scss")]
+               },
+               output: {}
+            }
+         ],
          null,
-         ['cx/ui', 'cx/widgets']
-      )
+         ["cx/ui", "cx/widgets"]
+      );
    }
 
    await buildTheme("cx-theme-material");
    await buildTheme("cx-theme-aquamarine");
    await buildTheme("cx-theme-dark");
    await buildTheme("cx-theme-frost");
-
 })();
