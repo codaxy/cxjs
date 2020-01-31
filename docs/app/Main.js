abe956fdae0e0ab8953e672527aed0b807dff21e
Marko Stijak <mstijak@gmail.com>
2020-01-31 22:50:34 +0100

    ...

--- docs/app/Main.js
+++ docs/app/Main.js
@@ -1,19 +1,24 @@
-import {DocumentTitle, PureContainer, Content, Link, Button} from "cx/widgets";
-import {HtmlElement} from "cx/widgets";
-import {Layout} from "./Layout";
-import {Contents} from "../content/Contents";
-import {ContentRouter} from "../content/ContentRouter";
-import {Floater} from "../components/Floater";
-import {MasterLayout} from "../../misc/layout";
-import {NavTree} from "../../misc/components/NavTree";
-import {docsNav, docsNavTree, DocsNav} from "./DocsNav";
-import {SideNav} from "../components/SideNav";
+import {
+    DocumentTitle,
+    PureContainer,
+    Content,
+    Link,
+    Button
+} from "cx/widgets";
+import { HtmlElement } from "cx/widgets";
+import { Layout } from "./Layout";
+import { Contents } from "../content/Contents";
+import { ContentRouter } from "../content/ContentRouter";
+import { Floater } from "../components/Floater";
+import { MasterLayout } from "../../misc/layout";
+import { NavTree } from "../../misc/components/NavTree";
+import { docsNav, docsNavTree, DocsNav } from "./DocsNav";
+import { SideNav } from "../components/SideNav";
 
 export const Main = (
     <cx>
-
         <MasterLayout app="docs">
-            <DocumentTitle text="CxJS Docs"/>
+            <DocumentTitle text="CxJS Docs" />
             {/* <Content name="aside" items={Contents}/>
     <Floater if-expr="{layout.touch}"/> */}
             <div class="sticky topbanner">
@@ -21,7 +26,7 @@ export const Main = (
 
                 <div class="topbanner_tabs">
                     <Link href="~/intro" url-bind="url" match="subroute">
-                        Overview
+                        Intro
                     </Link>
                     <Link href="~/concepts" url-bind="url" match="subroute">
                         Concepts
@@ -40,18 +45,20 @@ export const Main = (
             <div class="docsmain" style="display: flex">
                 <div
                     class={{
-                        "gray": true,
-                        "sticky": true,
-                        "sidenav": true,
-                        "extended": () => document.scrollingElement.scrollHeight > document.scrollingElement.clientHeight
+                        gray: true,
+                        sticky: true,
+                        sidenav: true,
+                        extended: () =>
+                            document.scrollingElement.scrollHeight >
+                            document.scrollingElement.clientHeight
                     }}
                 >
-                    <div class='navtree_container'>
-                        <NavTree tree={docsNavTree} url-bind="url"/>
+                    <div class="navtree_container">
+                        <NavTree tree={docsNavTree} url-bind="url" />
                     </div>
                 </div>
                 <div class="docscontent">
-                    <ContentRouter/>
+                    <ContentRouter />
                 </div>
             </div>
         </MasterLayout>
