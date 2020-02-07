55b47d7993143beb43eb870b082e39d22aa62792
nebojsa-peric <peric.nebojsa@ymail.com>
2020-02-07 13:40:58 +0100

    Fix parse issue

--- packages/cx/src/widgets/form/DateTimeField.js
+++ packages/cx/src/widgets/form/DateTimeField.js
@@ -162,8 +162,8 @@ export class DateTimeField extends Field {
          return date;
       if (this.onParseInput) {
          let result = instance.invoke("onParseInput", date, instance);
-         if (result != undefined)
-            return date;
+         if (result !== undefined)
+            return result;
       }
       date = Culture.getDateTimeCulture().parse(date, {useCurrentDateForDefaults: true});
       return date;
