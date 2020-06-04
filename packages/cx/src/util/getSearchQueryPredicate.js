3194791ca09cd8f577297a086d66a8c863ecc01c
Marko Stijak <mstijak@gmail.com>
2020-06-04 18:25:57 +0200

    Ignore null values in getSearchQueryPredicate

--- packages/cx/src/util/getSearchQueryPredicate.js
+++ packages/cx/src/util/getSearchQueryPredicate.js
@@ -1,20 +1,17 @@
-import { escapeSpecialRegexCharacters } from './escapeSpecialRegexCharacters';
+import { escapeSpecialRegexCharacters } from "./escapeSpecialRegexCharacters";
 
 export function getSearchQueryPredicate(query, options) {
-   if (!query)
-      return () => true;
+   if (!query) return () => true;
 
-   let terms = query.split(' ').filter(Boolean);
-   if (terms.length == 0)
-      return () => true;
+   let terms = query.split(" ").filter(Boolean);
+   if (terms.length == 0) return () => true;
 
-   let regexes = terms.map(w => new RegExp(escapeSpecialRegexCharacters(w), 'gi'));
+   let regexes = terms.map((w) => new RegExp(escapeSpecialRegexCharacters(w), "gi"));
 
    if (regexes.length == 1) {
       let regex = regexes[0];
-      return text => text.match(regex);
+      return (text) => text && text.match(regex);
    }
 
-   return text => regexes.every(re => text.match(re));
+   return (text) => text && regexes.every((re) => text.match(re));
 }
-
