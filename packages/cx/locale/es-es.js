28d652f0efab38828d68dc5866b295f749262473
nebojsa-peric <peric.nebojsa@ymail.com>
2020-07-23 16:37:47 +0200

    add calendar today button text translations

--- packages/cx/locale/es-es.js
+++ packages/cx/locale/es-es.js
@@ -25,7 +25,10 @@ const calendarErrorMessages = {
    minExclusiveErrorText: 'Seleccione una fecha después de {0: d}.',
 };
 // Calendar
-Localization.localize(c, 'cx/widgets/Calendar', calendarErrorMessages);
+Localization.localize(c, 'cx/widgets/Calendar', {
+   ...calendarErrorMessages,
+   todayButtonText: 'Hoy'
+});
 // MonthPicker
 Localization.localize(c, 'cx/widgets/MonthPicker', calendarErrorMessages);
 
@@ -62,6 +65,6 @@ Localization.localize(c, 'cx/widgets/UploadButton', {
 
 // MsgBox
 Localization.localize(c, 'cx/widgets/MsgBox', {
-   yesText:"Sí",
+   yesText: "Sí",
    noText: "No"
 });
--- packages/cx/locale/nl-nl.js
+++ packages/cx/locale/nl-nl.js
@@ -2,73 +2,75 @@ import { Localization } from 'cx/ui';
 
 var c = 'nl-nl';
 
-
 // Field
 Localization.localize(c, 'cx/widgets/Field', {
-    requiredText: 'Dit veld is verplicht.',
-    validatingText: 'Validatie is bezig ...',
-    validationExceptionText: 'Er is een probleem opgetreden bij het valideren van de gegevens. Controleer het logboek voor meer details '
- });
+   requiredText: 'Dit veld is verplicht.',
+   validatingText: 'Validatie is bezig ...',
+   validationExceptionText: 'Er is een probleem opgetreden bij het valideren van de gegevens. Controleer het logboek voor meer details '
+});
 
- // LookupField
- Localization.localize(c, 'cx/widgets/LookupField', {
-    loadingText: 'Bezig met laden ...',
-    requiredText: 'Dit veld is verplicht.',
-    queryErrorText: 'Er is een fout opgetreden bij het weergeven van gegevens.' ,
-    noResultsText: 'Geen resultaten gevonden',
-    minQueryLengthMessageText: 'Voer minimaal {0} tekens in.'
- });
+// LookupField
+Localization.localize(c, 'cx/widgets/LookupField', {
+   loadingText: 'Bezig met laden ...',
+   requiredText: 'Dit veld is verplicht.',
+   queryErrorText: 'Er is een fout opgetreden bij het weergeven van gegevens.',
+   noResultsText: 'Geen resultaten gevonden',
+   minQueryLengthMessageText: 'Voer minimaal {0} tekens in.'
+});
 
- // In common for Calendar and MonthPicker
- const calendarErrorMessages = {
-    requiredText: 'Dit veld is verplicht.',
-    maxValueErrorText: 'De geselecteerde datum is later dan de laatst toegestane datum {0: d}',
-    maxExclusiveErrorText: 'De geselecteerde datum moet vóór {0: d}' ,
-    minValueErrorText: 'De geselecteerde datum is eerder dan {0: d}',
-    minExclusiveErrorText: 'De geselecteerde datum moet later zijn dan {0: d}',
- };
+// In common for Calendar and MonthPicker
+const calendarErrorMessages = {
+   requiredText: 'Dit veld is verplicht.',
+   maxValueErrorText: 'De geselecteerde datum is later dan de laatst toegestane datum {0: d}',
+   maxExclusiveErrorText: 'De geselecteerde datum moet vóór {0: d}',
+   minValueErrorText: 'De geselecteerde datum is eerder dan {0: d}',
+   minExclusiveErrorText: 'De geselecteerde datum moet later zijn dan {0: d}',
+};
 
- // Calendar
- Localization.localize(c, 'cx/widgets/Calendar', calendarErrorMessages);
- // MonthPicker
- Localization.localize(c, 'cx/widgets/MonthPicker', calendarErrorMessages);
+// Calendar
+Localization.localize(c, 'cx/widgets/Calendar', {
+   ...calendarErrorMessages,
+   todayButtonText: 'Vandaag'
+});
+// MonthPicker
+Localization.localize(c, 'cx/widgets/MonthPicker', calendarErrorMessages);
 
- // In common for DateField and MonthField
- const dateFieldErrorMessages = {
-    ...calendarErrorMessages,
-    inputErrorText: 'Ongeldige datum.'
- };
+// In common for DateField and MonthField
+const dateFieldErrorMessages = {
+   ...calendarErrorMessages,
+   inputErrorText: 'Ongeldige datum.'
+};
 
- // MonthField
- Localization.localize(c, 'cx/widgets/MonthField', dateFieldErrorMessages);
- // DateField
- Localization.localize(c, 'cx/widgets/DateField', dateFieldErrorMessages);
+// MonthField
+Localization.localize(c, 'cx/widgets/MonthField', dateFieldErrorMessages);
+// DateField
+Localization.localize(c, 'cx/widgets/DateField', dateFieldErrorMessages);
 
- // NumberField
- Localization.localize(c, 'cx/widgets/NumberField', {
-    requiredText: 'Dit veld is verplicht.',
-    maxValueErrorText: 'Voer {0} of minder in.',
-    maxExclusiveErrorText: 'Voer een nummer in dat kleiner is dan {0}.',
-    minValueErrorText:'Voer {0} of meer in.',
-    minExclusiveErrorText: 'Voer een getal in dat groter is dan {0}.',
-    inputErrorText: 'Ongeldig nummer.'
- });
+// NumberField
+Localization.localize(c, 'cx/widgets/NumberField', {
+   requiredText: 'Dit veld is verplicht.',
+   maxValueErrorText: 'Voer {0} of minder in.',
+   maxExclusiveErrorText: 'Voer een nummer in dat kleiner is dan {0}.',
+   minValueErrorText: 'Voer {0} of meer in.',
+   minExclusiveErrorText: 'Voer een getal in dat groter is dan {0}.',
+   inputErrorText: 'Ongeldig nummer.'
+});
 
- // TextField
- Localization.localize(c, 'cx/widgets/TextField', {
-    requiredText: 'Dit veld is verplicht.',
-    validationErrorText: 'De ingevoerde waarde is ongeldig.',
-    minLengthValidationErrorText: 'Vul {[{0} - {1}]} extra karakters in.',
-    maxLengthValidationErrorText: 'Gebruik {0} tekens of minder.'
- });
+// TextField
+Localization.localize(c, 'cx/widgets/TextField', {
+   requiredText: 'Dit veld is verplicht.',
+   validationErrorText: 'De ingevoerde waarde is ongeldig.',
+   minLengthValidationErrorText: 'Vul {[{0} - {1}]} extra karakters in.',
+   maxLengthValidationErrorText: 'Gebruik {0} tekens of minder.'
+});
 
- // UploadButton
- Localization.localize(c, 'cx/widgets/UploadButton', {
-    validationErrorText: 'Upload is bezig.'
- });
+// UploadButton
+Localization.localize(c, 'cx/widgets/UploadButton', {
+   validationErrorText: 'Upload is bezig.'
+});
 
- // MsgBox
+// MsgBox
 Localization.localize(c, 'cx/widgets/MsgBox', {
-   yesText:"Ja",
+   yesText: "Ja",
    noText: "Nee"
 });
