676811a62784be3cd2f1580dd3c91f066a656eb7
Marko Stijak <marko.stijak@codaxy.com>
2020-10-02 15:15:53 +0200

    Flyweight tooltips (#696)
    * Add Restate's cacheKey to typings
    
    * ...
    
    * ...
    
    * ...

---
+++ packages/cx/src/widgets/overlay/FlyweightTooltipTracker.d.ts
@@ -0,0 +1,7 @@
+import { Instance } from "../../ui/Instance";
+
+export interface FlyweightTooltipTrackerProps extends Cx.WidgetProps {
+   onGetTooltip(element: DOMElement, instance: Instance) : Cx.Config;
+}
+
+export class FlyweightTooltipTracker extends Cx.Widget<FlyweightTooltipTrackerProps>;
