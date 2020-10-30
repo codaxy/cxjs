e1ccec11c6073f368ccdd572236e179bd4d2b092
Marko Stijak <marko.stijak@codaxy.com>
2020-10-30 16:28:21 +0100

    Fix bad typing for updateTree

--- packages/cx/src/data/ops/updateTree.d.ts
+++ packages/cx/src/data/ops/updateTree.d.ts
@@ -1,12 +1,7 @@
-interface TreeItem {
-    [key: string]: any,
-    [childrenField: string]: Array<TreeItem>
-}
-
 export function updateTree(
-    array: Array<TreeItem>,
-    updateCallback: (item?: TreeItem, index?: number) => TreeItem,
-    itemFilter?: (item?: TreeItem, index?: number) => boolean,
-    childrenField?: string,
-    removeFilter?: (item?: TreeItem, index?: number) => boolean,
-): Array<TreeItem>;
+   array: Cx.Record[],
+   updateCallback: (item?: Cx.Record, index?: number) => Cx.Record,
+   itemFilter?: (item?: Cx.Record, index?: number) => boolean,
+   childrenField?: string,
+   removeFilter?: (item?: Cx.Record, index?: number) => boolean
+): Cx.Record[];
