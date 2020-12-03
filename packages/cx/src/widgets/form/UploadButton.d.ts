ad2df78bfd6fde2df147296a65ff9f67605fbafa
Danijela Cvisic <d.cvisic91@gmail.com>
2020-12-03 09:26:13 +0100

    Add accept attribute to filter file types in file expolorer #726 (#727)
    * Add accept attribute on UploadButton
    
    * Fixes

--- packages/cx/src/widgets/form/UploadButton.d.ts
+++ packages/cx/src/widgets/form/UploadButton.d.ts
@@ -2,23 +2,26 @@ import * as Cx from '../../core';
 import { FieldProps } from './Field';
 
 interface UploadButtonProps extends FieldProps {
- 
-   /** Text description. */
-   text?: Cx.StringProp,
 
-   url?: Cx.StringProp, 
+    /** Text description. */
+    text?: Cx.StringProp,
 
-   /** Base CSS class to be applied to the element. Default is 'uploadbutton'. */
-   baseClass?: string,
-   
-   /** Defaults to `false`. Set to `true` to enable multiple selection. */
-   multiple?: boolean,
-   
-   method?: string,
-   uploadInProgressText?: string,
+    url?: Cx.StringProp,
 
-   /** Defaults to `false`. Set to `true` to abort uploads if the button is destroyed (unmounted). */
-   abortOnDestroy: boolean
+    /** Base CSS class to be applied to the element. Default is 'uploadbutton'. */
+    baseClass?: string,
+
+    /** Defaults to `false`. Set to `true` to enable multiple selection. */
+    multiple?: boolean,
+
+    method?: string,
+    uploadInProgressText?: string,
+
+    /** Defines file types that are accepted for upload. */
+    accept?: Cx.StringProp,
+
+    /** Defaults to `false`. Set to `true` to abort uploads if the button is destroyed (unmounted). */
+    abortOnDestroy: boolean
 }
 
-export class UploadButton extends Cx.Widget<UploadButtonProps> {}
+export class UploadButton extends Cx.Widget<UploadButtonProps> { }
