03c41c41833b8c907fef5c073b3dc4c74a45fbb8
DESKTOP-AG59P0V\Uzelac <vladimir.uzelac@codaxy.com>
2020-12-25 12:41:38 +0100

    ...

--- docs/content/examples/grid/CellEditing.js
+++ docs/content/examples/grid/CellEditing.js
@@ -4,6 +4,7 @@ import {casual} from '../data/casual';
 import {CodeSplit} from "../../../components/CodeSplit";
 import {Md} from "../../../components/Md";
 import {CodeSnippet} from "../../../components/CodeSnippet";
+import { KeyCode } from "cx/util";
 
 class PageController extends Controller {
     onInit() {
@@ -40,6 +41,7 @@ export const CellEditing = (
 
                 <Grid
                     cellEditable
+                    onCellKeyDown={(e, {store}) => { if(e.keyCode == KeyCode.delete) { }}}
                     onCellEdited={(change, record) => {
                         console.log(change, record);
                     }}
