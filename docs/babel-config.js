686859260976a3caad033e9a57b0d6379023e4da
Marko Stijak <mstijak@gmail.com>
2021-02-23 08:37:55 +0100

    Package upgrades

--- docs/babel-config.js
+++ docs/babel-config.js
@@ -26,14 +26,14 @@ module.exports = function (options) {
                 trimWhitespace: true,
                 trimWhitespaceExceptions: ['Md', 'CodeSnippet', 'CodeSplit']
             }],
-            ["@babel/transform-react-jsx", {"pragma": 'VDOM.createElement'}],
+            ["@babel/transform-react-jsx", { "pragma": 'VDOM.createElement' }],
             //'@babel/external-helpers',
-            '@babel/proposal-object-rest-spread',
+            //'@babel/proposal-object-rest-spread',
             "@babel/proposal-function-bind",
-            '@babel/transform-parameters',
-            '@babel/syntax-dynamic-import',
-            "@babel/plugin-proposal-export-namespace-from",
-            isProduction && ["transform-cx-imports", {useSrc: true}],
+            // '@babel/transform-parameters',
+            // '@babel/syntax-dynamic-import',
+            // "@babel/plugin-proposal-export-namespace-from",
+            isProduction && ["transform-cx-imports", { useSrc: true }],
         ].filter(Boolean)
     }
 };
