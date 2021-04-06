07d39a6fd16a7e579f6916319fff4c17b9a0d34c
Marko Stijak <mstijak@gmail.com>
2021-04-06 14:13:36 +0200

    Fix null refrence bug in mobile layout

--- misc/layout/index.js
+++ misc/layout/index.js
@@ -14,11 +14,21 @@ const TopLinks = ({ topLinks, mod, alternativeLinks }) => (
       <div>
          {Object.keys(topLinks || {}).map((url) => (
             <cx>
-               <Link href={url} text={topLinks[url]} url-bind="url" match="subroute" mod={mod}
-                  active={!alternativeLinks[url] ? null : computable("url", currentUrl => {
-                     let links = alternativeLinks[url];
-                     return isArray(links) && links.some(link => currentUrl.startsWith(link)) ? true : null;
-                  })} />
+               <Link
+                  href={url}
+                  text={topLinks[url]}
+                  url-bind="url"
+                  match="subroute"
+                  mod={mod}
+                  active={
+                     alternativeLinks &&
+                     alternativeLinks[url] &&
+                     computable("url", (currentUrl) => {
+                        let links = alternativeLinks[url];
+                        return isArray(links) && links.some((link) => currentUrl.startsWith(link)) ? true : null;
+                     })
+                  }
+               />
             </cx>
          ))}
       </div>
