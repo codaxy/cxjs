a4c08038cd5df4bdcac5bc7160cc4dd279ac9230
Marko Stijak <marko.stijak@codaxy.com>
2021-06-07 11:00:40 +0200

    Algolia search fixes

--- docs/components/DocSearch.js
+++ docs/components/DocSearch.js
@@ -1,28 +1,27 @@
-import {VDOM} from 'cx/ui';
+import { VDOM } from "cx/ui";
+import { Icon } from "cx/widgets";
 
-export class DocSearch extends VDOM.Component
-{
-    render() {
-        return <input
-            type="text"
-            id="docsearch"
-            placeholder="Search..."
-            className="docsearch"
-        />
-    }
+export class DocSearch extends VDOM.Component {
+   render() {
+      return (
+         <div className="cxb-docsearch">
+            <input type="text" id="docsearch" placeholder="Search..." className="cxe-docsearch-input" />
+            {Icon.render("search", { className: "cxe-docsearch-icon" })}
+         </div>
+      );
+   }
 
-    componentDidMount() {
-        if (typeof docsearch == "function")
-            try {
+   componentDidMount() {
+      if (typeof docsearch == "function")
+         try {
             docsearch({
-                apiKey: 'b77ab797ddcee40f03751aeb694168ed',
-                indexName: 'cxjs',
-                inputSelector: '#docsearch',
-                debug: false // Set debug to true if you want to inspect the dropdown
+               apiKey: "b77ab797ddcee40f03751aeb694168ed",
+               indexName: "cxjs",
+               inputSelector: "#docsearch",
+               debug: false, // Set debug to true if you want to inspect the dropdown
             });
-            } 
-        catch (err) {
+         } catch (err) {
             console.log("DocSearch init error.", err);
-        }
-    }
+         }
+   }
 }
--- packages/cx-cli/index.js
+++ packages/cx-cli/index.js
@@ -1,58 +1,69 @@
 #! /usr/bin/env node
 
-let program = require('commander'),
-   pkg = require('./package.json'),
-   spawn = require('cross-spawn'),
-   chalk = require('chalk');
+let program = require("commander"),
+   pkg = require("./package.json"),
+   spawn = require("cross-spawn"),
+   chalk = require("chalk"),
+   cliSelect = require("cli-select");
 
-let commands = require('./commands');
+let commands = require("./commands");
 
 program
    .version(pkg.version)
-   .option('-i, install', 'Install necessary packages')
-   .option('scaffold', 'Scaffold application structure')
-   .option('-s, start', 'Start the development server')
-   .option('-b, build', 'Make a new production build')
-   .option('create', 'Create a new project directory with the basic app structure')
-   .option('--yarn', 'Use yarn instead of npm')
-   .option('add, route', 'Add new route folder')
+   .option("-i, install", "Install necessary packages")
+   .option("scaffold", "Scaffold application structure")
+   .option("-s, start", "Start the development server")
+   .option("-b, build", "Make a new production build")
+   .option("create", "Create a new project directory with the basic app structure")
+   .option("--yarn", "Use yarn instead of npm")
+   .option("add, route", "Add new route folder")
    .parse(process.argv);
 
 let useYarn = !!program.yarn;
 
-console.log('Cx CLI v' + pkg.version);
+console.log("Cx CLI v" + pkg.version);
 
-if (program.scaffold)
-   commands.scaffold(useYarn);
+if (program.scaffold) commands.scaffold(useYarn);
 else if (program.create) {
    let projectName = program.args[0];
    if (!projectName) {
-      console.error('Please specify the project directory:');
+      console.error("Please specify the project directory:");
       console.log(
-         `  ${chalk.cyan(useYarn ? 'yarn create cx-app' : 'create-cx-app')} ${chalk.green('<project-directory>')}`
+         `  ${chalk.cyan(useYarn ? "yarn create cx-app" : "create-cx-app")} ${chalk.green("<project-directory>")}`
       );
       console.log();
       return;
    }
    commands.create(projectName, useYarn);
-}
-else if (program.install) {
+} else if (program.install) {
    commands.install(useYarn);
-}
-else if (program.start || program.open) {
-   console.log('npm start');
-   return spawn.sync('npm', ['start'], {stdio: 'inherit'});
-}
-else if (program.build) {
-   console.log('npm run build');
-   return spawn.sync('npm', ['run', 'build'], {stdio: 'inherit'});
-}
-else if (program.route) {
+} else if (program.start || program.open) {
+   console.log("npm start");
+   return spawn.sync("npm", ["start"], { stdio: "inherit" });
+} else if (program.build) {
+   console.log("npm run build");
+   return spawn.sync("npm", ["run", "build"], { stdio: "inherit" });
+} else if (program.route) {
    let routeName = program.args[0];
    console.log();
    if (!routeName) {
-      console.log("Syntax error: missing route name. Correct syntax: cx add route route_name");
+      console.log("Syntax error: missing route name. Correct syntax: cx add route <route_name>.");
       return;
    }
    commands.addRoute(routeName);
 }
+
+async function pickYarnOrNpm() {
+   console.log("Please select which package manager you wish to use:");
+   let option = await cliSelect({
+      values: {
+         npm: "NPM",
+         yarn: "Yarn",
+      },
+      defaultValue: 0,
+      valueRenderer: (value, selected) => (selected ? chalk.yellow(value) : value),
+   });
+   console.log("Selected option is ", option);
+}
+
+pickYarnOrNpm();
