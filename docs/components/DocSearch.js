b37e4559e4872432ab5a62c153a9eeda786ddf51
Marko Stijak <mstijak@gmail.com>
2021-07-08 09:07:51 +0200

    Fix search in dev mode

--- docs/components/DocSearch.js
+++ docs/components/DocSearch.js
@@ -2,38 +2,51 @@ import { VDOM } from "cx/ui";
 import { Icon } from "cx/widgets";
 
 export class DocSearch extends VDOM.Component {
-   render() {
-      return (
-         <div className="cxb-docsearch">
-            <input type="text" id="docsearch" placeholder="Search..." className="cxe-docsearch-input" />
-            {Icon.render("search", { className: "cxe-docsearch-icon" })}
-         </div>
-      );
-   }
+    render() {
+        return (
+            <div className="cxb-docsearch">
+                <input type="text" id="docsearch" placeholder="Search..." className="cxe-docsearch-input" />
+                {Icon.render("search", { className: "cxe-docsearch-icon" })}
+            </div>
+        );
+    }
 
-   delayedInit() {
-      if (this.initialized) return;
-      if (typeof docsearch != "function") return;
-      clearInterval(this.delayedInitTimer);
-      this.initialized = true;
-      try {
-         docsearch({
-            apiKey: "b77ab797ddcee40f03751aeb694168ed",
-            indexName: "cxjs",
-            inputSelector: "#docsearch",
-            debug: false, // Set debug to true if you want to inspect the dropdown
-         });
-      } catch (err) {
-         console.log("DocSearch init error.", err);
-      }
-   }
+    delayedInit() {
+        if (this.initialized) return;
+        if (typeof docsearch != "function") return;
+        clearInterval(this.delayedInitTimer);
+        this.initialized = true;
+        try {
+            docsearch({
+                apiKey: "b77ab797ddcee40f03751aeb694168ed",
+                indexName: "cxjs",
+                inputSelector: "#docsearch",
+                debug: false, // Set debug to true if you want to inspect the dropdown,
 
-   componentDidMount() {
-      this.delayedInit();
-      this.delayedInitTimer = setInterval(() => this.delayedInit(), 100);
-   }
+                // #if development
+                transformData: function (hits) {
+                    let address = `${location.protocol}//${location.hostname}`;
+                    if (location.port)
+                        address += ':' + location.port;
 
-   componentWillUnmount() {
-      clearInterval(this.delayedInitTimer);
-   }
+                    return hits.map(hit => ({
+                        ...hit,
+                        url: hit.url.replace("https://docs.cxjs.io", address)
+                    }))
+                }
+                // #end
+            });
+        } catch (err) {
+            console.log("DocSearch init error.", err);
+        }
+    }
+
+    componentDidMount() {
+        this.delayedInit();
+        this.delayedInitTimer = setInterval(() => this.delayedInit(), 100);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.delayedInitTimer);
+    }
 }
