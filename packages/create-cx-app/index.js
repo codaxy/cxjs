d8ff370cc5e274e0baf919f019af6f5311115173
Marko Stijak <mstijak@gmail.com>
2021-07-19 12:16:47 +0200

    Fix for create-cx-app CLI

--- packages/create-cx-app/index.js
+++ packages/create-cx-app/index.js
@@ -1,6 +1,8 @@
-let { createNewApp } = require("cx-cli/commands");
+#! /usr/bin/env node
 
-let projectName = process.argv[2];
+const { createNewApp } = require("cx-cli/commands");
+
+const projectName = process.argv[2];
 
 if (!projectName) {
    console.error('Please specify the project name.');
