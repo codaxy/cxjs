54c975bdb56165318d33d0ad624c9f1758334760
Marko Stijak <mstijak@gmail.com>
2021-08-25 16:02:02 +0200

    Wrong nesting in SCSS files

--- packages/cx/src/widgets/FlexBox.d.ts
+++ packages/cx/src/widgets/FlexBox.d.ts
@@ -1,66 +1,65 @@
-import * as Cx from '../core';
+import * as Cx from "../core";
 
 interface FlexBoxProps extends Cx.StyledContainerProps {
-
    /** Base CSS class. Default is `flexbox`. */
-   baseClass?: string,
+   baseClass?: string;
 
    /** Flex direction. Default is `row`.  */
-   direction?: string,
+   direction?: string;
 
    /**
     * Add spacing between items by applying a margin to children.
     * Allowed values are `xsmall`, `small`, `medium`, `large` and `xlarge`.
     * Value `true` is equivalent to `medium`.
     */
-   spacing?: string | boolean,
+   spacing?: string | boolean;
 
    /**
     * Add horizontal spacing between items by applying a margin to children.
     * Allowed values are `xsmall`, `small`, `medium`, `large` and `xlarge`.
     * Value `true` is equivalent to `medium`. \
     */
-   hspacing?: boolean | string,
+   hspacing?: boolean | string;
 
    /**
     * Add vertical spacing between items by applying a margin to children.
     * Allowed values are xsmall, small, medium, large and xlarge.
     * Value true is equivalent to medium.
     */
-   vspacing?: boolean | string,
+   vspacing?: boolean | string;
 
-   styled?: boolean,
+   styled?: boolean;
 
    /**
     * Add padding to the box. Allowed values are `xsmall`, `small`, `medium`, `large` and `xlarge`.
     *  Value `true` is equivalent to `medium`.
     */
-   pad?: boolean | string,
-   padding?: boolean | string,
+   pad?: boolean | string;
+   padding?: boolean | string;
 
    /**
     * Add horizontal padding to the box. Allowed values are `xsmall`, `small`, `medium`, `large` and `xlarge`.
     *  Value `true` is equivalent to `medium`.
     */
-   hpad?: boolean | string,
-   hpadding?: boolean | string,
+   hpad?: boolean | string;
+   hpadding?: boolean | string;
 
    /**
     * Add vertical padding to the box. Allowed values are `xsmall`, `small`, `medium`, `large` and `xlarge`.
     *  Value `true` is equivalent to `medium`.
     */
-   vpad?: boolean | string,
-   vpadding?: boolean | string,
+   vpad?: boolean | string;
+   vpadding?: boolean | string;
 
-   wrap?: boolean,
-   align?: 'start' | 'center' | 'end' | 'baseline',
-   justify?: 'start' | 'center' | 'end' | 'space-between' | 'space-around' | 'space-evenly',
-   target?: string,
+   wrap?: boolean;
+   align?: "start" | "center" | "end" | "baseline" | "stretch";
+   justify?: "start" | "center" | "end" | "space-between" | "space-around" | "space-evenly";
+   target?: string;
 
    /**
     * Set to true to add overflow styles required for deeply nested flexbox calculations.
     */
-   nested?: boolean
+   nested?: boolean;
 }
 
 export class FlexBox extends Cx.Widget<FlexBoxProps> {}
