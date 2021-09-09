8bbee01147048cbc02b3daa44bb8ef573f67088d
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2021-09-09 13:02:58 +0200

    Fixed #875

--- docs/content/svg/NonOverlappingRects.js
+++ docs/content/svg/NonOverlappingRects.js
@@ -1,4 +1,4 @@
-import { HtmlElement } from 'cx/widgets';
+import { Content, HtmlElement, Tab } from 'cx/widgets';
 import { Controller, Repeater } from "cx/ui";
 import { Svg, Rectangle, ClipRect, Ellipse, Text, NonOverlappingRect, NonOverlappingRectGroup } from 'cx/svg';
 import { Md } from '../../components/Md';
@@ -71,52 +71,59 @@ export const NonOverlappingRects = <cx>
 
 
 
-            <CodeSnippet putInto="code">{`
-            class PageController extends Controller {
-                onInit() {
-                    this.store.set(
-                        "$page.data",
-                        Array.from({ length: 30 }, (_, i) => ({
-                            x: 100 + Math.random() * 300,
-                            y: Math.random() * 300
-                        }))
-                    );
+            <Content name="code">
+                <div>
+                    <Tab value-bind="$page.code.tab" tab="controller" mod="code"><code>Controller</code></Tab>
+                    <Tab value-bind="$page.code.tab" tab="chart" mod="code" default><code>Chart</code></Tab>
+                </div>
+                <CodeSnippet visible-expr="{$page.code.tab}=='controller'" fiddle="dAzljo79">{`
+                class PageController extends Controller {
+                    onInit() {
+                        this.store.set(
+                            "$page.data",
+                            Array.from({ length: 30 }, (_, i) => ({
+                                x: 100 + Math.random() * 300,
+                                y: Math.random() * 300
+                            }))
+                        );
+                    }
                 }
-            }
-            ...
-            <div class="widgets" controller={PageController}>
-                <Svg style="width:500px; height:450px;">
-                    <Chart
-                        offset="50 -20 -40 130"
-                        axes={{
-                            x: { type: NumericAxis, snapToTicks: 1 },
-                            y: { type: NumericAxis, vertical: true, snapToTicks: 1 }
-                        }}
-                    >
-                        <NonOverlappingRectGroup>
-                            <Gridlines />
-                            <Repeater records:bind="$page.data" recordName="$point">
-                                <Marker
-                                    colorIndex={0}
-                                    size={10}
-                                    x:bind="$point.x"
-                                    y:bind="$point.y"
-                                    style={{ fillOpacity: 0.5 }}
-                                    draggableX
-                                    draggableY
-                                >
-                                    <NonOverlappingRect offset="-15 25 0 -25" anchors="0 0.5 0 0.5">
-                                        <Rectangle style="fill: white; stroke: red; stroke-width: 0.5" anchors="0 1 1 0">
-                                            <Text value-tpl="{$point.x:n;0}, {$point.y:n;0}" textAnchor="middle" style="font-size: 10px" dy="0.35em" />
-                                        </Rectangle>
-                                    </NonOverlappingRect>
-                                </Marker>
-                            </Repeater>
-                        </NonOverlappingRectGroup>
-                    </Chart>
-                </Svg>
-            </div>
             `}</CodeSnippet>
+            <CodeSnippet visible-expr="{$page.code.tab}=='chart'" fiddle="dAzljo79">{`
+                <div class="widgets" controller={PageController}>
+                    <Svg style="width:500px; height:450px;">
+                        <Chart
+                            offset="50 -20 -40 130"
+                            axes={{
+                                x: { type: NumericAxis, snapToTicks: 1 },
+                                y: { type: NumericAxis, vertical: true, snapToTicks: 1 }
+                            }}
+                        >
+                            <NonOverlappingRectGroup>
+                                <Gridlines />
+                                <Repeater records:bind="$page.data" recordName="$point">
+                                    <Marker
+                                        colorIndex={0}
+                                        size={10}
+                                        x:bind="$point.x"
+                                        y:bind="$point.y"
+                                        style={{ fillOpacity: 0.5 }}
+                                        draggableX
+                                        draggableY
+                                    >
+                                        <NonOverlappingRect offset="-15 25 0 -25" anchors="0 0.5 0 0.5">
+                                            <Rectangle style="fill: white; stroke: red; stroke-width: 0.5" anchors="0 1 1 0">
+                                                <Text value-tpl="{$point.x:n;0}, {$point.y:n;0}" textAnchor="middle" style="font-size: 10px" dy="0.35em" />
+                                            </Rectangle>
+                                        </NonOverlappingRect>
+                                    </Marker>
+                                </Repeater>
+                            </NonOverlappingRectGroup>
+                        </Chart>
+                    </Svg>
+                </div>
+                `}</CodeSnippet>
+            </Content>
         </CodeSplit>
 
         ## Configuration
