a131ba42890e6b610cdb6c3e8c8b4a9b6763e372
Marko Stijak <mstijak@gmail.com>
2021-09-18 21:55:43 +0200

    Fix github stars

--- misc/components/GitHubStarCount.js
+++ misc/components/GitHubStarCount.js
@@ -1,18 +1,31 @@
-import {VDOM} from "cx/ui";
+import { VDOM } from "cx/ui";
 
 export class GitHubStarCount extends VDOM.Component {
    render() {
-      return <div className="master_githubstars"><a
-         className="github-button"
-         href="https://github.com/codaxy/cxjs"
-         data-size="large"
-         data-show-count="true"
-         aria-label="Star codaxy/cxjs on GitHub"
-      >Star</a>
-      </div>
+      return (
+         <div className="master_githubstars">
+            <a
+               className="github-button"
+               href="https://github.com/codaxy/cxjs"
+               data-size="large"
+               data-show-count="true"
+               aria-label="Star codaxy/cxjs on GitHub"
+            >
+               Star
+            </a>
+         </div>
+      );
    }
 
    shouldComponentUpdate() {
       return false;
+   }
+
+   componentDidMount() {
+      let script = document.createElement("script");
+      script.async = true;
+      script.defer = true;
+      script.src = "https://buttons.github.io/buttons.js";
+      document.body.appendChild(script);
    }
 }
