e443c98c89f4a25169bb5c410adef6b608718d64
Nebojsa <peric.nebojsa@ymail.com>
2022-03-08 11:11:41 +0100

    tab change confirmatin

--- packages/cx/src/ui/app/History.js
+++ packages/cx/src/ui/app/History.js
@@ -1,6 +1,6 @@
-import {Url} from './Url';
-import {batchUpdatesAndNotify} from '../batchUpdates';
-import {SubscriberList} from '../../util/SubscriberList';
+import { Url } from "./Url";
+import { batchUpdatesAndNotify } from "../batchUpdates";
+import { SubscriberList } from "../../util/SubscriberList";
 
 let last = 0,
    next = 1,
@@ -10,16 +10,14 @@ let last = 0,
    navigateConfirmationCallback = null,
    permanentNavigateConfirmation = false;
 
-
 export class History {
-
    static connect(store, urlBinding, hashBinding) {
       this.store = store;
       this.urlBinding = urlBinding;
       this.hashBinding = hashBinding;
       this.updateStore();
       window.onpopstate = () => {
-         this.updateStore()
+         this.updateStore();
       };
    }
 
@@ -40,21 +38,25 @@ export class History {
       permanentNavigateConfirmation = permanent;
    }
 
+   static confirm(continueCallback, state) {
+      if (!navigateConfirmationCallback) {
+         continueCallback();
+         return;
+      }
+
+      let result = navigateConfirmationCallback(state);
+      Promise.resolve(result).then((value) => {
+         if (value) {
+            if (!permanentNavigateConfirmation) navigateConfirmationCallback = null;
+            continueCallback();
+         }
+      });
+   }
+
    static confirmAndNavigate(state, title, url, replace) {
-      if (!navigateConfirmationCallback)
-         return this.navigate(state, title, url, replace);
-
-      let result = navigateConfirmationCallback(url);
-
-      Promise
-         .resolve(result)
-         .then(value => {
-            if (value) {
-               if (!permanentNavigateConfirmation)
-                  navigateConfirmationCallback = null;
-               this.navigate(state, title, url, replace);
-            }
-         });
+      this.confirm(() => {
+         this.navigate(state, title, url, replace);
+      }, url);
       return false;
    }
 
@@ -66,50 +68,51 @@ export class History {
          return true;
       }
 
-      let transition, changed = false;
-      batchUpdatesAndNotify(() => {
-         changed = this.updateStore(url);
-         if (changed)
-            transitions[++last] = transition = {
-               url,
-               state,
-               title,
-               replace
-            }
-      }, () => {
-         if (transition)
-            transition.completed = true;
+      let transition,
+         changed = false;
+      batchUpdatesAndNotify(
+         () => {
+            changed = this.updateStore(url);
+            if (changed)
+               transitions[++last] = transition = {
+                  url,
+                  state,
+                  title,
+                  replace,
+               };
+         },
+         () => {
+            if (transition) transition.completed = true;
 
-         //update history once the page is rendered and the title is set
-         while (transitions[next] && transitions[next].completed) {
-            let tr = transitions[next];
-            delete transitions[next];
-            next++;
-            let op = tr.replace ? "replaceState" : "pushState";
-            window.history[op](tr.state, tr.title, tr.url);
-            if (subscribers)
-               subscribers.notify(tr.url, op);
+            //update history once the page is rendered and the title is set
+            while (transitions[next] && transitions[next].completed) {
+               let tr = transitions[next];
+               delete transitions[next];
+               next++;
+               let op = tr.replace ? "replaceState" : "pushState";
+               window.history[op](tr.state, tr.title, tr.url);
+               if (subscribers) subscribers.notify(tr.url, op);
+            }
          }
-      });
+      );
 
       return changed;
    }
 
    static updateStore(href) {
-      let url = Url.unresolve(href || document.location.href), hash = null;
-      let hashIndex = url.indexOf('#');
+      let url = Url.unresolve(href || document.location.href),
+         hash = null;
+      let hashIndex = url.indexOf("#");
       if (hashIndex !== -1) {
          hash = url.substring(hashIndex);
          url = url.substring(0, hashIndex);
       }
-      if (this.hashBinding)
-         this.store.set(this.hashBinding, hash);
+      if (this.hashBinding) this.store.set(this.hashBinding, hash);
       return this.store.set(this.urlBinding, url);
    }
 
    static subscribe(callback) {
-      if (!subscribers)
-         subscribers = new SubscriberList();
-      return subscribers.subscribe(callback)
+      if (!subscribers) subscribers = new SubscriberList();
+      return subscribers.subscribe(callback);
    }
 }
