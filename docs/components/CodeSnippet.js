7f4dabc7335ae4c60b367f78355c8eb306788cdb
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2022-03-11 13:51:56 +0100

    Fix Fiddle button layout

--- docs/components/CodeSnippet.js
+++ docs/components/CodeSnippet.js
@@ -1,63 +1,60 @@
-import { HtmlElement } from 'cx/widgets';
-import { Widget, VDOM } from 'cx/ui';
-import {removeCommonIndent} from './removeCommonIndent';
+import { HtmlElement } from "cx/widgets";
+import { Widget, VDOM } from "cx/ui";
+import { removeCommonIndent } from "./removeCommonIndent";
 
-import {highlight} from "illuminate-js";
+import { highlight } from "illuminate-js";
 
 function lazyHighlight(text, lang) {
-    var cache;
-    return () => {
-        if (typeof cache == 'undefined') {
-            var withoutIndent = removeCommonIndent(text);
-            cache = highlight(withoutIndent, lang);
-        }
-        return cache;
-    }
+   var cache;
+   return () => {
+      if (typeof cache == "undefined") {
+         var withoutIndent = removeCommonIndent(text);
+         cache = highlight(withoutIndent, lang);
+      }
+      return cache;
+   };
 }
 
 export class CodeSnippet extends HtmlElement {
+   render(context, instance, key) {
+      let { data } = instance,
+         fiddleLink;
 
-    render(context, instance, key) {
-        let {data} = instance,
-            fiddleLink;
+      if (this.fiddle) {
+         fiddleLink = (
+            <a
+               href={`https://fiddle.cxjs.io/?f=${this.fiddle}`}
+               className={this.CSS.element(this.baseClass, "link")}
+               target="_blank"
+            >
+               <i className="fa fa-external-link"></i>
+               Cx Fiddle
+            </a>
+         );
+      }
 
+      return (
+         <div key={key} className={data.classNames}>
+            <pre className={`language-${this.lang}`}>{this.renderChildren(context, instance)}</pre>
+            {fiddleLink}
+         </div>
+      );
+   }
 
-        if (this.fiddle) {
-            fiddleLink = (
-                <a
-                    href={`https://fiddle.cxjs.io/?f=${this.fiddle}`}
-                    className={this.CSS.element(this.baseClass, 'link')}
-                    target="_blank"
-                >
-                    Cx Fiddle
-                </a>
-            )
-        }
+   add(text) {
+      if (typeof text != "string") return super.add(...arguments);
 
-        return (
-            <div key={key} className={data.classNames}>
-                <pre className={`language-${this.lang}`}>
-                    {this.renderChildren(context, instance)}
-                </pre>
-                {fiddleLink}
-            </div>);
-    }
-
-    add(text) {
-        if (typeof text != 'string')
-            return super.add(...arguments);
-
-        super.add({
-            type: HtmlElement,
-            innerHtml: lazyHighlight(text, this.lang),
-            tag: 'code',
-            class: `language-${this.lang}`
-        });
-    }
+      super.add({
+         type: HtmlElement,
+         innerHtml: lazyHighlight(text, this.lang),
+         tag: "code",
+         class: `language-${this.lang}`,
+      });
+   }
 }
 
 CodeSnippet.prototype.plainText = true;
-CodeSnippet.prototype.tag = 'pre';
-CodeSnippet.prototype.lang = 'jsx';
-CodeSnippet.prototype.baseClass = 'codesnippet';
-CodeSnippet.prototype.CSS = 'dx';
+CodeSnippet.prototype.tag = "pre";
+CodeSnippet.prototype.lang = "jsx";
+CodeSnippet.prototype.baseClass = "codesnippet";
+CodeSnippet.prototype.CSS = "dx";
