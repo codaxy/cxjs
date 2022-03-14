3f6ea9ce75159cb1309cc40ca0936c3561e8f878
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2022-03-14 22:39:40 +0100

    Concepts > Outer Layouts: Tabs added

--- docs/content/concepts/OuterLayouts.js
+++ docs/content/concepts/OuterLayouts.js
@@ -1,4 +1,4 @@
-import { Button, HtmlElement, TextField, Checkbox, Select, LabeledContainer, PureContainer, Menu } from 'cx/widgets';
+import { Button, HtmlElement, TextField, Checkbox, Select, LabeledContainer, PureContainer, Menu, Tab } from 'cx/widgets';
 import { ContentPlaceholder, Content, ContentPlaceholderScope } from 'cx/ui';
 import { Md } from 'docs/components/Md';
 import { CodeSplit } from 'docs/components/CodeSplit';
@@ -66,36 +66,39 @@ export const OuterLayouts = <cx>
             </div>
 
             <Content name="code">
-                <CodeSnippet fiddle="BzPHusws">{`
-               var AppLayout = <cx>
-                  <div style={{height: '200px', width: '300px', display: 'flex', flexDirection: 'column', border: '1px solid black'}}>
-                     <header style={{background: "lightblue", padding: '5px'}}>App Header</header>
-                     <div style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
-                        <aside style={{width: '70px', background: 'white', padding: '5px'}}>
-                           <ContentPlaceholder name="sidebar" />
-                        </aside>
-                        <main style={{flex: 1, padding: '5px'}}>
-                           <ContentPlaceholder /* name="body" *//>
-                        </main>
-                     </div>
-                  </div>
-               </cx>;
-               ...
-               <div outerLayout={AppLayout}>
-                  <Content for="sidebar">
-                     Nav 1
-                  </Content>
-                  Main 1
-               </div>
-               ...
-               <div outerLayout={AppLayout}>
-                  <Content for="sidebar">
-                     Nav 2
-                  </Content>
-                  Main 2
-               </div>
+                <Tab value-bind="$page.code.tab" mod="code" tab="layout" text="Layout" default/>
+                <Tab value-bind="$page.code.tab" mod="code" tab="widget" text="Widget" default/>
+                <CodeSnippet visible-expr="{$page.code.tab}=='layout'" fiddle="BzPHusws">{`
+                var AppLayout = <cx>
+                    <div style={{height: '200px', width: '300px', display: 'flex', flexDirection: 'column', border: '1px solid black'}}>
+                        <header style={{background: "lightblue", padding: '5px'}}>App Header</header>
+                        <div style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
+                            <aside style={{width: '70px', background: 'white', padding: '5px'}}>
+                            <ContentPlaceholder name="sidebar" />
+                            </aside>
+                            <main style={{flex: 1, padding: '5px'}}>
+                            <ContentPlaceholder /* name="body" *//>
+                            </main>
+                        </div>
+                    </div>
+                </cx>;
+                `}</CodeSnippet>
+                <CodeSnippet visible-expr="{$page.code.tab}=='widget'" fiddle="BzPHusws">{`
+                <div outerLayout={AppLayout}>
+                    <Content for="sidebar">
+                        Nav 1
+                    </Content>
+                    Main 1
+                </div>
+                
+                <div outerLayout={AppLayout}>
+                    <Content for="sidebar">
+                        Nav 2
+                    </Content>
+                    Main 2
+                </div>
 
-            `}</CodeSnippet>
+                `}</CodeSnippet>
             </Content>
         </CodeSplit>
 
