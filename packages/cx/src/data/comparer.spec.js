d5cb0dd6aaa6aaeba85ba74227c05dd70beec04b
Marko Stijak <mstijak@gmail.com>
2022-04-12 22:27:08 +0200

    Fix tests

--- packages/cx/src/data/comparer.spec.js
+++ packages/cx/src/data/comparer.spec.js
@@ -1,38 +1,38 @@
-import {sorter} from './comparer';
-import assert from 'assert';
+import { sorter } from "./comparer";
+import assert from "assert";
 
-describe('comparer', function() {
-   describe('sorter', function() {
-      it('orders by numeric values ASC', function () {
+describe("comparer", function () {
+   describe("sorter", function () {
+      it("orders by numeric values ASC", function () {
          let records = [
-            {value: 3, id: 1},
-            {value: -1, id: 2},
-            {value: 2, id: 3},
-            {value: null, id: 4},
-            {value: null, id: 5},
+            { value: 3, id: 1 },
+            { value: -1, id: 2 },
+            { value: 2, id: 3 },
+            { value: null, id: 4 },
+            { value: null, id: 5 },
          ];
 
-         let sort = sorter([{value: {bind: 'value'}, direction: 'ASC'}]);
+         let sort = sorter([{ value: { bind: "value" }, direction: "ASC" }]);
 
          let sorted = sort(records);
 
-         assert.equal(sorted[0].value, null);
-         assert.equal(sorted[1].value, null);
-         assert.equal(sorted[2].value, -1);
-         assert.equal(sorted[3].value, 2);
-         assert.equal(sorted[4].value, 3);
+         assert.equal(sorted[0].value, -1);
+         assert.equal(sorted[1].value, 2);
+         assert.equal(sorted[2].value, 3);
+         assert.equal(sorted[3].value, null);
+         assert.equal(sorted[4].value, null);
       });
 
-      it('orders by numeric values DESC', function () {
+      it("orders by numeric values DESC", function () {
          let records = [
-            {value: 3, id: 1},
-            {value: 1, id: 2},
-            {value: -2, id: 3},
-            {value: null, id: 4},
-            {value: null, id: 5},
+            { value: 3, id: 1 },
+            { value: 1, id: 2 },
+            { value: -2, id: 3 },
+            { value: null, id: 4 },
+            { value: null, id: 5 },
          ];
 
-         let sort = sorter([{value: {bind: 'value'}, direction: 'DESC'}]);
+         let sort = sorter([{ value: { bind: "value" }, direction: "DESC" }]);
 
          let sorted = sort(records);
 
@@ -43,14 +43,14 @@ describe('comparer', function() {
          assert.equal(sorted[4].value, null);
       });
 
-      it('supports field', function () {
+      it("supports field", function () {
          let records = [
-            {value: 3, id: 1},
-            {value: 1, id: 2},
-            {value: 2, id: 3}
+            { value: 3, id: 1 },
+            { value: 1, id: 2 },
+            { value: 2, id: 3 },
          ];
 
-         let sort = sorter([{field: 'value', direction: 'ASC'}]);
+         let sort = sorter([{ field: "value", direction: "ASC" }]);
          let sorted = sort(records);
          assert.equal(sorted[0].value, 1);
          assert.equal(sorted[1].value, 2);
