845fef5cee7e160cac944a68f313d263cb6aa28f
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2022-05-03 14:07:59 +0200

    TS > Missing Props: Content

--- packages/cx/src/ui/layout/Content.d.ts
+++ packages/cx/src/ui/layout/Content.d.ts
@@ -2,8 +2,13 @@ import * as Cx from '../../core';
 
 interface ContentProps extends Cx.PureContainerProps {
 
+
+   /** Placeholder name where the content is rendered. Interchangeable with `for` property, but has a lower priority. */
    name?: string,
-   putInto?: string,
+
+     /** Placeholder name where the content is rendered. Interchangeable with `name` property, but has a higher priority. */
+   for?: string,
+
    isContent?: boolean
 
 }
