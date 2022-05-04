5105ed294bec56ace64a731448bebd545f553ede
Marko Stijak <marko.stijak@codaxy.com>
2022-05-04 09:02:43 +0200

    Small typing fix

--- packages/cx/src/ui/layout/Content.d.ts
+++ packages/cx/src/ui/layout/Content.d.ts
@@ -1,13 +1,14 @@
-import * as Cx from '../../core';
+import * as Cx from "../../core";
 
 interface ContentProps extends Cx.PureContainerProps {
-
+   /** Placeholder name where the content is rendered. */
+   name?: string;
 
    /** Placeholder name where the content is rendered. */
-   name?: string,
+   for?: string;
 
-   isContent?: boolean
-
+   /** Placeholder name where the content is rendered. */
+   putInto?: string;
 }
 
 export class Content extends Cx.Widget<ContentProps> {}
