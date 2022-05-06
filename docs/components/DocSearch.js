4af372d2e0882acbe90b5dcc921066b6f828b4f4
Marko Stijak <marko.stijak@codaxy.com>
2022-05-06 13:29:51 +0200

    Upgrade docsearch to v3

--- docs/components/DocSearch.js
+++ docs/components/DocSearch.js
@@ -2,51 +2,46 @@ import { VDOM } from "cx/ui";
 import { Icon } from "cx/widgets";
 
 export class DocSearch extends VDOM.Component {
-    render() {
-        return (
-            <div className="cxb-docsearch">
-                <input type="text" id="docsearch" placeholder="Search..." className="cxe-docsearch-input" />
-                {Icon.render("search", { className: "cxe-docsearch-icon" })}
-            </div>
-        );
-    }
+   render() {
+      return <div className="cxb-docsearch"></div>;
+   }
 
-    delayedInit() {
-        if (this.initialized) return;
-        if (typeof docsearch != "function") return;
-        clearInterval(this.delayedInitTimer);
-        this.initialized = true;
-        try {
-            docsearch({
-                apiKey: "b77ab797ddcee40f03751aeb694168ed",
-                indexName: "cxjs",
-                inputSelector: "#docsearch",
-                debug: false, // Set debug to true if you want to inspect the dropdown,
+   delayedInit() {
+      if (this.initialized) return;
+      if (typeof docsearch != "function") return;
+      clearInterval(this.delayedInitTimer);
+      this.initialized = true;
+      try {
+         docsearch({
+            apiKey: "f693216413fd12fc412bdd65f66e8acc",
+            indexName: "cxjs",
+            container: ".cxb-docsearch",
+            appId: "E6T8DCDI3M",
+            debug: false, // Set debug to true if you want to inspect the dropdown,
 
-                // #if development
-                transformData: function (hits) {
-                    let address = `${location.protocol}//${location.hostname}`;
-                    if (location.port)
-                        address += ':' + location.port;
+            // #if development
+            transformData: function (hits) {
+               let address = `${location.protocol}//${location.hostname}`;
+               if (location.port) address += ":" + location.port;
 
-                    return hits.map(hit => ({
-                        ...hit,
-                        url: hit.url.replace("https://docs.cxjs.io", address)
-                    }))
-                }
-                // #end
-            });
-        } catch (err) {
-            console.log("DocSearch init error.", err);
-        }
-    }
+               return hits.map((hit) => ({
+                  ...hit,
+                  url: hit.url.replace("https://docs.cxjs.io", address),
+               }));
+            },
+            // #end
+         });
+      } catch (err) {
+         console.log("DocSearch init error.", err);
+      }
+   }
 
-    componentDidMount() {
-        this.delayedInit();
-        this.delayedInitTimer = setInterval(() => this.delayedInit(), 100);
-    }
+   componentDidMount() {
+      this.delayedInit();
+      this.delayedInitTimer = setInterval(() => this.delayedInit(), 100);
+   }
 
-    componentWillUnmount() {
-        clearInterval(this.delayedInitTimer);
-    }
+   componentWillUnmount() {
+      clearInterval(this.delayedInitTimer);
+   }
 }
