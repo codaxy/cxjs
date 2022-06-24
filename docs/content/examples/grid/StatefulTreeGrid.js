e09abec8a2bb04bbcdb424f8f82620780cb53803
Nebojsa <peric.nebojsa@ymail.com>
2022-06-24 15:58:37 +0200

    small grid changes

--- docs/content/examples/grid/StatefulTreeGrid.js
+++ docs/content/examples/grid/StatefulTreeGrid.js
@@ -1,16 +1,6 @@
 import { removeTreeNodes, updateTree } from "cx/data";
-import { bind, Controller, expr, KeySelection, TreeAdapter } from "cx/ui";
-import {
-  Button,
-  Content,
-  FlexRow,
-  Grid,
-  Menu,
-  MenuItem,
-  openContextMenu,
-  Tab,
-  TreeNode,
-} from "cx/widgets";
+import { bind, Controller, expr, KeySelection, PureContainer, TreeAdapter } from "cx/ui";
+import { Button, Content, FlexRow, Grid, Menu, MenuItem, openContextMenu, Tab, TreeNode } from "cx/widgets";
 import { CodeSnippet } from "../../../components/CodeSnippet";
 import { CodeSplit } from "../../../components/CodeSplit";
 import { Md } from "../../../components/Md";
@@ -18,269 +8,223 @@ import { casual } from "../data/casual";
 
 let idSeq = 0;
 let records = generateRecords();
-console.log(records);
 class PageController extends Controller {
-  onInit() {
-    this.load();
-    this.store.init("$page.treeExpanded", false);
-  }
+   onInit() {
+      this.load();
+      this.store.init("$page.treeExpanded", false);
+   }
 
-  async load() {
-    this.store.set("loading", true);
-    // fake loading
-    await waitFor(500);
+   async load() {
+      this.store.set("$page.loading", true);
+      // fake loading
+      await waitFor(500);
 
-    this.store.set("loading", false);
-    let newRecords = structuredClone(records);
+      this.store.set("$page.loading", false);
+      let newRecords = structuredClone(records);
 
-    this.store.set("$page.data", newRecords);
-  }
+      this.store.set("$page.data", newRecords);
+   }
 
-  async reset() {
-    this.store.delete("$page.data");
-    records = generateRecords();
-    this.load();
-  }
+   deleteRecordFromContextMenu(e, { store }) {
+      let recordId = store.get("$record.id");
+      this.doDelete(recordId);
+   }
 
-  deleteRecordFromContextMenu(e, { store }) {
-    let recordId = store.get("$record.id");
-    this.doDelete(recordId);
-  }
+   deleteRecord() {
+      let selection = this.store.get("$page.selection");
+      this.doDelete(selection);
+   }
 
-  deleteRecord() {
-    let selection = this.store.get("$page.selection");
-    this.doDelete(selection);
-  }
+   doDelete(id) {
+      records = removeTreeNodes(records, (r) => r.id === id);
+      this.load();
+   }
 
-  doDelete(id) {
-    records = removeTreeNodes(records, (r) => r.id === id);
-    this.load();
-  }
+   expandCollapseTree() {
+      this.store.toggle("$page.treeExpanded");
+      let expanded = this.store.get("$page.treeExpanded");
 
-  expandCollapseTree() {
-    this.store.toggle("$page.treeExpanded");
-    let expanded = this.store.get("$page.treeExpanded");
+      records = updateTree(
+         records,
+         (node) => ({
+            ...node,
+            $expanded: expanded,
+         }),
+         (node) => !node.$leaf,
+         "$children"
+      );
 
-    records = updateTree(
-      records,
-      (node) => ({
-        ...node,
-        $expanded: expanded,
-      }),
-      (node) => !node.leaf,
-      "$children"
-    );
+      this.load();
+   }
 
-    this.load();
-  }
+   addFolder() {
+      this.addEntry(false);
+   }
 
-  addFolder() {
-    this.addEntry(false);
-  }
+   addLeaf() {
+      this.addEntry(true);
+   }
 
-  addLeaf() {
-    this.addEntry(true);
-  }
+   addEntry(leaf) {
+      const newEntry = getNewEntry(leaf);
+      records = [...records, newEntry];
+      this.load();
+   }
 
-  addEntry(leaf) {
-    const newEntry = getNewEntry(leaf);
-    const selection = this.store.get("$page.selection");
-    const record = this.store.get("$record");
-    
-    if (selection && record.$leaf) {
-      return this.insertEntry(leaf, selection);
-    }
+   addFolderFromContextMenu(e, { store }) {
+      const recordId = store.get("$record.id");
+      this.addEntryFromContextMenu(recordId, false);
+   }
 
-    records = [...records, newEntry];
-    this.load();
-  }
+   addLeafFromContextMenu(e, { store }) {
+      const recordId = store.get("$record.id");
+      this.addEntryFromContextMenu(recordId, true);
+   }
 
-  addEntryFromContextMenu(e, { store }, leaf) {
-    const recordId = store.get("$record.id");
-    this.insertEntry(leaf, recordId);
+   addEntryFromContextMenu(parentId, leaf) {
+      const newEntry = getNewEntry(leaf);
 
-    this.load();
-  }
+      records = updateTree(
+         records,
+         (node) => ({
+            ...node,
+            $expanded: true,
+            $children: node?.$children?.length > 0 ? [...node.$children, newEntry] : [newEntry],
+         }),
+         (node) => node.id == parentId,
+         "$children"
+      );
 
-  insertEntry(leaf, targetId) {
-   const newEntry = getNewEntry(leaf);
-
-   records = updateTree(
-      records,
-      (node) => ({
-        ...node,
-        $expanded: true,
-        $children:
-          node?.$children?.length > 0
-            ? [...node.$children, newEntry]
-            : [newEntry],
-      }),
-      (node) => node.id == targetId,
-      "$children"
-    );
-
-    this.load();
-  }
+      this.load();
+   }
 }
 
 export const StatefulTreeGrid = (
-  <cx>
-    <Md controller={PageController}>
-      <CodeSplit>
-        # Stateful Tree Grid The following example shows how to make a tree out
-        of grid control.
-        <FlexRow spacing style="margin-bottom: 10px">
-          <Button onClick="load" text="Reload" mod="primary" />
-          <Button onClick="reset" mod="primary" text="Reset" />
-          <Button
-            onClick="expandCollapseTree"
-            text="Expand All"
-            text-expr="{$page.treeExpanded} ? 'Collapse All' : 'Expand All'"
-          />
-          <Button onClick="addFolder" text="Add Folder" />
-          <Button onClick="addLeaf" text="Add File" />
-          <Button
-            onClick="deleteRecord"
-            text="Delete"
-            mod="danger"
-            disabled-expr="!{$page.selection}"
-          />
-        </FlexRow>
-        <Grid
-          // buffered
-          records-bind="$page.data"
-          mod="tree"
-          style={{
-            width: "100%",
-            opacity: expr("{loading} ? 0.4 : 1"),
-            height: 500,
-          }}
-          scrollable={true}
-          dataAdapter={{
-            type: TreeAdapter,
-            restoreExpandedNodesOnLoad: true,
-            expandedNodesIdsMap: bind("$page.expandedNodesIds"),
-          }}
-          selection={{ type: KeySelection, bind: "$page.selection" }}
-          columns={[
-            {
-              header: "Name",
-              field: "fullName",
-              sortable: true,
-              items: (
-                <cx>
-                  <TreeNode
-                    expanded-bind="$record.$expanded"
-                    leaf-bind="$record.$leaf"
-                    level-bind="$record.$level"
-                    loading-bind="$record.$loading"
-                    text-bind="$record.fullName"
-                    icon-bind="$record.icon"
-                  />
-                </cx>
-              ),
-            },
-            { header: "Phone", field: "phone" },
-            { header: "City", field: "city", sortable: true },
-            {
-              header: "Notified",
-              field: "notified",
-              sortable: true,
-              value: { expr: '{$record.notified} ? "Yes" : "No"' },
-            },
-          ]}
-          onRowContextMenu={(e, instance) => {
-            let { store, controller } = instance;
-            openContextMenu(
-              e,
-              <cx>
-                <Menu>
-                  <MenuItem onClick="deleteRecordFromContextMenu" autoClose>
-                    Delete
-                  </MenuItem>
-                  <MenuItem
-                    onClick={(e) =>
-                      controller.addEntryFromContextMenu(e, { store }, false)
-                    }
-                    autoClose
-                  >
-                    Add Folder
-                  </MenuItem>
-                  <MenuItem
-                    onClick={(e) =>
-                      controller.addEntryFromContextMenu(e, { store }, true)
-                    }
-                    autoClose
-                  >
-                    Add File
-                  </MenuItem>
-                </Menu>
-              </cx>,
-              instance
-            );
-          }}
-        />
-        <Content name="code">
-          <Tab
-            value-bind="$page.code.tab"
-            mod="code"
-            tab="controller"
-            text="Controller"
-          />
-          <Tab
-            value-bind="$page.code.tab"
-            mod="code"
-            tab="index"
-            text="Index"
-            default
-          />
-          <CodeSnippet
-            visible-expr="{$page.code.tab}=='controller'"
-            fiddle="ozMlaYlG"
-          >
-            {`...`}
-          </CodeSnippet>
-          <CodeSnippet
-            visible-expr="{$page.code.tab}=='index'"
-            fiddle="ozMlaYlG"
-          >
-            {`...`}
-          </CodeSnippet>
-        </Content>
-      </CodeSplit>
-    </Md>
-  </cx>
+   <cx>
+      <Md controller={PageController}>
+         <CodeSplit>
+            # Stateful Tree Grid The following example shows how to make a tree out of grid control.
+            <FlexRow spacing style="margin-bottom: 10px">
+               <Button onClick="load" text="Reload" mod="primary" />
+               <Button
+                  onClick="expandCollapseTree"
+                  text="Expand All"
+                  text-expr="{$page.treeExpanded} ? 'Collapse All' : 'Expand All'"
+               />
+               <Button onClick="addFolder" text="Add Folder" />
+               <Button onClick="addLeaf" text="Add File" />
+               <Button onClick="deleteRecord" text="Delete" mod="danger" disabled-expr="!{$page.selection}" />
+            </FlexRow>
+            <Grid
+               emptyText="Loading data..."
+               records-bind="$page.data"
+               mod="tree"
+               style={{
+                  width: "100%",
+                  opacity: expr("{$page.loading} ? 0.4 : 1"),
+                  height: 500,
+               }}
+               scrollable={true}
+               dataAdapter={{
+                  type: TreeAdapter,
+                  restoreExpandedNodesOnLoad: true,
+                  expandedNodesIdsMap: bind("$page.expandedNodesIds"),
+               }}
+               selection={{ type: KeySelection, bind: "$page.selection" }}
+               columns={[
+                  {
+                     header: "Name",
+                     field: "fullName",
+                     sortable: true,
+                     items: (
+                        <cx>
+                           <TreeNode
+                              expanded-bind="$record.$expanded"
+                              leaf-bind="$record.$leaf"
+                              level-bind="$record.$level"
+                              loading-bind="$record.$loading"
+                              text-bind="$record.fullName"
+                              icon-bind="$record.icon"
+                           />
+                        </cx>
+                     ),
+                  },
+                  { header: "Phone", field: "phone" },
+                  { header: "City", field: "city", sortable: true },
+                  {
+                     header: "Notified",
+                     field: "notified",
+                     sortable: true,
+                     value: { expr: '{$record.notified} ? "Yes" : "No"' },
+                  },
+               ]}
+               onRowContextMenu={(e, instance) => {
+                  openContextMenu(
+                     e,
+                     <cx>
+                        <Menu>
+                           <MenuItem onClick="deleteRecordFromContextMenu" autoClose>
+                              Delete
+                           </MenuItem>
+                           <PureContainer visible-expr="!{$record.$leaf}">
+                              <MenuItem onClick="addFolderFromContextMenu" autoClose>
+                                 Add Folder
+                              </MenuItem>
+                              <MenuItem onClick="addLeafFromContextMenu" autoClose>
+                                 Add File
+                              </MenuItem>
+                           </PureContainer>
+                        </Menu>
+                     </cx>,
+                     instance
+                  );
+               }}
+            />
+            <Content name="code">
+               <Tab value-bind="$page.code.tab" mod="code" tab="controller" text="Controller" />
+               <Tab value-bind="$page.code.tab" mod="code" tab="index" text="Index" default />
+               <CodeSnippet visible-expr="{$page.code.tab}=='controller'" fiddle="ozMlaYlG">
+                  {`...`}
+               </CodeSnippet>
+               <CodeSnippet visible-expr="{$page.code.tab}=='index'" fiddle="ozMlaYlG">
+                  {`...`}
+               </CodeSnippet>
+            </Content>
+         </CodeSplit>
+      </Md>
+   </cx>
 );
 
-function generateRecords(array = [], childrenCounter = 0) {
-  for (let i = 0; i < 15; i++) {
-    const leaf = Math.floor(Math.random() * 10) < 4;
-    array.push(getNewEntry(leaf));
+function generateRecords(level = 0) {
+   let array = [];
+   for (let i = 0; i < 15 - 5 * level; i++) {
+      const leaf = Math.floor(Math.random() * 10) < 4;
+      array.push(getNewEntry(leaf));
 
-    if (childrenCounter < 3) {
-      generateRecords(array[i].$children, childrenCounter + 1);
-    }
-  }
+      if (!leaf && level < 3) {
+         array[i].$children = generateRecords(level + 1);
+      }
+   }
 
-  return array;
+   return array;
 }
 
 function getNewEntry(leaf) {
-  return {
-    id: ++idSeq,
-    fullName: casual.full_name,
-    phone: casual.phone,
-    city: casual.city,
-    notified: false,
-    $children: [],
-    $leaf: leaf,
-  };
+   return {
+      id: ++idSeq,
+      fullName: casual.full_name,
+      phone: casual.phone,
+      city: casual.city,
+      notified: false,
+      $children: [],
+      $leaf: leaf,
+   };
 }
 
 function waitFor(time) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve();
-    }, time);
-  });
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         resolve();
+      }, time);
+   });
 }
