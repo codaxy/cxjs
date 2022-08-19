10efeaaa62ade7213fed65c8e29faa7520ebc75f
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2022-08-19 11:10:32 +0200

    TS - Fix ColorMapProps definition

--- packages/cx/src/charts/ColorMap.d.ts
+++ packages/cx/src/charts/ColorMap.d.ts
@@ -1,27 +1,21 @@
-import * as Cx from '../core';
-import {PureContainer} from '../ui/PureContainer';
+import * as Cx from "../core";
+import { PureContainer } from "../ui/PureContainer";
 
-interface ColorMapProps extends Cx.WidgetProps {}
-
-export class ColorMap extends Cx.Widget<ColorMapProps> {
-
-   static Scope: ColorMapScope;
-
+interface ColorMapProps extends Cx.WidgetProps {
    onGetCache?: string | (() => Cx.Record);
-
    names?: Cx.Prop<string[]>;
-
    step?: Cx.NumberProp;
    offset?: Cx.NumberProp;
    size?: Cx.NumberProp;
 }
 
+export class ColorMap extends Cx.Widget<ColorMapProps> {
+   static Scope: ColorMapScope;
+}
 
 export class ColorMapScope extends PureContainer {}
 
-export class ColorIndex  {
-
+export class ColorIndex {
    acknowledge(name: string);
-
-   map(name: string) : number;
+   map(name: string): number;
 }
