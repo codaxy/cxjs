429bacd475615107e776220b8ec279638a791e58
Marko Stijak <mstijak@gmail.com>
2022-08-23 16:51:44 +0200

    Tests

--- packages/cx/src/ui/Repeater.spec.js
+++ packages/cx/src/ui/Repeater.spec.js
@@ -1,59 +1,130 @@
-import { Cx } from './Cx';
-import { VDOM } from './Widget';
-import { HtmlElement } from '../widgets/HtmlElement';
-import { Store } from '../data/Store';
-import { Repeater } from './Repeater';
+import { Cx } from "./Cx";
+import { VDOM } from "./Widget";
+import { HtmlElement } from "../widgets/HtmlElement";
+import { Store } from "../data/Store";
+import { Repeater } from "./Repeater";
 
-import renderer from 'react-test-renderer';
-import assert from 'assert';
+import renderer from "react-test-renderer";
+import assert from "assert";
 
-describe('Repeater', () => {
+describe("Repeater", () => {
+   it("allows sorting", () => {
+      let data = [
+         {
+            value: "C",
+         },
+         {
+            value: "B",
+         },
+         {
+            value: "A",
+         },
+      ];
 
-   it('allows sorting', () => {
-      let data = [{
-         value: 'C',
-      }, {
-         value: 'B'
-      }, {
-         value: 'A'
-      }];
-
-      let widget = <cx>
-         <div>
-            <Repeater records={data} sorters={[{ field: 'value', direction: 'ASC'}]} recordAlias="$item">
-               <div text:bind="$item.value" />
-            </Repeater>
-         </div>
-      </cx>;
+      let widget = (
+         <cx>
+            <div>
+               <Repeater records={data} sorters={[{ field: "value", direction: "ASC" }]} recordAlias="$item">
+                  <div text:bind="$item.value" />
+               </Repeater>
+            </div>
+         </cx>
+      );
 
       let store = new Store();
 
-      const component = renderer.create(
-         <Cx widget={widget} store={store} subscribe immediate />
-      );
+      const component = renderer.create(<Cx widget={widget} store={store} subscribe immediate />);
 
       let tree = component.toJSON();
       assert.deepEqual(tree, {
-         type: 'div',
+         type: "div",
          props: {},
          children: [
             {
-               type: 'div',
+               type: "div",
                props: {},
-               children: ["A"]
+               children: ["A"],
             },
             {
-               type: 'div',
+               type: "div",
                props: {},
-               children: ["B"]
+               children: ["B"],
             },
             {
-               type: 'div',
+               type: "div",
                props: {},
-               children: ["C"]
-            }
-         ]
-      })
+               children: ["C"],
+            },
+         ],
+      });
+   });
+
+   it("changes are properly updated", () => {
+      let widget = (
+         <cx>
+            <div>
+               <Repeater records-bind="data" sorters={[{ field: "value", direction: "ASC" }]} recordAlias="$item">
+                  <div text:bind="$item.value" />
+               </Repeater>
+            </div>
+         </cx>
+      );
+
+      let store = new Store({
+         data: {
+            data: [
+               {
+                  value: "C",
+               },
+               {
+                  value: "B",
+               },
+            ],
+         },
+      });
+
+      const component = renderer.create(<Cx widget={widget} store={store} subscribe immediate />);
+
+      let tree = component.toJSON();
+      assert.deepEqual(tree, {
+         type: "div",
+         props: {},
+         children: [
+            {
+               type: "div",
+               props: {},
+               children: ["B"],
+            },
+            {
+               type: "div",
+               props: {},
+               children: ["C"],
+            },
+         ],
+      });
+
+      store.update("data", (data) => [{ value: "A" }, ...data]);
+
+      assert.deepEqual(component.toJSON(), {
+         type: "div",
+         props: {},
+         children: [
+            {
+               type: "div",
+               props: {},
+               children: ["A"],
+            },
+            {
+               type: "div",
+               props: {},
+               children: ["B"],
+            },
+            {
+               type: "div",
+               props: {},
+               children: ["C"],
+            },
+         ],
+      });
    });
 });
-
