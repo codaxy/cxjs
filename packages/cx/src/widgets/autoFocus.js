c0607023d6c260c804300c74909bc0a650f52851
Marko Stijak <marko.stijak@codaxy.com>
2022-10-05 13:19:37 +0200

    Prevent late autofocus for touch devices

--- packages/cx/src/widgets/autoFocus.js
+++ packages/cx/src/widgets/autoFocus.js
@@ -2,10 +2,8 @@ import { FocusManager } from "../ui/FocusManager";
 import { isTouchEvent } from "../util/isTouchEvent";
 
 export function autoFocus(el, component) {
-   if (isTouchEvent()) return;
    let data = component.props.data || component.props.instance.data;
    let autoFocusValue = el && data.autoFocus;
-   if (autoFocusValue && autoFocusValue != component.autoFocusValue)
-      FocusManager.focus(el);
+   if (autoFocusValue && autoFocusValue != component.autoFocusValue && !isTouchEvent()) FocusManager.focus(el);
    component.autoFocusValue = autoFocusValue;
 }
