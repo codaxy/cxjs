ccd0d29a3590e260cf9f0bd40938016a1f268431
Marko Stijak <mstijak@gmail.com>
2022-12-17 21:04:00 +0100

    Add missing props to SnapPointFinder

--- packages/cx/src/charts/helpers/SnapPointFinder.d.ts
+++ packages/cx/src/charts/helpers/SnapPointFinder.d.ts
@@ -1,19 +1,24 @@
-import * as Cx from '../../core';
-import {PointReducerProps} from './PointReducer';
+import * as Cx from "../../core";
+import { PointReducerProps } from "./PointReducer";
 
 interface SnapPointFinderProps extends PointReducerProps {
-
    /* Cursor X value. */
-   cursorX?: Cx.NumberProp,
+   cursorX?: Cx.NumberProp;
 
    /* Cursor Y value */
-   cursorY?: Cx.NumberProp,
+   cursorY?: Cx.NumberProp;
 
    /* A binding used to receive the x value of the nearest point.*/
-   snapX?: Cx.Bind,
+   snapX?: Cx.Prop<any>;
 
    /* A binding used to receive the y value of the nearest point. */
-   snapY?: Cx.Bind
+   snapY?: Cx.Prop<any>;
+
+   /* A binding used to receive the record prop */
+   snapRecord?: Cx.Prop<Cx.Record>;
+
+   /* Maximum distance between cursor and the snap point. */
+   maxDistance?: number;
 }
 
 export class SnapPointFinder extends Cx.Widget<SnapPointFinderProps> {}
