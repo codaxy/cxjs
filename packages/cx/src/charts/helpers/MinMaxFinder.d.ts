09621a045ab8d0a10689e83be4228ea478126a87
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2023-01-30 12:39:29 +0100

    Rename MinMaxReducer to MinMaxFinder. Add export declaration to PointReducerProps class.

--- packages/cx/src/charts/helpers/MinMaxFinder.d.ts
+++ packages/cx/src/charts/helpers/MinMaxFinder.d.ts
@@ -1,23 +1,22 @@
-import * as Cx from '../../core';
-import {PointReducerProps} from './PointReducer';
+import * as Cx from "../../core";
+import { PointReducerProps } from "./PointReducer";
 
 interface MinMaxFinderProps extends PointReducerProps {
-
    /* A binding used to receive the x value of the point with the minimum value */
-   minX?: Cx.Bind,
+   minX?: Cx.Bind;
 
    /* A binding used to receive the y value of the point with the minimum value */
-   minY?: Cx.Bind,
+   minY?: Cx.Bind;
 
    /* A binding used to receive the x value of the point with the maximum value */
-   maxX?: Cx.Bind,
+   maxX?: Cx.Bind;
 
    /* A binding used to receive the x value of the point with the maximum value */
-   maxY?: Cx.Bind,
+   maxY?: Cx.Bind;
 
    /* An object used for filtering data points. Available as accumulator.params inside the onMap function. */
-   params: Cx.StructuredProp
+   params: Cx.StructuredProp;
 }
 
-/** Find minimum and maximum points of a point serie */
+/** Find minimum and maximum points of a point series */
 export class MinMaxFinder extends Cx.Widget<MinMaxFinderProps> {}
