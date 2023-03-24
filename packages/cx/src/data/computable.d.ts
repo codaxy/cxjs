0df9350f97b7c11e63f4c8911f77078799f0d119
Marko Stijak <marko.stijak@codaxy.com>
2023-03-24 13:03:00 +0100

    Allow up to 8 typed arguments for computables and triggers

--- packages/cx/src/data/computable.d.ts
+++ packages/cx/src/data/computable.d.ts
@@ -21,6 +21,44 @@ export function computable(
    p4: string,
    computeFn: (v1: any, v2: any, v3: any, v4: any) => any
 ): Computable;
+export function computable(
+   p1: string,
+   p2: string,
+   p3: string,
+   p4: string,
+   p5: string,
+   computeFn: (v1: any, v2: any, v3: any, v4: any, v5: any) => any
+): Computable;
+export function computable(
+   p1: string,
+   p2: string,
+   p3: string,
+   p4: string,
+   p5: string,
+   p6: string,
+   computeFn: (v1: any, v2: any, v3: any, v4: any, v5: any, v6: any) => any
+): Computable;
+export function computable(
+   p1: string,
+   p2: string,
+   p3: string,
+   p4: string,
+   p5: string,
+   p6: string,
+   p7: string,
+   computeFn: (v1: any, v2: any, v3: any, v4: any, v5: any, v6: any, v7: any) => any
+): Computable;
+export function computable(
+   p1: string,
+   p2: string,
+   p3: string,
+   p4: string,
+   p5: string,
+   p6: string,
+   p7: string,
+   p8: string,
+   computeFn: (v1: any, v2: any, v3: any, v4: any, v5: any, v6: any, v7: any, v8: any) => any
+): Computable;
 
 export function computable<V1, R>(arg1: AccessorChain<V1>, compute: (v1: V1) => R): Computable<R>;
 export function computable<V1, V2, R>(
@@ -42,4 +80,46 @@ export function computable<V1, V2, V3, V4, R>(
    arg3: AccessorChain<V3>,
    arg4: AccessorChain<V4>,
    compute: (v1: V1, v2: V2, v3: V3, v4: V4) => R
+): Computable<R>;
+
+export function computable<V1, V2, V3, V4, V5, R>(
+   arg1: AccessorChain<V1>,
+   arg2: AccessorChain<V2>,
+   arg3: AccessorChain<V3>,
+   arg4: AccessorChain<V4>,
+   arg5: AccessorChain<V5>,
+   compute: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5) => R
+): Computable<R>;
+
+export function computable<V1, V2, V3, V4, V5, V6, R>(
+   arg1: AccessorChain<V1>,
+   arg2: AccessorChain<V2>,
+   arg3: AccessorChain<V3>,
+   arg4: AccessorChain<V4>,
+   arg5: AccessorChain<V5>,
+   arg6: AccessorChain<V6>,
+   compute: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6) => R
+): Computable<R>;
+
+export function computable<V1, V2, V3, V4, V5, V6, V7, R>(
+   arg1: AccessorChain<V1>,
+   arg2: AccessorChain<V2>,
+   arg3: AccessorChain<V3>,
+   arg4: AccessorChain<V4>,
+   arg5: AccessorChain<V5>,
+   arg6: AccessorChain<V6>,
+   arg7: AccessorChain<V7>,
+   compute: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6, v7: V7) => R
+): Computable<R>;
+
+export function computable<V1, V2, V3, V4, V5, V6, V7, V8, R>(
+   arg1: AccessorChain<V1>,
+   arg2: AccessorChain<V2>,
+   arg3: AccessorChain<V3>,
+   arg4: AccessorChain<V4>,
+   arg5: AccessorChain<V5>,
+   arg6: AccessorChain<V6>,
+   arg7: AccessorChain<V7>,
+   arg8: AccessorChain<V8>,
+   compute: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6, v7: V7, v8: V8) => R
 ): Computable<R>;
--- packages/cx/src/ui/Controller.d.ts
+++ packages/cx/src/ui/Controller.d.ts
@@ -37,6 +37,60 @@ export class Controller<D = any> {
       callback: (v1: V1, v2: V2, v3: V3, v4: V4) => void,
       autoRun?: boolean
    ): void;
+   addTrigger<V1, V2, V3, V4, V5>(
+      name: string,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5) => void,
+      autoRun?: boolean
+   ): void;
+   addTrigger<V1, V2, V3, V4, V5, V6>(
+      name: string,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>,
+         Cx.AccessorChain<V6>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6) => void,
+      autoRun?: boolean
+   ): void;
+   addTrigger<V1, V2, V3, V4, V5, V6, V7>(
+      name: string,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>,
+         Cx.AccessorChain<V6>,
+         Cx.AccessorChain<V7>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6, v7: V7) => void,
+      autoRun?: boolean
+   ): void;
+   addTrigger<V1, V2, V3, V4, V5, V6, V7, V8>(
+      name: string,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>,
+         Cx.AccessorChain<V6>,
+         Cx.AccessorChain<V7>,
+         Cx.AccessorChain<V8>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6, v7: V7, v8: v8) => void,
+      autoRun?: boolean
+   ): void;
 
    addTrigger(
       name: string,
@@ -60,6 +114,56 @@ export class Controller<D = any> {
       path: Cx.AccessorChain<R>,
       args: [Cx.AccessorChain<V1>, Cx.AccessorChain<V2>, Cx.AccessorChain<V3>, Cx.AccessorChain<V4>],
       callback: (v1: V1, v2: V2, v3: V3, v4: V4) => R
+   ): void;
+   addComputable<V1, V2, V3, V4, V5, R>(
+      path: Cx.AccessorChain<R>,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5) => R
+   ): void;
+   addComputable<V1, V2, V3, V4, V5, V6, R>(
+      path: Cx.AccessorChain<R>,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>,
+         Cx.AccessorChain<V6>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6) => R
+   ): void;
+   addComputable<V1, V2, V3, V4, V5, V6, V7, R>(
+      path: Cx.AccessorChain<R>,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>,
+         Cx.AccessorChain<V6>,
+         Cx.AccessorChain<V7>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6, v7: V7) => R
+   ): void;
+   addComputable<V1, V2, V3, V4, V5, V6, V7, V8, R>(
+      path: Cx.AccessorChain<R>,
+      args: [
+         Cx.AccessorChain<V1>,
+         Cx.AccessorChain<V2>,
+         Cx.AccessorChain<V3>,
+         Cx.AccessorChain<V4>,
+         Cx.AccessorChain<V5>,
+         Cx.AccessorChain<V6>,
+         Cx.AccessorChain<V7>,
+         Cx.AccessorChain<V8>
+      ],
+      callback: (v1: V1, v2: V2, v3: V3, v4: V4, v5: V5, v6: V6, v7: V7, v8: V8) => R
    ): void;
 
    addComputable(path: string, args: (string | Cx.AccessorChain<any>)[], callback: (...args) => any): void;
