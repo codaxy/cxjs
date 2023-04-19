f0fda92174b20df65474bf514555485179aa4376
Marko Stijak <marko.stijak@codaxy.com>
2023-04-19 16:26:35 +0200

    Add missing field in the type definition

--- packages/cx/src/widgets/form/Validator.d.ts
+++ packages/cx/src/widgets/form/Validator.d.ts
@@ -1,6 +1,8 @@
-import * as Cx from '../../core';
-import { FieldProps } from './Field';
+import * as Cx from "../../core";
+import { FieldProps } from "./Field";
 
-interface ValidatorProps extends FieldProps {}
+interface ValidatorProps extends FieldProps {
+   value: Cx.StructuredProp;
+}
 
 export class Validator extends Cx.Widget<ValidatorProps> {}
