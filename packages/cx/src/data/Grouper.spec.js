c1deb8e374d8f1b47596ab43c0c0133b8bd8a213
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2023-06-12 11:27:31 +0200

    Add tests for multi field groping

--- packages/cx/src/data/Grouper.spec.js
+++ packages/cx/src/data/Grouper.spec.js
@@ -1,23 +1,42 @@
-var Grouper = require('./Grouper').Grouper;
-import assert from 'assert';
+var Grouper = require("./Grouper").Grouper;
+import assert from "assert";
 
-describe('Grouper', function() {
-   describe('grouping', function () {
-      it('should work', function () {
-         var data = [{
-            key: 1,
-            value: 1
-         }, {
-            key: 2,
-            value: 2
-         }];
+describe("Grouper", function () {
+   describe("single grouping", function () {
+      it("should work", function () {
+         var data = [
+            { name: "John", age: 12 },
+            { name: "Jane", age: 12 },
+         ];
 
-         var grouper = new Grouper({ key: {bind:'key'}});
+         var grouper = new Grouper({ name: { bind: "name" } });
          grouper.processAll(data);
 
          var results = grouper.getResults();
-         //console.log(results);
+         // console.log(results);
          assert.equal(results.length, 2);
+      });
+   });
+
+   describe("multi grouping", function () {
+      it("should work", function () {
+         var data = [
+            { name: "John", age: 12 },
+            { name: "John", age: 12 },
+            { name: "John", age: 13 },
+            { name: "John", age: 14 },
+            { name: "Jane", age: 12 },
+            { name: "Jane", age: 13 },
+            { name: "Jane", age: 14 },
+         ];
+
+         var grouper = new Grouper({ name: { bind: "name" }, age: { bind: "age" } });
+         grouper.processAll(data);
+
+         var results = grouper.getResults();
+         // console.log(results);
+         assert.equal(results.length, 6);
+         assert.equal(results[0].records.length, 2);
       });
    });
 });
