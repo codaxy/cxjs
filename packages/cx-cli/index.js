0e0f0156e45c4b8da3421f35923d9ce36ee36b6a
Andrew Shulgin <andrewshulginua@gmail.com>
2023-11-08 00:22:33 +0200

    cx-cli: fixed command parsing

--- packages/cx-cli/index.js
+++ packages/cx-cli/index.js
@@ -1,33 +1,26 @@
 #! /usr/bin/env node
 
 let program = require("commander"),
-   pkg = require("./package.json");
+    pkg = require("./package.json");
 
-let { addRoute, createNewApp } = require("./commands");
-
-program
-   .version(pkg.version)
-   .option("create", "Create a new project directory with the basic app structure")
-   .option("add, route", "Add new route folder")
-   .parse(process.argv);
+let {addRoute, createNewApp} = require("./commands");
 
 console.log("Cx CLI v" + pkg.version);
 
-if (program.create)
-   createNewApp(program.args[0]);
-else if (program.route) {
-   let routeName = program.args[0];
-   console.log();
-   if (!routeName) {
-      console.log("Syntax error: missing route name. Correct syntax: cx add route <route_name>.");
-      return;
-   }
-   addRoute(routeName);
-}
+program.version(pkg.version);
 
+program.command("create")
+    .description("Create a new project directory with the basic app structure")
+    .argument('<name>', 'project directory')
+    .action((name) => {
+        createNewApp(name);
+    });
 
+program.command("add, route")
+    .description("Add new route folder")
+    .argument('<name>', 'route folder')
+    .action((routeName) => {
+        addRoute(routeName);
+    });
 
-
-
-
-
+program.parse();
