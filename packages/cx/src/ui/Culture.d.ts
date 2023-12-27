14ebd45f6a603012dffb20ca6029b54f2ba129ca
Nebojsa <peric.nebojsa@ymail.com>
2023-12-27 13:46:23 +0100

    add Culture setNumberCulture and setDateTimeCulture methods for customer number/date formatting

--- packages/cx/src/ui/Culture.d.ts
+++ packages/cx/src/ui/Culture.d.ts
@@ -1,22 +1,26 @@
-import * as Cx from '../core';
+import * as Cx from "../core";
 //import {NumberCulture, DateTimeCulture} from 'intl-io';
 
 declare type DateEncoding = (date: Date) => any;
 
 export class Culture {
-   static setCulture(cultureCode: string);
+   static setCulture(cultureCode: string): void;
 
-   static setDefaultCurrency(currencyCode: string);
+   static setDefaultCurrency(currencyCode: string): void;
 
    static readonly defaultCurrency: string;
 
    static readonly culture: string;
 
+   static setNumberCulture(cultureCode: string): void;
+
    static getNumberCulture(): any;
+
+   static setDateTimeCulture(cultureCode: string): void;
 
    static getDateTimeCulture(): any;
 
    static getDefaultDateEncoding(): DateEncoding;
 
-   static setDefaultDateEncoding(encoding: DateEncoding);
+   static setDefaultDateEncoding(encoding: DateEncoding): void;
 }
--- packages/cx/src/ui/Culture.js
+++ packages/cx/src/ui/Culture.js
@@ -1,20 +1,34 @@
-import {NumberCulture, DateTimeCulture} from 'intl-io';
-import {Localization} from './Localization';
-import {GlobalCacheIdentifier} from '../util/GlobalCacheIdentifier';
-import {invalidateExpressionCache} from "../data/Expression";
-import {invalidateStringTemplateCache} from "../data/StringTemplate";
-import {defaultCompare} from "../data/defaultCompare";
+import { NumberCulture, DateTimeCulture } from "intl-io";
+import { Localization } from "./Localization";
+import { GlobalCacheIdentifier } from "../util/GlobalCacheIdentifier";
+import { invalidateExpressionCache } from "../data/Expression";
+import { invalidateStringTemplateCache } from "../data/StringTemplate";
+import { defaultCompare } from "../data/defaultCompare";
 
-let culture = 'en';
+let culture = "en";
+let numberCulture = null;
+let dateTimeCulture = null;
 let cultureCache = {};
-let defaultCurrency = 'USD';
-let dateEncoding = date => date.toISOString();
+let defaultCurrency = "USD";
+let dateEncoding = (date) => date.toISOString();
 
 export class Culture {
    static setCulture(cultureCode) {
       culture = cultureCode;
       cultureCache = {};
       Localization.setCulture(cultureCode);
+      this.invalidateCache();
+   }
+
+   static setNumberCulture(cultureCode) {
+      numberCulture = cultureCode;
+      delete cultureCache.numberCulture;
+      this.invalidateCache();
+   }
+
+   static setDateTimeCulture(cultureCode) {
+      dateTimeCulture = cultureCode;
+      delete cultureCache.dateTimeCulture;
       this.invalidateCache();
    }
 
@@ -30,23 +44,21 @@ export class Culture {
    }
 
    static get defaultCurrency() {
-      return defaultCurrency
-   };
+      return defaultCurrency;
+   }
 
    static get culture() {
       return culture;
    }
 
    static getNumberCulture() {
-      if (!cultureCache.numberCulture)
-         cultureCache.numberCulture = new NumberCulture(culture);
+      if (!cultureCache.numberCulture) cultureCache.numberCulture = new NumberCulture(numberCulture ?? culture);
       return cultureCache.numberCulture;
    }
 
    static getDateTimeCulture() {
-      if (!cultureCache.dateCulture)
-         cultureCache.dateCulture = new DateTimeCulture(culture);
-      return cultureCache.dateCulture;
+      if (!cultureCache.dateTimeCulture) cultureCache.dateTimeCulture = new DateTimeCulture(dateTimeCulture ?? culture);
+      return cultureCache.dateTimeCulture;
    }
 
    static getDefaultDateEncoding() {
@@ -58,8 +70,7 @@ export class Culture {
    }
 
    static getComparer(options) {
-      if (typeof Intl.Collator != "undefined")
-         return new Intl.Collator(culture, options).compare;
+      if (typeof Intl.Collator != "undefined") return new Intl.Collator(culture, options).compare;
       return defaultCompare;
    }
 }
