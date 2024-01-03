e579dd615e789bea806a4261a235dfcaf105ea96
Marko Stijak <marko.stijak@codaxy.com>
2024-01-03 10:16:18 +0100

    Fix build errors

--- benchmark/babel-config.js
+++ benchmark/babel-config.js
@@ -2,10 +2,7 @@ module.exports = function (production) {
    let config = {
       cacheDirectory: true,
       cacheIdentifier: "v14",
-      plugins: [
-         ["@babel/plugin-proposal-private-methods", { loose: false }],
-         ["@babel/plugin-proposal-private-property-in-object", { loose: false }],
-      ],
+      plugins: [],
       presets: [
          [
             "cx-env",
