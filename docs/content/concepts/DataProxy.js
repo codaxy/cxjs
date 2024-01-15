b7932c33072c934f26e41a50f1fcf782092db46e
Nikola Grujic <nikola-grujic@hotmail.com>
2024-01-15 10:55:00 +0100

    Improve DataProxy docs

--- docs/content/concepts/DataProxy.js
+++ docs/content/concepts/DataProxy.js
@@ -20,7 +20,7 @@ export const DataProxyPage = <cx>
                 The simplest use case for `DataProxy` is when we want to create an alias for a certain store binding.
                 In the example below, `level` binding is also made available as `$level`.
                 This creates a simple two-way mapping between the two store values. Moving one slider will affect the other.
-                
+
                 <div class="widgets flex-row">
                     <LabelsTopLayout>
                         <Slider value-bind="level" label="Level" />
@@ -34,6 +34,7 @@ export const DataProxyPage = <cx>
                         </LabelsTopLayout>
                     </DataProxy>
                 </div>
+
                 <Content name="code">
                     <Tab value-bind="$page.code1.tab" mod="code" tab="code" text="DataProxy" default />
                     <CodeSnippet fiddle="YwPbwL8u">{`
@@ -54,19 +55,22 @@ export const DataProxyPage = <cx>
                 </Content>
             </CodeSplit>
         </Rescope>
+
         ### Defining multiple aliases
+
+        `data` property is used to define multiple mappings. `data` is an object whose property names serve as aliases,
+        and their values are objects with `expr` and `set` properties that define custom getter and setter logic:
+
         <Rescope>
             <CodeSplit>
-                `data` property is used to define multiple mappings. `data` is an object whose property names serve as aliases, 
-                and their values are objects with `expr` and `set` properties that define custom getter and setter logic: 
                 - `expr` defines a getter logic and can be a Cx computable or an expression,
-                - `set` is a function that receives the alias value and the `instance` object as parameters. The `store` can be accessed directly 
+                - `set` is a function that receives the alias value and the `instance` object as parameters. The `store` can be accessed directly
                     with destructuring assignment syntax. Note that the setter function needs to call the `store.set` method explicitly
-                    in order to set the `level` value, as opposed to just returning the calculated value. 
-                    This is because we can use any number of store values to calculate the alias, 
+                    in order to set the `level` value, as opposed to just returning the calculated value.
+                    This is because we can use any number of store values to calculate the alias,
                     and it's up to us to define the setter logic correctly.
 
-                Omitting the `set` property will make the alias itself a read-only. Attempting to change its value will log 
+                Omitting the `set` property will make the alias itself a read-only. Attempting to change its value will log
                 an error to the console, so the UI should not allow it.
 
                 <div class="widgets flex-row flex-start">  
@@ -89,53 +93,51 @@ export const DataProxyPage = <cx>
                     >
                         <div class="flex-column">
                             <Slider value-bind="$invertedLevel" label="Inverted level" />
-                            <Slider value-bind="$level" label="Level (read-only)" readOnly />
+                            <Slider value-bind="$level" label="Level (read-only)" />
                         </div>
                     </DataProxy>
                 </div>
+
                 <Content name="code">
-                <Tab value-bind="$page.code2.tab" mod="code" tab="code" text="DataProxy" default />
-                <CodeSnippet fiddle="2HxXBe43">{`
-                    <div class="widgets flex-row flex-start">  
-                        <LabelsTopLayout>
-                            <Slider value-bind="level" label="Level" />
-                        </LabelsTopLayout>
-                        <DataProxy
-                            data={{
-                                $invertedLevel: {
-                                    expr: computable("level", v => 100 - v),
-                                    set: (value, {store}) => {
-                                        store.set("level", 100 - value);
+                    <Tab value-bind="$page.code2.tab" mod="code" tab="code" text="DataProxy" default />
+                    <CodeSnippet fiddle="o5C83HBH">{`
+                        <FlexRow spacing="xlarge" align="start">
+                            <LabelsTopLayout>
+                                <Slider value-bind="level" label="Level" />
+                            </LabelsTopLayout>
+                            <DataProxy
+                                data={{
+                                    $invertedLevel: {
+                                        expr: computable("level", (v) => 100 - v),
+                                        set: (value, { store }) => {
+                                            store.set("level", 100 - value);
+                                        }
+                                    },
+                                    $level: {
+                                        expr: "{level}"
                                     }
-                                },
-                                // read-only
-                                $level: {
-                                    expr: "{level}"
-                                }
-                            }}
-                        >
-                            <div class="flex-column">
-                                <Slider value-bind="$invertedLevel" label="Inverted level" />
-                                <Slider value-bind="$level" label="Level (read-only)" />
-                            </div>
-                        </DataProxy>
-                    </div>
-                `}</CodeSnippet>
+                                }}
+                            >
+                                <FlexCol>
+                                    <Slider value-bind="$invertedLevel" label="Inverted level" />
+                                    <Slider value-bind="$level" label="Level (read-only)" />
+                                </FlexCol>
+                            </DataProxy>
+                        </FlexRow>
+                    `}</CodeSnippet>
                 </Content>
-
-                If mapping is done in both directions (both getter and setter are used), it is important that both operations are reversible, without any data loss. 
-                This means, for any alias value, we should be able to get back all of the store values that were used to calculate it. 
-                Failing to do so will cause bugs that are hard to detect.
-
-                **Note**: It is good practice to prefix the alias name with a `$` sign in order to avoid unintentional name shadowing 
-                which will cause an infinite get-set loop and a `Maximum call stack exceeded` error.
-
-                ### Advanced example
-
-                [Exposing currently selected Grid record](~/examples/grid/form-edit) for real time editing is a common use case for `DataProxy`.
-
             </CodeSplit>
         </Rescope>
+
+        If mapping is done in both directions (both getter and setter are used), it is important that both operations are reversible, without any data loss. 
+        This means, for any alias value, we should be able to get back all of the store values that were used to calculate it. 
+        Failing to do so will cause bugs that are hard to detect.
+
+        **Note**: It is good practice to prefix the alias name with a `$` sign in order to avoid unintentional name shadowing 
+        which will cause an infinite get-set loop and a `Maximum call stack exceeded` error.
+
+        ### Advanced example
+        [Exposing currently selected Grid record](~/examples/grid/form-edit) for real time editing is a common use case for `DataProxy`.
 
         ### Configuration
         <ConfigTable props={dataProxyConfig} />
