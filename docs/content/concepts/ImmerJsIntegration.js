9baad8b48ac9edc3ea7c78ad63f0448382c53ac9
nikolatechie <nikola-grujic@hotmail.com>
2024-01-28 10:17:56 +0100

    Change layout

--- docs/content/concepts/ImmerJsIntegration.js
+++ docs/content/concepts/ImmerJsIntegration.js
@@ -7,31 +7,31 @@ export const ImmerJsIntegration = <cx>
     <Md>
         # Immer.js Integration
 
-        CxJS requires all data to be immutable. Each change (`update`) operation is required to return a new object.
-        This can be very challenging with complex and deeply nested data models, especially for developers who
-        are not used to that.
-
-        [Immer.js](https://immerjs.github.io/immer/) can help greatly with that, especially in combination with [typed data models](~/concepts/typed-models).
-
-        In order to use Immer.js, you need to install `immer` and `cx-immer` npm packages.
-
-        ```
-        npm install immer cx-immer
-        ```
-
-        Once installed you should enable Immer in CxJS stores. This should be done on application startup.
-
         <CodeSplit>
-            <CodeSnippet copy={false}>{`
+            CxJS requires all data to be immutable. Each change (`update`) operation is required to return a new object.
+            This can be very challenging with complex and deeply nested data models, especially for developers who
+            are not used to that.
+
+            [Immer.js](https://immerjs.github.io/immer/) can help greatly with that, especially in combination with [typed data models](~/concepts/typed-models).
+
+            In order to use Immer.js, you need to install `immer` and `cx-immer` npm packages.
+
+            ```
+            npm install immer cx-immer
+            ```
+
+            Once installed you should enable Immer in CxJS stores. This should be done on application startup.
+
+            <CodeSplit>
+                <CodeSnippet copy={false}>{`
                 import { enableImmerMutate } from "cx-immer";
 
                 enableImmerMutate();
             `}</CodeSnippet>
-        </CodeSplit>
+            </CodeSplit>
 
-        Once this is implemented you can use the `mutate` method for updating data in the store.
+            Once this is implemented you can use the `mutate` method for updating data in the store.
 
-        <CodeSplit>
             <Content name="code">
                 <Tab value={"controller"} tab="controller" mod="code" text="Controller.ts" default />
 
