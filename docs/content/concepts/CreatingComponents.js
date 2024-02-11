4278b2a48d3eefd5c0a1d43b94288881d64bc3c3
nikolatechie <nikola-grujic@hotmail.com>
2024-02-11 13:06:55 +0100

    Add explanation for creating components

--- docs/content/concepts/CreatingComponents.js
+++ docs/content/concepts/CreatingComponents.js
@@ -77,7 +77,7 @@ export const CreatingComponents = <cx>
 
         Although CxJS contains a large set of built-in components, you might sometimes need to create
         a custom CxJS component for your use case. In this guide, we'll go over the steps to create
-        a simple custom component.
+        a simple custom component, `Square`, that changes its background color when clicked.
 
         <CodeSplit>
             <div class="widgets">
@@ -122,7 +122,7 @@ export const CreatingComponents = <cx>
                             const { red, green, blue } = props;
                             this.state = {
                                 color: (red && green && blue) ?
-                                this.getColorFromRgb(red, green, blue) : this.getRandomColor()
+                                    this.getColorFromRgb(red, green, blue) : this.getRandomColor()
                             };
                         }
 
@@ -168,5 +168,42 @@ export const CreatingComponents = <cx>
                 `}</CodeSnippet>
             </Content>
         </CodeSplit>
+
+        Before proceeding, we highly recommend reviewing the [Widgets](/concepts/widgets) documentation
+        for better understanding.
+
+        To create a custom component, you typically define a class that extends one of the existing CxJS
+        component classes, e.g. `Widget` or `Field`, based on the functionality and purpose of the
+        component you're building.
+
+        ### `declareData()`
+
+        This method is often used in custom components to declare the expected properties
+        (data fields) that can be passed to the component. In the `Square` component, `declareData()`
+        is used to specify the expected data fields `red`, `green`, and `blue`. These fields correspond
+        to the RGB values determining the inital color of the square, if specified.
+
+        ### `render()`
+
+        The `render()` method is a fundamental part of CxJS components. It defines how the component
+        should be rendered based on its current state and props. In the `Square` component, the
+        `render()` method is responsible for rendering the actual `SquareCmp` JSX component. It
+        extracts properties from the component's data and passes them down to `SquareCmp`.
+
+        ### `Widget.alias()`
+
+        `Widget.alias()` is used to register a custom component under a specific alias.
+
+        ### `VDOM.Component`
+
+        `VDOM.Component` is the base class for CxJS components that use **virtual DOM** (**VDOM**) for
+        rendering. In the `SquareCmp` class, we extend `VDOM.Component` to create a new component
+        class that can manage its own state and lifecycle methods.
+
+        In our case, the state holds square's current color. We set the state in the constructor
+        depending on the passed props. If the initial color is provided using the RGB props, we call
+        `getColorFromRgb()` method to set the color. Otherwise, we invoke `getRandomColor()` to assign
+        a random color. `render()` method renders a square and assigns an `onClick()` event callback
+        that updates the state (color) and causes a re-render.
     </Md>
 </cx>
