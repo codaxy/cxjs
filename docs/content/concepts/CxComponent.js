51336c4998b3ff6cced28b709df45534127a4684
Marko Stijak <marko.stijak@codaxy.com>
2024-02-21 12:17:37 +0100

    Update the Cx component docs

--- docs/content/concepts/CxComponent.js
+++ docs/content/concepts/CxComponent.js
@@ -13,41 +13,43 @@ export const CxComponent = <cx>
 
         <CodeSplit>
             The `Cx` component is a React component designed to integrate CxJS widgets
-            into a React application. It acts as a mediator between React and CxJS.
+            into React applications. It acts as a mediator between React and CxJS.
 
             <Content name="code">
-                <Tab value="example" tab="example" text="startAppLoop.js" mod="code" default />
+                <Tab value="example" tab="example" text="ReactApp.js" mod="code" default />
 
                 <CodeSnippet>{`
-                    let root = <Cx
-                        store={store}
-                        widget={widget}
-                        instance={instance}
-                        parentInstance={parentInstance}
-                        options={options}
-                        subscribe
-                    />;
+                    let store = new Store();
+                    ...
+                    // React
+                    <div>
+                        <Cx store={store} subscribe>
+                            // CxJS widgets
+                            <TextField value-bind="name" />
+                        </Cx>
+                        // React again
+                        <button
+                            onClick={(e) => {
+                                alert("Hello " + store.get("name"));
+                            }}
+                        >
+                            Say Hello
+                        </button>
+                    </div>
                 `}</CodeSnippet>
             </Content>
         </CodeSplit>
 
         ## Injecting CxJS into React
+
         Since the `Cx` component contains mechanisms needed for rendering CxJS widgets,
         we can use e.g. `Button` or `Grid` in our React applications.
 
-        1. First we need to install CxJS and its dependencies in our project. It can be
-        done running the `npm install cx` command.
+        1. Create a new [Store]("~/concepts/store") or use an existing one.
 
-        2. In the application's entry point, invoke `startAppLoop()` to begin processing
-        and rendering CxJS components. That method accepts a parent's `DOM element` which
-        contains the whole application, a `Store` or an `Instance` object, and the
-        component that we want to render.
+        2. Within the Cx component, we can use any CxJS widget.
 
-        3. Finally, we can import widgets that we want to use.
-        CxJS processes its own code internally within the framework, employing its
-        own tree traversal logic and store management. When widgets are encountered,
-        CxJS processes them and generates a **virtual DOM** (**VDOM**) representation.
-        This VDOM is then seamlessly integrated into React's rendering process.
+        3. Access or manipulate data in the store to communicate with the CxJS widgets.
 
         ### Configuration
         <ConfigTable props={configs} />
