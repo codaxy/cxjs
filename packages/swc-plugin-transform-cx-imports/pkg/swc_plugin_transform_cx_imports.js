ab7eac91bb76e9e5bf88becd52db4f68e79dde1e
nikolatechie <nikola-grujic@hotmail.com>
2024-03-13 12:27:18 +0100

    Add SWC plugins

---
+++ packages/swc-plugin-transform-cx-imports/pkg/swc_plugin_transform_cx_imports.js
@@ -0,0 +1,4 @@
+import * as wasm from "./swc_plugin_transform_cx_imports_bg.wasm";
+import { __wbg_set_wasm } from "./swc_plugin_transform_cx_imports_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./swc_plugin_transform_cx_imports_bg.js";
---
+++ packages/swc-plugin-transform-cx-jsx/pkg/swc_plugin_transform_cx_jsx_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __get_transform_plugin_core_pkg_diag(): number;
+export function __transform_plugin_process_impl(a: number, b: number, c: number, d: number): number;
+export function __alloc(a: number): number;
+export function __free(a: number, b: number): number;
