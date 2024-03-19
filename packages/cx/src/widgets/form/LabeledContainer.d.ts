1dc101acfcf355bd3cc4a79ba75e25d46999b2ed
Nebojsa Peric <peric.nebojsa@ymail.com>
2024-03-19 22:45:57 +0100

    * ValidationGroup asterisk prop should be a prop * LabeledContainer disabled prop shouldn't be inherited from FieldGroup

--- packages/cx/src/widgets/form/LabeledContainer.d.ts
+++ packages/cx/src/widgets/form/LabeledContainer.d.ts
@@ -1,17 +1,12 @@
-import * as Cx from '../../core';
-import { FieldGroupProps } from './FieldGroup';
+import * as Cx from "../../core";
+import { FieldGroupProps } from "./FieldGroup";
 
 interface LabeledContainerProps extends FieldGroupProps {
-
    /** The label. */
-   label?: Cx.StringProp | Cx.Config,
-   
-   /** Set to true to disable all fields inside the container. */
-   disabled?: boolean,
+   label?: Cx.StringProp | Cx.Config;
 
    /** Apply asterisk to the label. */
-   asterisk?: boolean
-   
+   asterisk?: boolean;
 }
 
 export class LabeledContainer extends Cx.Widget<LabeledContainerProps> {}
