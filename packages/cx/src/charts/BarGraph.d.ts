3ce4c5cdb5be179e45d37d000ba46d050ef68d20
Nebojsa <peric.nebojsa@ymail.com>
2024-03-27 15:56:26 +0100

    allow setting x0Field to false explicitely

--- packages/cx/src/charts/BarGraph.d.ts
+++ packages/cx/src/charts/BarGraph.d.ts
@@ -1,17 +1,15 @@
-import * as Cx from '../core';
-import { ColumnBarGraphBaseProps } from './ColumnBarGraphBase';
+import * as Cx from "../core";
+import { ColumnBarGraphBaseProps } from "./ColumnBarGraphBase";
 
 interface BarGraphProps extends ColumnBarGraphBaseProps {
-
    /** Base CSS class to be applied to the element. Defaults to `bargraph`. */
-   baseClass?: string,
+   baseClass?: string;
 
-   /** 
-    * Name of the property which holds the base value. 
+   /**
+    * Name of the property which holds the base value.
     * Default value is `false`, which means x0 value is not read from the data array.
     */
-   x0Field?: string,
-  
+   x0Field?: string | false;
 }
 
 export class BarGraph extends Cx.Widget<BarGraphProps> {}
