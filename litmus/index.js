cf2a6933ff2f836bb5878e8eb3a2819a33e486ee
Nebojsa <peric.nebojsa@ymail.com>
2024-04-08 14:10:42 +0200

    add litmus for window dismiss on dropdown dismiss issue

--- litmus/index.js
+++ litmus/index.js
@@ -112,7 +112,8 @@ import "./index.scss";
 //import Demo from "./bugs/1075-complex-column-resizing";
 //import Demo from "./features/grid/RowDndAndGrouping";
 //import Demo from "./features/localization/culture-scope";
-import Demo from "./bugs/large_and_small_values_inside_pie_charts_with_gaps";
+// import Demo from "./bugs/large_and_small_values_inside_pie_charts_with_gaps";
+import Demo from "./bugs/window_dissmisses_on_dropdown_dismiss";
 
 let store = (window.store = new Store());
 
