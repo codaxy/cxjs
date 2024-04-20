118901200db40e39d58ae34d7da3f9924327b6cc
Marko Stijak <mstijak@gmail.com>
2024-04-20 15:35:24 +0200

    Allow model accessors to be initialized via a string

--- packages/cx/src/data/createAccessorModelProxy.d.ts
+++ packages/cx/src/data/createAccessorModelProxy.d.ts
@@ -3,4 +3,4 @@ import { AccessorChain } from "../core";
 export function isAccessorChain(value: any): boolean;
 export function isAccessorChain<V>(value: any): value is AccessorChain<V>;
 
-export function createAccessorModelProxy<M>(): AccessorChain<M>;
+export function createAccessorModelProxy<M>(basePath: string = ""): AccessorChain<M>;
