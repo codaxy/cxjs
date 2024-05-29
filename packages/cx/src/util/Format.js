cae505848e52d6f3c6e899e95279406def49d8cb
vedranvucic77 <vedran.vucic@codaxy.com>
2024-05-29 09:20:26 +0200

    use substring instead of substr

--- packages/cx/src/util/Format.js
+++ packages/cx/src/util/Format.js
@@ -143,7 +143,7 @@ let formatFactory = {
       return (value) => {
          let s = String(value);
          return s.replace(/\w\S*/g, function (txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
          });
       };
    },
