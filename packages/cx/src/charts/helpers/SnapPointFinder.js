1a068911f6e0c15f1163470e35c4ef197b1c23ed
Nebojsa Peric <peric.nebojsa@ymail.com>
2024-06-12 23:34:16 +0200

    add SnapPointFinder convert function

--- packages/cx/src/charts/helpers/SnapPointFinder.js
+++ packages/cx/src/charts/helpers/SnapPointFinder.js
@@ -1,4 +1,4 @@
-import {PointReducer} from "./PointReducer";
+import { PointReducer } from "./PointReducer";
 
 export class SnapPointFinder extends PointReducer {
    declareData() {
@@ -8,11 +8,11 @@ export class SnapPointFinder extends PointReducer {
          snapX: undefined,
          snapY: undefined,
          snapRecord: undefined,
-         maxDistance: undefined
-      })
+         maxDistance: undefined,
+      });
    }
 
-   onInitAccumulator(acc, {data}) {
+   onInitAccumulator(acc, { data }) {
       acc.cursorX = data.cursorX;
       acc.cursorY = data.cursorY;
       acc.dist = data.maxDistance > 0 ? Math.pow(data.maxDistance, 2) : Number.POSITIVE_INFINITY;
@@ -22,26 +22,26 @@ export class SnapPointFinder extends PointReducer {
 
    onMap(acc, x, y, name, p) {
       let d = null;
+      let cx = this.convert(x);
 
-      if (acc.cursorX != null && x != null)
-         d = (d || 0) + Math.pow(Math.abs(x - acc.cursorX), 2);
+      if (acc.cursorX != null && cx != null) d = (d || 0) + Math.pow(Math.abs(cx - acc.cursorX), 2);
 
-      if (acc.cursorY != null && y != null)
-         d = (d || 0) + Math.pow(Math.abs(y - acc.cursorY), 2);
+      if (acc.cursorY != null && y != null) d = (d || 0) + Math.pow(Math.abs(y - acc.cursorY), 2);
 
       if (d != null && d < acc.dist) {
          acc.dist = d;
-         acc.snapX = x;
+         acc.snapX = cx;
          acc.snapY = y;
          acc.snapRecord = p;
       }
    }
 
    onReduce(acc, instance) {
-      instance.set('snapX', acc.snapX);
-      instance.set('snapY', acc.snapY);
-      instance.set('snapRecord', acc.snapRecord);
+      instance.set("snapX", acc.snapX);
+      instance.set("snapY", acc.snapY);
+      instance.set("snapRecord", acc.snapRecord);
    }
 }
 
 SnapPointFinder.prototype.maxDistance = 50;
+SnapPointFinder.prototype.convert = (x) => x;
