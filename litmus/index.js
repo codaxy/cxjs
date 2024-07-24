afd5d84f82080c55cc916423f1eb23a12a16b899
Nebojsa <peric.nebojsa@ymail.com>
2024-07-24 10:05:24 +0200

    add litmus import for ColumnGraphVSColumnZeroHeight

--- litmus/index.js
+++ litmus/index.js
@@ -118,7 +118,8 @@ import "./index.scss";
 //import Demo from "./features/formats/zeroPadFormat";
 //import Demo from "./bugs/window_dissmisses_on_dropdown_dismiss";
 // import Demo from "./bugs/grid-grouping-incorrect-text-prop-description";
-import Demo from "./features/calendar/year-selection";
+// import Demo from "./features/calendar/year-selection";
+import Demo from "./features/charts/column/ColumnGraphVSColumnZeroHeight";
 
 let store = (window.store = new Store());
 
