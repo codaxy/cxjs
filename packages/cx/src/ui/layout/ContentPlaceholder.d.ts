bcdc50085bacef665c69f95928b5386c78b88d6f
Marko Stijak <mstijak@gmail.com>
2024-10-05 22:06:41 +0200

    Add typing

--- packages/cx/src/ui/layout/ContentPlaceholder.d.ts
+++ packages/cx/src/ui/layout/ContentPlaceholder.d.ts
@@ -1,18 +1,19 @@
-import * as Cx from '../../core';
-import {RenderingContext} from '../RenderingContext';
+import * as Cx from "../../core";
+import { RenderingContext } from "../RenderingContext";
 
 interface ContentPlaceholderProps extends Cx.PureContainerProps {
+   name?: Cx.StringProp;
 
-   name?: Cx.StringProp,
+   scoped?: boolean;
 
-   scoped?: boolean
+   /* Set to true to allow all registered content elements to render inside the placeholder. Otherwise only one element is rendered. */
+   allowMultiple?: boolean;
 }
 
 export class ContentPlaceholder extends Cx.Widget<ContentPlaceholderProps> {}
 
-
 interface ContentPlaceholderScopeProps extends Cx.PureContainerProps {
-   name?: string | string[]
+   name?: string | string[];
 }
 
 export class ContentPlaceholderScope extends Cx.Widget<ContentPlaceholderScopeProps> {}
