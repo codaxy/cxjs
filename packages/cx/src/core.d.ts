4df22c6840035ed699bd42228487af169a4bdf78
Nebojsa <peric.nebojsa@ymail.com>
2025-02-20 11:41:41 +0100

    fix HtmlElementProps id prop type definition

--- packages/cx/src/core.d.ts
+++ packages/cx/src/core.d.ts
@@ -160,7 +160,7 @@ declare namespace Cx {
 
    interface HtmlElementProps extends StyledContainerProps {
       /** Id of the element */
-      id?: string | number | Binding | Selector<string | number>;
+      id?: Cx.StringProp | Cx.NumberProp;
 
       /** Inner text contents. */
       text?: Cx.StringProp | Cx.NumberProp;
@@ -251,6 +251,39 @@ declare global {
          tooltip?: Cx.StringProp | Cx.StructuredProp;
       }
    }
+
+   interface JSON {
+      /* JSON doesn't support symbol keys, and number keys
+       * are coerced to strings, even in arrays */
+
+      /**
+       * Converts a JavaScript Object Notation (JSON) string into an object.
+       * @param text A valid JSON string.
+       * @param reviver A function that transforms the results. This function is called for each member of the object.
+       * If a member contains nested objects, the nested objects are transformed before the parent object is.
+       */
+      parse(text: string, reviver?: (this: unknown, key: string, value: unknown) => unknown): unknown;
+
+      /**
+       * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+       * @param value A JavaScript value, usually an object or array, to be converted.
+       * @param replacer A function that transforms the results.
+       * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+       */
+      stringify(
+         text: unknown,
+         replacer?: (this: unknown, key: string, value: unknown) => unknown,
+         space?: string | number,
+      ): string;
+   }
+
+   interface ArrayConstructor {
+      isArray(a: unknown): a is unknown[];
+   }
+
+   interface Body {
+      json(): Promise<unknown>;
+   }
 }
 
 declare module "react" {
@@ -262,6 +295,12 @@ declare module "react" {
       html?: Cx.StringProp;
       innerHtml?: Cx.StringProp;
       tooltip?: Cx.StringProp | Cx.StructuredProp;
+   }
+   namespace React {
+      interface ImgHTMLAttributes<T> extends HTMLAttributes<T> {
+         alt?: Cx.StringProp | undefined;
+         src?: Cx.StringProp | undefined;
+      }
    }
 
    //this doesn't work, however, it would be nice if it does
