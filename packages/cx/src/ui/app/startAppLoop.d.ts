11f56d3dbbbb2bc0f98c07180176a3c0ce74d6ba
vedranvucic77 <vedran.vucic@codaxy.com>
2025-10-10 12:48:45 +0200

    remove d.ts

--- packages/cx/src/ui/app/startAppLoop.d.ts
+++
@@ -1,4 +0,0 @@
-import { View } from '../../data/View';
-import { Instance } from '../Instance';
-
-export function startAppLoop(parentDOMElement: Element, storeOrInstance: View | Instance, widget?: any, options?: any);
