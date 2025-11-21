317b00dc14edd68d460cbca3c4e2427c75043b70
Marko Stijak <mstijak@gmail.com>
2025-11-21 11:03:37 +0100

    ...

--- packages/cx/src/widgets/autoFocus.ts
+++ packages/cx/src/widgets/autoFocus.ts
@@ -1,9 +1,9 @@
 import { FocusManager } from "../ui/FocusManager";
 import { isTouchEvent } from "../util/isTouchEvent";
 
-export function autoFocus(el: HTMLElement, component: any): void {
+export function autoFocus(el: HTMLElement | undefined | null, component: any): void {
    let data = component.props.data || component.props.instance.data;
    let autoFocusValue = el && data.autoFocus;
-   if (autoFocusValue && autoFocusValue != component.autoFocusValue && !isTouchEvent()) FocusManager.focus(el);
+   if (autoFocusValue && autoFocusValue != component.autoFocusValue && !isTouchEvent()) FocusManager.focus(el!);
    component.autoFocusValue = autoFocusValue;
 }
