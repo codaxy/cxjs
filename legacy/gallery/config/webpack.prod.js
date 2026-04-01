f14037e466e538bac3b260b20022d889b9bbaae5
Marko Stijak <marko.stijak@codaxy.com>
2026-04-01 15:51:34 +0200

    Update public path

--- legacy/gallery/config/webpack.prod.js
+++ legacy/gallery/config/webpack.prod.js
@@ -1,44 +1,48 @@
-var webpack = require('webpack'),
-   CopyWebpackPlugin = require('copy-webpack-plugin'),
-   { CleanWebpackPlugin } = require('clean-webpack-plugin'),
-   { merge } = require('webpack-merge'),
-   common = require('./webpack.config'),
-   path = require('path');
+var webpack = require("webpack"),
+   CopyWebpackPlugin = require("copy-webpack-plugin"),
+   { CleanWebpackPlugin } = require("clean-webpack-plugin"),
+   { merge } = require("webpack-merge"),
+   common = require("./webpack.config"),
+   path = require("path");
 
-let root = process.env.npm_lifecycle_event.indexOf(':root') != -1;
+let root = process.env.npm_lifecycle_event.indexOf(":root") != -1;
 
 var specific = {
-   mode: 'production',
-   target: ['web', 'es5'], //IE 11
+   mode: "production",
+   target: ["web", "es5"], //IE 11
    plugins: [
       new webpack.DefinePlugin({
-         'process.env.NODE_ENV': JSON.stringify('production')
+         "process.env.NODE_ENV": JSON.stringify("production"),
       }),
       //new WebpackCleanupPlugin(),
       new CopyWebpackPlugin({
-         patterns: [{
-            from: path.join(__dirname, '../assets'),
-            to: path.join(__dirname, '../dist/assets'),
-         }, {
-            from: path.resolve(__dirname, '../../misc/netlify.redirects'),
-            to: '_redirects',
-            toType: 'file'
-         }, {
-            from: path.resolve(__dirname, '../../misc/netlify.headers'),
-            to: '_headers',
-            toType: 'file'
-         }]
+         patterns: [
+            {
+               from: path.join(__dirname, "../assets"),
+               to: path.join(__dirname, "../dist/assets"),
+            },
+            {
+               from: path.resolve(__dirname, "../../misc/netlify.redirects"),
+               to: "_redirects",
+               toType: "file",
+            },
+            {
+               from: path.resolve(__dirname, "../../misc/netlify.headers"),
+               to: "_headers",
+               toType: "file",
+            },
+         ],
       }),
 
-      new CleanWebpackPlugin()
+      new CleanWebpackPlugin(),
    ],
 
    output: {
-      path: path.join(__dirname, '../dist'),
+      path: path.join(__dirname, "../dist"),
       filename: "[name].ltc.[chunkhash].js",
       hashDigestLength: 5,
-      publicPath: root ? "/" : "/gallery/"
-   }
+      publicPath: "/",
+   },
 };
 
 module.exports = merge(common(true), specific);
